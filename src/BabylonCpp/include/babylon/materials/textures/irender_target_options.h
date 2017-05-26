5550a8e0cf33b42c69b5ab4a81637894bf213991
samdauwe <sam.dauwe@gmail.com>
2017-05-26 21:06:48 +0200

    Added IRenderTargetOptions struct

---
+++ src/BabylonCpp/include/babylon/materials/textures/irender_target_options.h
@@ -0,0 +1,20 @@
+#ifndef BABYLON_MATERIALS_TEXTURES_IRENDER_TARGET_OPTIONS_H
+#define BABYLON_MATERIALS_TEXTURES_IRENDER_TARGET_OPTIONS_H
+
+#include <babylon/babylon_global.h>
+#include <babylon/engine/engine_constants.h>
+#include <babylon/materials/textures/texture_constants.h>
+
+namespace BABYLON {
+
+struct BABYLON_SHARED_EXPORT IRenderTargetOptions {
+  bool generateMipMaps       = false;
+  bool generateDepthBuffer   = true;
+  bool generateStencilBuffer = false;
+  unsigned int type          = EngineConstants::TEXTURETYPE_UNSIGNED_INT;
+  unsigned int samplingMode  = TextureConstants::TRILINEAR_SAMPLINGMODE;
+}; // end of struct RenderTargetOptions
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_TEXTURES_IRENDER_TARGET_OPTIONS_H
