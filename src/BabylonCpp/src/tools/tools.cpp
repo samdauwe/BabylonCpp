1e74820cad14da14a3b857b54237ca096968151a
samdauwe <sam.dauwe@gmail.com>
2017-07-27 22:12:27 +0200

    Hiding the warning: "Use of GNU statement expression extension".

--- src/BabylonCpp/src/tools/tools.cpp
+++ src/BabylonCpp/src/tools/tools.cpp
@@ -6,6 +6,7 @@
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wconversion"
 #if __GNUC__ > 5
+#pragma GCC diagnostic ignored "-Wgnu"
 #pragma GCC diagnostic ignored "-Wmisleading-indentation"
 #pragma GCC diagnostic ignored "-Wshift-negative-value"
 #endif
