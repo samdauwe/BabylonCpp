589b22212808cbefc2163722e84d20cce7769588
samdauwe <sam.dauwe@gmail.com>
2017-09-02 20:02:25 +0200

    Added FramingBehavior class

---
+++ src/BabylonCpp/include/babylon/behaviors/cameras/framing_behavior.h
@@ -0,0 +1,247 @@
+#ifndef BABYLON_BEHAVIORS_CAMERAS_FRAMING_BEHAVIOR_H
+#define BABYLON_BEHAVIORS_CAMERAS_FRAMING_BEHAVIOR_H
+
+#include <babylon/babylon_global.h>
+
+#include <babylon/animations/easing/exponential_ease.h>
+#include <babylon/behaviors/behavior.h>
+#include <babylon/core/nullable.h>
+#include <babylon/tools/observer.h>
+
+namespace BABYLON {
+
+class BABYLON_SHARED_EXPORT FramingBehavior : public Behavior<ArcRotateCamera> {
+
+public:
+  // Statics
+  /**
+   * The easing function used by animations
+   */
+  static ExponentialEase _EasingFunction;
+
+  /**
+   * The easing mode used by animations
+   */
+  static unsigned int EasingMode;
+
+  /**
+   * The camera can move all the way towards the mesh.
+   */
+  static constexpr unsigned int IgnoreBoundsSizeMode = 0;
+
+  /**
+   * The camera is not allowed to zoom closer to the mesh than the point at
+   * which the adjusted bounding sphere touches the frustum sides
+   */
+  static constexpr unsigned int FitFrustumSidesMode = 1;
+
+public:
+  FramingBehavior();
+  virtual ~FramingBehavior();
+
+  const char* name() const;
+
+  /**
+   * @brief Sets the current mode used by the behavior
+   */
+  void setMode(unsigned int mode);
+
+  /**
+   * @brief Gets current mode used by the behavior.
+   */
+  unsigned int mode() const;
+
+  /**
+   * @brief Sets the scale applied to the radius (1 by default)
+   */
+  void setRadiusScale(float radius);
+
+  /**
+   * @brief Gets the scale applied to the radius
+   */
+  float radiusScale() const;
+
+  /**
+   * @brief Sets the scale to apply on Y axis to position camera focus. 0.5 by
+   * default which means the center of the bounding box.
+   */
+  void setPositionScale(float scale);
+
+  /**
+   * @brief Gets the scale to apply on Y axis to position camera focus. 0.5 by
+   * default which means the center of the bounding box.
+   */
+  float positionScale() const;
+
+  /**
+   * @brief Sets the angle above/below the horizontal plane to return to when
+   * the return to default elevation idle behaviour is triggered, in radians.
+   */
+  void setDefaultElevation(float elevation);
+
+  /**
+   * @brief Gets the angle above/below the horizontal plane to return to when
+   * the return to default elevation idle behaviour is triggered, in radians.
+   */
+  float defaultElevation() const;
+
+  /**
+   * @brief Sets the time (in milliseconds) taken to return to the default beta
+   * position.
+   * Negative value indicates camera should not return to default.
+   */
+  void setElevationReturnTime(float speed);
+
+  /**
+   * @brief Gets the time (in milliseconds) taken to return to the default beta
+   * position.
+   * Negative value indicates camera should not return to default.
+   */
+  float elevationReturnTime() const;
+
+  /**
+   * @brief Sets the delay (in milliseconds) taken before the camera returns to
+   * the default beta position.
+   */
+  void setElevationReturnWaitTime(float time);
+
+  /**
+   * @brief Gets the delay (in milliseconds) taken before the camera returns to
+   * the default beta position.
+   */
+  float elevationReturnWaitTime() const;
+
+  /**
+   * @brief Sets the flag that indicates if user zooming should stop animation.
+   */
+  void setZoomStopsAnimation(bool flag);
+
+  /**
+   * @brief Gets the flag that indicates if user zooming should stop animation.
+   */
+  bool zoomStopsAnimation() const;
+
+  /**
+   * Sets the transition time when framing the mesh, in milliseconds
+   */
+  void setFramingTime(float time);
+
+  /**
+   * @brief Gets the transition time when framing the mesh, in milliseconds
+   */
+  float framingTime() const;
+
+  void attach(ArcRotateCamera* camera) override;
+
+  void detach() override;
+
+  /**
+   * @brief Targets the given mesh and updates zoom level accordingly.
+   * @param mesh  The mesh to target.
+   * @param radius Optional. If a cached radius position already exists,
+   * overrides default.
+   * @param framingPositionY Position on mesh to center camera focus where 0
+   * corresponds bottom of its bounding box and 1, the top
+   * @param focusOnOriginXZ Determines if the camera should focus on 0 in the X
+   * and Z axis instead of the mesh
+   */
+  void zoomOnMesh(AbstractMesh* mesh, bool focusOnOriginXZ = false);
+
+  /**
+   * @brief Targets the given mesh and updates zoom level accordingly.
+   * @param mesh  The mesh to target.
+   * @param radius Optional. If a cached radius position already exists,
+   * overrides default.
+   * @param framingPositionY Position on mesh to center camera focus where 0
+   * corresponds bottom of its bounding box and 1, the top
+   * @param focusOnOriginXZ Determines if the camera should focus on 0 in the X
+   * and Z axis instead of the mesh
+   */
+  void zoomOnBoundingInfo(const Vector3& minimumWorld,
+                          const Vector3& maximumWorld,
+                          bool focusOnOriginXZ = false);
+
+protected:
+  /**
+   * @brief Calculates the lowest radius for the camera based on the bounding
+   * box of the mesh.
+   * @param mesh The mesh on which to base the calculation. mesh boundingInfo
+   * used to estimate necessary frustum width.
+   * @return The minimum distance from the primary mesh's center point at which
+   * the camera must be kept in order to fully enclose the mesh in the viewing
+   * frustum.
+   */
+  float
+  _calculateLowerRadiusFromModelBoundingSphere(const Vector3& minimumWorld,
+                                               const Vector3& maximumWorld);
+
+private:
+  /**
+   * @brief Keeps the camera above the ground plane. If the user pulls the
+   * camera below the ground plane, the camera is automatically returned to its
+   * default position (expected to be above ground plane).
+   */
+  void _maintainCameraAboveGround();
+
+  /**
+   * @brief Returns the frustum slope based on the canvas ratio and camera FOV
+   * @returns The frustum slope represented as a Vector2 with X and Y slopes
+   */
+  Vector2 _getFrustumSlope() const;
+
+  /**
+   * @brief Removes all animation locks. Allows new animations to be added to
+   * any of the arcCamera properties.
+   */
+  void _clearAnimationLocks();
+
+  /**
+   * @brief Applies any current user interaction to the camera. Takes into
+   * account
+   * maximum alpha rotation.
+   */
+  void _applyUserInteraction();
+
+  /**
+   * @brief Stops and removes all animations that have been applied to the
+   * camera
+   */
+  void stopAllAnimations();
+
+  /**
+   * @brief Gets a value indicating if the user is moving the camera
+   */
+  bool isUserIsMoving() const;
+
+private:
+  unsigned int _mode;
+  float _radiusScale;
+  float _positionScale;
+  float _defaultElevation;
+  float _elevationReturnTime;
+  float _elevationReturnWaitTime;
+  bool _zoomStopsAnimation;
+  float _framingTime;
+
+  // Default behavior functions
+  Observer<PointerInfoPre>::Ptr _onPrePointerObservableObserver;
+  Observer<Camera>::Ptr _onAfterCheckInputsObserver;
+  Observer<AbstractMesh>::Ptr _onMeshTargetChangedObserver;
+  ArcRotateCamera* _attachedCamera;
+  bool _isPointerDown;
+  Nullable<high_res_time_point_t> _lastFrameTime;
+  high_res_time_point_t _lastInteractionTime;
+
+  // Framing control
+  std::vector<Animatable*> _animatables;
+  bool _betaIsAnimating;
+  Animation* _betaTransition;
+  Animation* _radiusTransition;
+  Animation* _vectorTransition;
+  float _lastFrameRadius;
+
+}; // end of class FramingBehavior
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_BEHAVIORS_CAMERAS_FRAMING_BEHAVIOR_H
---
+++ src/BabylonCpp/src/behaviors/cameras/framing_behavior.cpp
@@ -0,0 +1,371 @@
+#include <babylon/behaviors/cameras/framing_behavior.h>
+
+#include <babylon/animations/animatable.h>
+#include <babylon/animations/animation.h>
+#include <babylon/babylon_stl_util.h>
+#include <babylon/cameras/arc_rotate_camera.h>
+#include <babylon/core/time.h>
+#include <babylon/engine/engine.h>
+#include <babylon/engine/pointer_event_types.h>
+#include <babylon/mesh/abstract_mesh.h>
+
+namespace BABYLON {
+
+ExponentialEase FramingBehavior::_EasingFunction = ExponentialEase();
+unsigned int FramingBehavior::EasingMode = EasingFunction::EASINGMODE_EASEINOUT;
+
+FramingBehavior::FramingBehavior()
+    : _mode{FramingBehavior::FitFrustumSidesMode}
+    , _radiusScale{1.f}
+    , _positionScale{0.5f}
+    , _defaultElevation{0.3f}
+    , _elevationReturnTime{1500}
+    , _elevationReturnWaitTime{1000}
+    , _zoomStopsAnimation{false}
+    , _framingTime{1500}
+    , _isPointerDown{false}
+    , _betaIsAnimating{false}
+    , _betaTransition{nullptr}
+    , _radiusTransition{nullptr}
+    , _vectorTransition{nullptr}
+    , _lastFrameRadius{0.f}
+{
+}
+
+FramingBehavior::~FramingBehavior()
+{
+}
+
+const char* FramingBehavior::name() const
+{
+  return "Framing";
+}
+
+void FramingBehavior::setMode(unsigned int mode)
+{
+  _mode = mode;
+}
+
+unsigned int FramingBehavior::mode() const
+{
+  return _mode;
+}
+
+void FramingBehavior::setRadiusScale(float radius)
+{
+  _radiusScale = radius;
+}
+
+float FramingBehavior::radiusScale() const
+{
+  return _radiusScale;
+}
+
+void FramingBehavior::setPositionScale(float scale)
+{
+  _positionScale = scale;
+}
+
+float FramingBehavior::positionScale() const
+{
+  return _positionScale;
+}
+
+void FramingBehavior::setDefaultElevation(float elevation)
+{
+  _defaultElevation = elevation;
+}
+
+float FramingBehavior::defaultElevation() const
+{
+  return _defaultElevation;
+}
+
+void FramingBehavior::setElevationReturnTime(float speed)
+{
+  _elevationReturnTime = speed;
+}
+
+float FramingBehavior::elevationReturnTime() const
+{
+  return _elevationReturnTime;
+}
+
+void FramingBehavior::setElevationReturnWaitTime(float time)
+{
+  _elevationReturnWaitTime = time;
+}
+
+float FramingBehavior::elevationReturnWaitTime() const
+{
+  return _elevationReturnWaitTime;
+}
+
+void FramingBehavior::setZoomStopsAnimation(bool flag)
+{
+  _zoomStopsAnimation = flag;
+}
+
+bool FramingBehavior::zoomStopsAnimation() const
+{
+  return _zoomStopsAnimation;
+}
+
+void FramingBehavior::setFramingTime(float time)
+{
+  _framingTime = time;
+}
+
+float FramingBehavior::framingTime() const
+{
+  return _framingTime;
+}
+
+void FramingBehavior::attach(ArcRotateCamera* camera)
+{
+  _attachedCamera = camera;
+  auto scene      = _attachedCamera->getScene();
+
+  FramingBehavior::_EasingFunction.setEasingMode(FramingBehavior::EasingMode);
+
+  _onPrePointerObservableObserver
+    = scene->onPrePointerObservable.add([this](PointerInfoPre* pointerInfoPre) {
+        if (pointerInfoPre->type == PointerEventTypes::POINTERDOWN) {
+          _isPointerDown = true;
+          return;
+        }
+
+        if (pointerInfoPre->type == PointerEventTypes::POINTERUP) {
+          _isPointerDown = false;
+        }
+      });
+
+  _onMeshTargetChangedObserver
+    = camera->onMeshTargetChangedObservable.add([this](AbstractMesh* mesh) {
+        if (mesh) {
+          zoomOnMesh(mesh);
+        }
+      });
+
+  _onAfterCheckInputsObserver
+    = camera->onAfterCheckInputsObservable.add([this]() {
+        // Stop the animation if there is user interaction and the animation
+        // should stop for this interaction
+        _applyUserInteraction();
+
+        // Maintain the camera above the ground. If the user pulls the camera
+        // beneath the ground plane, lift it back to the default position after
+        // a given timeout
+        _maintainCameraAboveGround();
+      });
+}
+
+void FramingBehavior::detach()
+{
+  auto scene = _attachedCamera->getScene();
+
+  scene->onPrePointerObservable.remove(_onPrePointerObservableObserver);
+  _attachedCamera->onAfterCheckInputsObservable.remove(
+    _onAfterCheckInputsObserver);
+  _attachedCamera->onMeshTargetChangedObservable.remove(
+    _onMeshTargetChangedObserver);
+
+  _attachedCamera = nullptr;
+}
+
+void FramingBehavior::zoomOnMesh(AbstractMesh* mesh, bool focusOnOriginXZ)
+{
+  mesh->computeWorldMatrix(true);
+
+  const auto& boundingBox = mesh->getBoundingInfo()->boundingBox;
+  zoomOnBoundingInfo(boundingBox.minimumWorld, boundingBox.maximumWorld,
+                     focusOnOriginXZ);
+}
+
+void FramingBehavior::zoomOnBoundingInfo(const Vector3& minimumWorld,
+                                         const Vector3& maximumWorld,
+                                         bool focusOnOriginXZ = false)
+{
+  Vector3 zoomTarget;
+
+  // Find target by interpolating from bottom of bounding box in world-space to
+  // top via framingPositionY
+  auto bottom      = minimumWorld.y;
+  auto top         = maximumWorld.y;
+  auto zoomTargetY = bottom + (top - bottom) * _positionScale;
+  auto radiusWorld = maximumWorld.subtract(minimumWorld).scale(0.5f);
+
+  if (focusOnOriginXZ) {
+    zoomTarget = Vector3(0, zoomTargetY, 0);
+  }
+  else {
+    auto centerWorld = minimumWorld.add(radiusWorld);
+    zoomTarget       = Vector3(centerWorld.x, zoomTargetY, centerWorld.z);
+  }
+
+  if (!_vectorTransition) {
+    _vectorTransition
+      = Animation::CreateAnimation("target", Animation::ANIMATIONTYPE_VECTOR3,
+                                   60, FramingBehavior::_EasingFunction);
+  }
+
+  _betaIsAnimating = true;
+  _animatables.emplace_back(Animation::TransitionTo(
+    "target", zoomTarget, _attachedCamera, _attachedCamera->getScene(), 60,
+    _vectorTransition, _framingTime));
+
+  // sets the radius and lower radius bounds
+  // Small delta ensures camera is not always at lower zoom limit.
+  auto radius = 0.f;
+  if (_mode == FramingBehavior::FitFrustumSidesMode) {
+    auto position = _calculateLowerRadiusFromModelBoundingSphere(minimumWorld,
+                                                                 maximumWorld);
+    _attachedCamera->lowerRadiusLimit
+      = radiusWorld.length() + _attachedCamera->minZ;
+    radius = position;
+  }
+  else if (_mode == FramingBehavior::IgnoreBoundsSizeMode) {
+    radius = _calculateLowerRadiusFromModelBoundingSphere(minimumWorld,
+                                                          maximumWorld);
+    if (_attachedCamera->lowerRadiusLimit == 0.f) {
+      _attachedCamera->lowerRadiusLimit = _attachedCamera->minZ;
+    }
+  }
+
+  // transition to new radius
+  if (!_radiusTransition) {
+    _radiusTransition
+      = Animation::CreateAnimation("radius", Animation::ANIMATIONTYPE_FLOAT, 60,
+                                   FramingBehavior::_EasingFunction);
+  }
+
+  _animatables.emplace_back(Animation::TransitionTo(
+    "radius", radius, _attachedCamera, _attachedCamera->getScene(), 60,
+    _radiusTransition, _framingTime));
+}
+
+float FramingBehavior::_calculateLowerRadiusFromModelBoundingSphere(
+  const Vector3& minimumWorld, const Vector3& maximumWorld)
+{
+  auto size                    = maximumWorld.subtract(minimumWorld);
+  auto boxVectorGlobalDiagonal = size.length();
+  auto frustumSlope            = _getFrustumSlope();
+
+  // Formula for setting distance
+  // (Good explanation:
+  // http://stackoverflow.com/questions/2866350/move-camera-to-fit-3d-scene)
+  auto radiusWithoutFraming = boxVectorGlobalDiagonal * 0.5f;
+
+  // Horizon distance
+  auto radius = radiusWithoutFraming * _radiusScale;
+  auto distanceForHorizontalFrustum
+    = radius * std::sqrt(1.f + 1.f / (frustumSlope.x * frustumSlope.x));
+  auto distanceForVerticalFrustum
+    = radius * std::sqrt(1.f + 1.f / (frustumSlope.y * frustumSlope.y));
+  auto distance
+    = std::max(distanceForHorizontalFrustum, distanceForVerticalFrustum);
+  auto camera = _attachedCamera;
+
+  if (camera->lowerRadiusLimit != 0.f
+      && _mode == FramingBehavior::IgnoreBoundsSizeMode) {
+    // Don't exceed the requested limit
+    distance = distance < camera->lowerRadiusLimit ? camera->lowerRadiusLimit :
+                                                     distance;
+  }
+
+  // Don't exceed the upper radius limit
+  if (camera->upperRadiusLimit != 0.f) {
+    distance = distance > camera->upperRadiusLimit ? camera->upperRadiusLimit :
+                                                     distance;
+  }
+
+  return distance;
+}
+
+void FramingBehavior::_maintainCameraAboveGround()
+{
+  auto now = Time::highresTimepointNow();
+  auto timeSinceInteraction
+    = Time::fpTimeDiff<float, std::milli>(now, _lastInteractionTime);
+  auto defaultBeta = Math::PI * 0.5f - _defaultElevation;
+  auto limitBeta   = Math::PI * 0.5f;
+
+  // Bring the camera back up if below the ground plane
+  if (!_betaIsAnimating && _attachedCamera->beta > limitBeta
+      && timeSinceInteraction >= _elevationReturnWaitTime) {
+    _betaIsAnimating = true;
+
+    // Transition to new position
+    stopAllAnimations();
+
+    if (!_betaTransition) {
+      _betaTransition
+        = Animation::CreateAnimation("beta", Animation::ANIMATIONTYPE_FLOAT, 60,
+                                     FramingBehavior::_EasingFunction);
+    }
+
+    _animatables.emplace_back(Animation::TransitionTo(
+      "beta", defaultBeta, _attachedCamera, _attachedCamera->getScene(), 60,
+      _betaTransition, _elevationReturnTime, [this]() {
+        _clearAnimationLocks();
+        stopAllAnimations();
+      }));
+  }
+}
+
+Vector2 FramingBehavior::_getFrustumSlope() const
+{
+  // Calculate the viewport ratio
+  // Aspect Ratio is Height/Width.
+  auto camera      = _attachedCamera;
+  auto engine      = camera->getScene()->getEngine();
+  auto aspectRatio = engine->getAspectRatio(camera);
+
+  // Camera FOV is the vertical field of view (top-bottom) in radians.
+  // Slope of the frustum top/bottom planes in view space, relative to the
+  // forward vector.
+  auto frustumSlopeY = std::tan(camera->fov / 2);
+
+  // Slope of the frustum left/right planes in view space, relative to the
+  // forward vector. Provides the amount that one side (e.g. left) of the
+  // frustum gets wider for every unit along the forward vector.
+  auto frustumSlopeX = frustumSlopeY * aspectRatio;
+
+  return Vector2(frustumSlopeX, frustumSlopeY);
+}
+
+void FramingBehavior::_clearAnimationLocks()
+{
+  _betaIsAnimating = false;
+}
+
+void FramingBehavior::_applyUserInteraction()
+{
+  if (isUserIsMoving()) {
+    _lastInteractionTime = Time::highresTimepointNow();
+    stopAllAnimations();
+    _clearAnimationLocks();
+  }
+}
+
+void FramingBehavior::stopAllAnimations()
+{
+  _attachedCamera->animations.clear();
+  for (auto& animatable : _animatables) {
+    animatable->onAnimationEnd = nullptr;
+    animatable->stop();
+  }
+  _animatables.clear();
+}
+
+bool FramingBehavior::isUserIsMoving() const
+{
+  return !stl_util::almost_equal(_attachedCamera->inertialAlphaOffset, 0.f)
+         || !stl_util::almost_equal(_attachedCamera->inertialBetaOffset, 0.f)
+         || !stl_util::almost_equal(_attachedCamera->inertialRadiusOffset, 0.f)
+         || !stl_util::almost_equal(_attachedCamera->inertialPanningX, 0.f)
+         || !stl_util::almost_equal(_attachedCamera->inertialPanningY, 0.f)
+         || _isPointerDown;
+}
+
+} // end of namespace BABYLON
