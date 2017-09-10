d523166688e0d04dc0907e7979718a2bc54c3b5b
samdauwe <sam.dauwe@gmail.com>
2017-09-10 18:37:12 +0200

    Added DevicePose and PoseControlled classes

---
+++ src/BabylonCpp/include/babylon/cameras/vr/device_pose.h
@@ -0,0 +1,20 @@
+#ifndef BABYLON_CAMERAS_VR_DEVICE_POSE_H
+#define BABYLON_CAMERAS_VR_DEVICE_POSE_H
+
+#include <babylon/babylon_global.h>
+
+namespace BABYLON {
+
+struct BABYLON_SHARED_EXPORT DevicePose {
+  Float32Array position;
+  Float32Array linearVelocity;
+  Float32Array linearAcceleration;
+
+  Float32Array orientation;
+  Float32Array angularVelocity;
+  Float32Array angularAcceleration;
+}; // end of struct DevicePose
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_CAMERAS_VR_DEVICE_POSE_H
---
+++ src/BabylonCpp/include/babylon/cameras/vr/pose_controlled.h
@@ -0,0 +1,23 @@
+#ifndef BABYLON_CAMERAS_VR_POSE_CONTROLLED_H
+#define BABYLON_CAMERAS_VR_POSE_CONTROLLED_H
+
+#include <babylon/babylon_global.h>
+#include <babylon/cameras/vr/device_pose.h>
+#include <babylon/math/quaternion.h>
+#include <babylon/math/vector3.h>
+
+namespace BABYLON {
+
+struct BABYLON_SHARED_EXPORT PoseControlled {
+  Vector3 position;
+  Quaternion rotationQuaternion;
+  Vector3 devicePosition;
+  Quaternion deviceRotationQuaternion;
+  DevicePose rawPose;
+  float deviceScaleFactor;
+  virtual void updateFromDevice(const DevicePose& poseData) = 0;
+}; // end of struct PoseControlled
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_CAMERAS_VR_POSE_CONTROLLED_H
