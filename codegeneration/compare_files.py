f453a1cba71ae609d35e93114e657dff61e87eb9
samdauwe <sam.dauwe@gmail.com>
2017-10-13 22:26:53 +0200

    Added source tree compare script

---
+++ codegeneration/compare_files.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import os
+
+def getFilesRecursively(directory, extension = ".ts"):
+    """
+    Returns the files recursively in a directory matching a pattern.
+    """
+    fileList = []
+    for root, dirs, files in os.walk(directory, topdown=True):
+        for filename in files:
+            fullpath = os.path.join(root, filename)
+            if os.path.isfile(fullpath) and filename.endswith(extension):
+                fileList.append(fullpath)
+    return fileList
+
+def filesAreEqual(file1, file2):
+    """
+    Compare the files file1 and file2 and returns wheter or not they are equal.
+    """
+    import filecmp
+    return filecmp.cmp(file1, file2)
+
+def compareFiles(currentDir, previousDir, fileList):
+    """
+    Checks which files were changed or added compared to the previous version.
+    """
+    result = {"ChangedFiles" : [], "NewFiles" : []}
+    for file in fileList:
+        # ignore already updated files
+        if file.endswith("_done.ts"):
+            continue
+        # check if file has been changed
+        prevFullpathOrig = file.replace(currentDir, previousDir)
+        prevFullpathDone = prevFullpathOrig.replace(".ts", "_done.ts")
+        fileFound = False
+        for prevFileFullPath in [prevFullpathOrig, prevFullpathDone]:
+            if os.path.isfile(prevFileFullPath):
+                fileFound = True
+                if not filesAreEqual(file, prevFileFullPath):
+                    result["ChangedFiles"].append(file)
+        # check if previous file exists
+        if not fileFound:
+            result["NewFiles"].append(file)
+    return result
+
+def fileComparisonToStr(fileComparisonDict):
+    indent = ' ' * 6
+    result = "Found %d changed files:\n" % \
+             len(fileComparisonDict["ChangedFiles"])
+    for file in fileComparisonDict["ChangedFiles"]:
+        result += "%s|- %s\n" % (indent, file)
+    result += "Found %d new files:\n" % \
+              len(fileComparisonDict["NewFiles"])
+    for file in fileComparisonDict["NewFiles"]:
+        result += "%s|- %s\n" % (indent, file)
+    return result
+
+if __name__ == "__main__":
+    previous = "/home/sdauwe/Projects/Babylon.js-3.1.0_2017_08_28/src"
+    current = "/home/sdauwe/Projects/Babylon.js-3.1.0_2017_09_23/src"
+    files = getFilesRecursively(current)
+    fileComparisonDict = compareFiles(current, previous, files)
+    print fileComparisonToStr(fileComparisonDict)
