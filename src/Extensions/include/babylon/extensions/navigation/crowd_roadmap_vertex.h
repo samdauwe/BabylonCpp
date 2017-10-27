73acc2983f4108bb3666096f4fbeea1e9f923034
samdauwe <sam.dauwe@gmail.com>
2017-10-27 21:47:39 +0200

    Added CrowdRoadmapVertex to the navigation extension

---
+++ src/Extensions/include/babylon/extensions/navigation/crowd_roadmap_vertex.h
@@ -0,0 +1,19 @@
+#ifndef BABYLON_EXTENSIONS_NAVIGATION_CROWD_ROADMAP_VERTEX_H
+#define BABYLON_EXTENSIONS_NAVIGATION_CROWD_ROADMAP_VERTEX_H
+
+#include <babylon/babylon_global.h>
+#include <babylon/extensions/navigation/rvo2/vector2.h>
+
+namespace BABYLON {
+namespace Extensions {
+
+struct BABYLON_SHARED_EXPORT CrowdRoadmapVertex {
+  RVO2::Vector2 position;
+  Uint32Array neighbors;
+  Float32Array distToGoal;
+}; // end of struct CrowdRoadmapVertex
+
+} // end of namespace Extensions
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_EXTENSIONS_NAVIGATION_CROWD_ROADMAP_VERTEX_H
