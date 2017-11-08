8990d9eeed9ccd17c7005b8f01f4360cd32ef4cf
samdauwe <sam.dauwe@gmail.com>
2017-11-08 22:08:31 +0100

    Extended glTF 2.0 File Loader header and excluded from the build

--- src/Loaders/include/babylon/loading/glTF/2.0/gltf_loader.h
+++ src/Loaders/include/babylon/loading/glTF/2.0/gltf_loader.h
@@ -8,6 +8,12 @@
 namespace BABYLON {
 namespace GLTF2 {
 
+struct GLTFFileLoader;
+struct GLTFLoaderExtension;
+struct GLTFLoaderTracker;
+struct IGLTFLoaderData;
+struct ProgressEvent;
+
 /**
  * @brief Babylon.js glTF File Loader. This loader supports only glTF 2.0 and
  * will fail to load glTF 1.0.
@@ -15,16 +21,174 @@ namespace GLTF2 {
 class BABYLON_SHARED_EXPORT GLTFLoader {
 
 public:
-  GLTFLoader();
+  static void RegisterExtension(GLTFLoaderExtension* extension);
+
+public:
+  GLTFLoader(GLTFFileLoader* parent);
   ~GLTFLoader();
 
+  void dispose();
+  void importMeshAsync(
+    const vector_t<string_t>& meshesNames, Scene* scene,
+    const IGLTFLoaderData& data, const vector_t<string_t>& rootUrl,
+    const ::std::function<void(
+      const vector_t<AbstractMesh*>& meshes,
+      const vector_t<ParticleSystem*>& particleSystems,
+      const vector_t<Skeleton*>& skeletons,
+      const ::std::function<void(const ProgressEvent& event)>& onProgress,
+      const ::std::function<void(const string_t& message)>& onError)>&
+      onSuccess);
+  void
+  loadAsync(Scene* scene, const IGLTFLoaderData& data, const string_t& rootUrl,
+            const ::std::function<void()>& onSuccess,
+            const ::std::function<void(const ProgressEvent& event)>& onProgress,
+            const ::std::function<void(const string_t& message)>& onError);
+  void _executeWhenRenderReady(const ::std::function<void()>& func);
+  void _loadNode(const string_t& context, const IGLTFNode& node);
+  void _traverseNode(
+    const string_t& context, IGLTFNode* node,
+    ::std::function<bool(IGLTFNode* node, IGLTFNode* parentNode)>& action);
+  void _whenAction(const ::std::function<void()>& action,
+                   const ::std::function<void()>& onComplete);
+  void _loadMaterial(
+    const string_t& context, IGLTFMaterial& material,
+    const ::std::function<void(Material* babylonMaterial, bool isNew)>& assign);
+  void _createPbrMaterial(IGLTFMaterial& material);
+  void _loadMaterialBaseProperties(const string_t& context,
+                                   const IGLTFMaterial& material);
+  void _loadMaterialAlphaProperties(const string_t& context,
+                                    const IGLTFMaterial& material,
+                                    const Float32Array& colorFactor);
+  Texture* _loadTexture(const string_t& context, const IGLTFTexture& texture,
+                        int coordinatesIndex = 0);
+  void _loadUri(const string_t& context, const string_t& uri,
+                const ::std::function<void(ArrayBuffer& data)>& onSuccess);
+  void _tryCatchOnError(const ::std::function<void()>& handler);
+
 private:
+  void _loadAsync(
+    const vector_t<string_t>& nodeNames, Scene* scene,
+    const IGLTFLoaderData& data, const string_t& rootUrl,
+    const ::std::function<void()>& onSuccess,
+    const ::std::function<void(const ProgressEvent& event)>& onProgress,
+    const ::std::function<void(const string_t& message)>& onError);
+  void _onProgress(const ProgressEvent& event);
+  void _onRenderReady();
+  void _onComplete();
+  void _loadData(const IGLTFLoaderData& data);
+  vector_t<AbstractMesh*> _getMeshes();
+  vector_t<Skeleton*> _getSkeletons();
+  vector_t<Node*> _getAnimationTargets();
+  void _startAnimations();
+  void _loadDefaultScene(const vector_t<string_t>& nodeNames);
+  void _loadScene(const string_t& context, const IGLTFScene& scene,
+                  const vector_t<string_t>& nodeNames);
+  void _loadMesh(const string_t& context, const IGLTFNode& node,
+                 const IGLTFMesh& mesh);
+  void _loadAllVertexDataAsync(const string_t& context, const IGLTFMesh& mesh,
+                               const ::std::function<void()>& onSuccess);
+
+  /**
+   * @brief Converts a data bufferview into a Float4 Texture Coordinate Array,
+   * based on the accessor component type
+   * @param {ArrayBufferView} data
+   * @param {IGLTFAccessor} accessor
+   */
+  Float32Array _convertToFloat4TextureCoordArray(const string_t& context,
+                                                 const ArrayBufferView& data,
+                                                 const IGLTFAccessor& accessor);
+
+  /**
+   * @ brief Converts a data bufferview into a Float4 Color Array, based on the
+   * accessor component type
+   * @param {ArrayBufferView} data
+   * @param {IGLTFAccessor} accessor
+   */
+  Float32Array _convertToFloat4ColorArray(const string_t& context,
+                                          const ArrayBufferView& data,
+                                          const IGLTFAccessor& accessor);
+
+  void _loadVertexDataAsync(
+    const string_t& context, const IGLTFMesh& mesh,
+    const IGLTFMeshPrimitive& primitive,
+    const ::std::function<void(const VertexData& vertexData)>& onSuccess);
+  void _createMorphTargets(const string_t& context, const IGLTFNode& node,
+                           const IGLTFMesh& mesh);
+  void _loadMorphTargets(const string_t& context, const IGLTFNode& node,
+                         const IGLTFMesh& mesh);
+  void _loadAllMorphTargetVertexDataAsync(
+    const string_t& context, const IGLTFNode& node, const IGLTFMesh& mesh,
+    const ::std::function<void()>& onSuccess);
+  void _loadMorphTargetVertexDataAsync(
+    const string_t& context, const VertexData& vertexData,
+    const unordered_map_t<string_t, int>& attributes,
+    const ::std::function<void(const VertexData& vertexData)>& onSuccess);
+  void _loadTransform(IGLTFNode* node);
+  Skeleton* _loadSkin(const string_t& context, const IGLTFSkin& skin);
+  Bone* _createBone(IGLTFNode* node, const IGLTFSkin& skin, Bone* parent,
+                    const Matrix& localMatrix, const Matrix& baseMatrix,
+                    unsigned int index);
+  void _loadBone(const string_t& context, const IGLTFSkin& skin,
+                 const Float32Array& inverseBindMatrixData);
+  Bone* _loadBone(IGLTFNode* node, const IGLTFSkin& skin,
+                  const Float32Array& inverseBindMatrixData,
+                  const unordered_map_t<int, Bone*>& babylonBones);
+  Matrix _getNodeMatrix(const IGLTFNode& node);
+  void _traverseNodes(
+    const string_t& context, const Uint32Array& indices,
+    ::std::function<bool(IGLTFNode* node, IGLTFNode* parentNode)>& action,
+    IGLTFNode* parentNode = nullptr);
+  void _loadAnimations();
+  void _loadAnimation(const string_t& context, const IGLTFAnimation& animation);
+  void _loadAnimationChannel(const IGLTFAnimation& animation,
+                             const string_t& channelContext,
+                             const IGLTFAnimationChannel& channel,
+                             const string_t& samplerContext,
+                             const IGLTFAnimationSampler& sampler);
+  void
+  _loadBufferAsync(const string_t& context, const IGLTFBuffer& buffer,
+                   const ::std::function<void(ArrayBuffer& data)>& onSucces);
+  void _loadBufferViewAsync(
+    const string_t& context, const IGLTFBufferView& bufferView,
+    const ::std::function<void(ArrayBuffer& data)>& onSucces);
+  void
+  _loadAccessorAsync(const string_t& context, const IGLTFAccessor& accessor,
+                     const ::std::function<void(ArrayBuffer& data)>& onSuccess);
+  Material* _getDefaultMaterial();
+  void _loadMaterialMetallicRoughnessProperties(const string_t& context,
+                                                const IGLTFMaterial& material);
+  void _loadImage(const string_t& context, const IGLTFImage& image,
+                  const ::std::function<void(ArrayBuffer& data)>& onSuccess);
   static unsigned int
   _GetTextureWrapMode(const Nullable<ETextureWrapMode>& mode);
   static unsigned int
   _GetTextureSamplingMode(const Nullable<ETextureMagFilter>& magFilter,
                           const Nullable<ETextureMinFilter>& minFilter);
   static unsigned int _GetNumComponents(const string_t& type);
+
+public:
+  IGLTF* _gltf;
+  Scene* _babylonScene;
+
+public:
+  static unordered_map_t<string_t, GLTFLoaderExtension> Extensions;
+
+private:
+  bool _disposed;
+  GLTFFileLoader* _parent;
+  string_t _rootUrl;
+  PBRMaterial* _defaultMaterial;
+  IGLTFNode* _rootNode;
+  ::std::function<void()> _successCallback;
+  ::std::function<void(const ProgressEvent& event)> _progressCallback;
+  ::std::function<void(const string_t& message)> _errorCallback;
+  bool _renderReady;
+  Observable<GLTFLoader> _renderReadyObservable;
+  // Count of pending work that needs to complete before the asset is rendered.
+  size_t _renderPendingCount;
+  // Count of pending work that needs to complete before the loader is disposed.
+  size_t _loaderPendingCount;
+  vector_t<GLTFLoaderTracker> _loaderTrackers;
 
 }; // end of class GLTFLoader
 
--- src/Loaders/src/loading/glTF/2.0/gltf_loader.cpp
+++ src/Loaders/src/loading/glTF/2.0/gltf_loader.cpp
@@ -6,11 +6,287 @@
 namespace BABYLON {
 namespace GLTF2 {
 
-GLTFLoader::GLTFLoader()
+void GLTFLoader::RegisterExtension(GLTFLoaderExtension* extension)
+{
+}
+
+GLTFLoader::GLTFLoader(GLTFFileLoader* parent)
 {
 }
 
 GLTFLoader::~GLTFLoader()
+{
+}
+
+void GLTFLoader::dispose()
+{
+}
+
+void GLTFLoader::importMeshAsync(
+  const vector_t<string_t>& meshesNames, Scene* scene,
+  const IGLTFLoaderData& data, const vector_t<string_t>& rootUrl,
+  const ::std::function<void(
+    const vector_t<AbstractMesh*>& meshes,
+    const vector_t<ParticleSystem*>& particleSystems,
+    const vector_t<Skeleton*>& skeletons,
+    const ::std::function<void(const ProgressEvent& event)>& onProgress,
+    const ::std::function<void(const string_t& message)>& onError)>& onSuccess)
+{
+}
+
+void GLTFLoader::loadAsync(
+  Scene* scene, const IGLTFLoaderData& data, const string_t& rootUrl,
+  const ::std::function<void()>& onSuccess,
+  const ::std::function<void(const ProgressEvent& event)>& onProgress,
+  const ::std::function<void(const string_t& message)>& onError)
+{
+}
+
+void GLTFLoader::_loadAsync(
+  const vector_t<string_t>& nodeNames, Scene* scene,
+  const IGLTFLoaderData& data, const string_t& rootUrl,
+  const ::std::function<void()>& onSuccess,
+  const ::std::function<void(const ProgressEvent& event)>& onProgress,
+  const ::std::function<void(const string_t& message)>& onError)
+{
+}
+
+void GLTFLoader::_onProgress(const ProgressEvent& event)
+{
+}
+
+void GLTFLoader::_executeWhenRenderReady(const ::std::function<void()>& func)
+{
+}
+
+void GLTFLoader::_onRenderReady()
+{
+}
+
+void GLTFLoader::_onComplete()
+{
+}
+
+void GLTFLoader::_loadData(const IGLTFLoaderData& data)
+{
+}
+
+vector_t<AbstractMesh*> GLTFLoader::_getMeshes()
+{
+}
+
+vector_t<Skeleton*> GLTFLoader::_getSkeletons()
+{
+}
+
+vector_t<Node*> GLTFLoader::_getAnimationTargets()
+{
+}
+
+void GLTFLoader::_startAnimations()
+{
+}
+
+void GLTFLoader::_loadDefaultScene(const vector_t<string_t>& nodeNames)
+{
+}
+
+void GLTFLoader::_loadScene(const string_t& context, const IGLTFScene& scene,
+                            const vector_t<string_t>& nodeNames)
+{
+}
+
+void GLTFLoader::_loadNode(const string_t& context, const IGLTFNode& node)
+{
+}
+
+void GLTFLoader::_loadMesh(const string_t& context, const IGLTFNode& node,
+                           const IGLTFMesh& mesh)
+{
+}
+
+void GLTFLoader::_loadAllVertexDataAsync(
+  const string_t& context, const IGLTFMesh& mesh,
+  const ::std::function<void()>& onSuccess)
+{
+}
+
+Float32Array
+GLTFLoader::_convertToFloat4TextureCoordArray(const string_t& context,
+                                              const ArrayBufferView& data,
+                                              const IGLTFAccessor& accessor)
+{
+}
+
+Float32Array
+GLTFLoader::_convertToFloat4ColorArray(const string_t& context,
+                                       const ArrayBufferView& data,
+                                       const IGLTFAccessor& accessor)
+{
+}
+
+void GLTFLoader::_loadVertexDataAsync(
+  const string_t& context, const IGLTFMesh& mesh,
+  const IGLTFMeshPrimitive& primitive,
+  const ::std::function<void(const VertexData& vertexData)>& onSuccess)
+{
+}
+
+void GLTFLoader::_createMorphTargets(const string_t& context,
+                                     const IGLTFNode& node,
+                                     const IGLTFMesh& mesh)
+{
+}
+
+void GLTFLoader::_loadMorphTargets(const string_t& context,
+                                   const IGLTFNode& node, const IGLTFMesh& mesh)
+{
+}
+
+void GLTFLoader::_loadAllMorphTargetVertexDataAsync(
+  const string_t& context, const IGLTFNode& node, const IGLTFMesh& mesh,
+  const ::std::function<void()>& onSuccess)
+{
+}
+
+void GLTFLoader::_loadMorphTargetVertexDataAsync(
+  const string_t& context, const VertexData& vertexData,
+  const unordered_map_t<string_t, int>& attributes,
+  const ::std::function<void(const VertexData& vertexData)>& onSuccess)
+{
+}
+
+void GLTFLoader::_loadTransform(IGLTFNode* node)
+{
+}
+
+Skeleton* GLTFLoader::_loadSkin(const string_t& context, const IGLTFSkin& skin)
+{
+}
+
+Bone* GLTFLoader::_createBone(IGLTFNode* node, const IGLTFSkin& skin,
+                              Bone* parent, const Matrix& localMatrix,
+                              const Matrix& baseMatrix, unsigned int index)
+{
+}
+
+void GLTFLoader::_loadBone(const string_t& context, const IGLTFSkin& skin,
+                           const Float32Array& inverseBindMatrixData)
+{
+}
+
+Bone* GLTFLoader::_loadBone(IGLTFNode* node, const IGLTFSkin& skin,
+                            const Float32Array& inverseBindMatrixData,
+                            const unordered_map_t<int, Bone*>& babylonBones)
+{
+}
+
+Matrix GLTFLoader::_getNodeMatrix(const IGLTFNode& node)
+{
+}
+
+void GLTFLoader::_traverseNodes(
+  const string_t& context, const Uint32Array& indices,
+  ::std::function<bool(IGLTFNode* node, IGLTFNode* parentNode)>& action,
+  IGLTFNode* parentNode)
+{
+}
+
+void GLTFLoader::_traverseNode(
+  const string_t& context, IGLTFNode* node,
+  ::std::function<bool(IGLTFNode* node, IGLTFNode* parentNode)>& action)
+{
+}
+
+void GLTFLoader::_loadAnimations()
+{
+}
+
+void GLTFLoader::_loadAnimation(const string_t& context,
+                                const IGLTFAnimation& animation)
+{
+}
+
+void GLTFLoader::_loadAnimationChannel(const IGLTFAnimation& animation,
+                                       const string_t& channelContext,
+                                       const IGLTFAnimationChannel& channel,
+                                       const string_t& samplerContext,
+                                       const IGLTFAnimationSampler& sampler)
+{
+}
+
+void GLTFLoader::_loadBufferAsync(
+  const string_t& context, const IGLTFBuffer& buffer,
+  const ::std::function<void(ArrayBuffer& data)>& onSucces)
+{
+}
+
+void GLTFLoader::_loadBufferViewAsync(
+  const string_t& context, const IGLTFBufferView& bufferView,
+  const ::std::function<void(ArrayBuffer& data)>& onSucces)
+{
+}
+
+void GLTFLoader::_loadAccessorAsync(
+  const string_t& context, const IGLTFAccessor& accessor,
+  const ::std::function<void(ArrayBuffer& data)>& onSuccess)
+{
+}
+
+void GLTFLoader::_whenAction(const ::std::function<void()>& action,
+                             const ::std::function<void()>& onComplete)
+{
+}
+
+Material* GLTFLoader::_getDefaultMaterial()
+{
+}
+
+void GLTFLoader::_loadMaterialMetallicRoughnessProperties(
+  const string_t& context, const IGLTFMaterial& material)
+{
+}
+
+void GLTFLoader::_loadMaterial(
+  const string_t& context, IGLTFMaterial& material,
+  const ::std::function<void(Material* babylonMaterial, bool isNew)>& assign)
+{
+}
+
+void GLTFLoader::_createPbrMaterial(IGLTFMaterial& material)
+{
+}
+
+void GLTFLoader::_loadMaterialBaseProperties(const string_t& context,
+                                             const IGLTFMaterial& material)
+{
+}
+
+void GLTFLoader::_loadMaterialAlphaProperties(const string_t& context,
+                                              const IGLTFMaterial& material,
+                                              const Float32Array& colorFactor)
+{
+}
+
+Texture* GLTFLoader::_loadTexture(const string_t& context,
+                                  const IGLTFTexture& texture,
+                                  int coordinatesIndex)
+{
+}
+
+void GLTFLoader::_loadImage(
+  const string_t& context, const IGLTFImage& image,
+  const ::std::function<void(ArrayBuffer& data)>& onSuccess)
+{
+}
+
+void GLTFLoader::_loadUri(
+  const string_t& context, const string_t& uri,
+  const ::std::function<void(ArrayBuffer& data)>& onSuccess)
+{
+}
+
+void GLTFLoader::_tryCatchOnError(const ::std::function<void()>& handler)
 {
 }
 
