dac67933a40908509f80587d7392a92a1a000086
samdauwe <sam.dauwe@gmail.com>
2018-05-08 21:47:42 +0200

    Preparing for BabylonJs 3.2.0 release version

--- codegeneration/compare_files.py
+++ codegeneration/compare_files.py
@@ -61,13 +61,15 @@ def main():
     current, previous = "3.2.0-alpha7", "3.1-beta-6"
     current, previous = "3.2.0-beta.2", "3.2.0-alpha7"
     current, previous = "3.2.0-beta.5", "3.2.0-beta.2"
+    #current, previous = "3.2.0", "3.2.0-beta.5"
     # Dictionary mapping from BabylonJs version to relative path
     BabylonJsVersions = {
-        "3.1-alpha" : "3.1.0_2017_09_23",
-        "3.1-beta-6" : "3.1.0_2017_12_01",
+        "3.1-alpha"    : "3.1.0_2017_09_23",
+        "3.1-beta-6"   : "3.1.0_2017_12_01",
         "3.2.0-alpha7" : "3.2.0_2018_02_03",
         "3.2.0-beta.2" : "3.2.0_2018_03_22",
-        "3.2.0-beta.5" : "3.2.0_2018_04_14"
+        "3.2.0-beta.5" : "3.2.0_2018_04_14",
+        "3.2.0"        : "3.2.0_2018_05_01"
     }
     # List containing the files to ignore
     whiteList = ["babylon.assetContainer.ts", "babylon.nullEngine.ts",
