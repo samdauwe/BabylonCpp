c9f630c2b0fefe74280c0cb9a5f80bd0739f03f7
samdauwe <sam.dauwe@gmail.com>
2018-09-24 21:02:39 +0200

    Updated sample index for use without aliases

--- src/Samples/include/babylon/samples/samples_index.h
+++ src/Samples/include/babylon/samples/samples_index.h
@@ -1,7 +1,7 @@
 #ifndef BABYLON_SAMPLES_SAMPLES_INDEX_H
 #define BABYLON_SAMPLES_SAMPLES_INDEX_H
 
-#include <babylon/babylon_global.h>
+#include <babylon/babylon_api.h>
 #include <babylon/samples/samples_common.h>
 
 namespace BABYLON {
@@ -18,33 +18,33 @@ public:
    * @param sampleName the name of the sample to check
    * @return whether or not the example is enabled
    */
-  bool isSampleEnabled(const string_t& sampleName);
+  bool isSampleEnabled(const std::string& sampleName);
 
   /**
    * @brief Returns whether or not the example exists.
    * @param sampleName the name of the sample to check
    * @return whether or not the example exists
    */
-  bool sampleExists(const string_t& sampleName);
+  bool sampleExists(const std::string& sampleName);
 
   /**
    * @brief Returns a sorted list containing the enabled examples.
    * @return a sorted list containing the enabled examples.
    */
-  ::std::vector<string_t> getSampleNames() const;
+  ::std::vector<std::string> getSampleNames() const;
 
   /**
    * @brief Returns a sorted list containing the category names.
    * @return a sorted list containing the category names.
    */
-  ::std::vector<string_t> getCategoryNames() const;
+  ::std::vector<std::string> getCategoryNames() const;
 
   /**
    * @brief Returns whether or not the category exists.
    * @param categoryNameToSearch the name of the category to check
    * @return whether or not the category exists
    */
-  bool categoryExists(const string_t& categoryNameToSearch);
+  bool categoryExists(const std::string& categoryNameToSearch);
 
   /**
    * @brief Returns a sorted list containing the enabled examples in the
@@ -52,8 +52,8 @@ public:
    * @return a sorted list containing the enabled examples in the specified
    * category.
    */
-  ::std::vector<string_t>
-  getSampleNamesInCategory(const string_t& categoryName) const;
+  ::std::vector<std::string>
+  getSampleNamesInCategory(const std::string& categoryName) const;
 
   /**
    * @brief Sample factory method
@@ -62,11 +62,11 @@ public:
    * @return the renderable scene
    */
   std::unique_ptr<IRenderableScene>
-  createRenderableScene(const string_t& sampleName, ICanvas* iCanvas) const;
+  createRenderableScene(const std::string& sampleName, ICanvas* iCanvas) const;
 
 private:
   // Contains the mapping from category to samples index
-  ::std::unordered_map<string_t, _ISamplesIndex> _samplesIndex;
+  ::std::unordered_map<std::string, _ISamplesIndex> _samplesIndex;
 
 }; // end of class SamplesIndex
 
