18f031ef7c032f4b4a31eee3f3856782e2c0e9d5
samdauwe <sam.dauwe@gmail.com>
2018-11-11 18:21:12 +0100

    Enabled Box Instances Scene

--- src/Samples/src/samples/optimizations/_optimizations_samples_index.cpp
+++ src/Samples/src/samples/optimizations/_optimizations_samples_index.cpp
@@ -10,11 +10,11 @@ _OptimizationsSamplesIndex::_OptimizationsSamplesIndex()
   // -- Optimizations examples --
 
   // Box Instances Scene
-  _samples["BoxInstancesScene"] = std::make_tuple(
-    false,                                                   //
-    [](ICanvas* iCanvas) {                                   //
-      return std::make_unique<BoxInstancesScene>(iCanvas); //
-    });                                                      //
+  _samples["BoxInstancesScene"]
+    = std::make_tuple(true,                                                  //
+                      [](ICanvas* iCanvas) {                                 //
+                        return std::make_unique<BoxInstancesScene>(iCanvas); //
+                      });                                                    //
 }
 
 _OptimizationsSamplesIndex::~_OptimizationsSamplesIndex()
