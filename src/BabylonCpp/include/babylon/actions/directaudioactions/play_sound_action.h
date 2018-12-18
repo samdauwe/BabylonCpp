59634d3aeea5f520d800cc3f83337a4ae6ee227f
samdauwe <sam.dauwe@gmail.com>
2018-12-18 21:40:29 +0100

    Added direct audio actions

---
+++ src/BabylonCpp/include/babylon/actions/directaudioactions/play_sound_action.h
@@ -0,0 +1,52 @@
+#ifndef BABYLON_ACTIONS_DIRECT_AUDIO_ACTIONS_PLAY_SOUND_ACTION_H
+#define BABYLON_ACTIONS_DIRECT_AUDIO_ACTIONS_PLAY_SOUND_ACTION_H
+
+#include <babylon/actions/action.h>
+#include <babylon/babylon_api.h>
+
+namespace BABYLON {
+
+class Sound;
+
+/**
+ * @brief This defines an action helpful to play a defined sound on a triggered
+ * action.
+ */
+class BABYLON_SHARED_EXPORT PlaySoundAction : public Action {
+
+public:
+  /**
+   * @brief Instantiate the action.
+   * @param triggerOptions defines the trigger options
+   * @param sound defines the sound to play
+   * @param condition defines the trigger related conditions
+   */
+  PlaySoundAction(unsigned int triggerOptions, Sound* sound,
+                  Condition* condition = nullptr);
+  ~PlaySoundAction() override;
+
+  /**
+   * @brief Hidden
+   */
+  void _prepare() override;
+
+  /**
+   * @brief Execute the action and play the sound.
+   */
+  void execute(const ActionEvent& evt) override;
+
+  /**
+   * @brief Serializes the actions and its related information.
+   * @param parent defines the object to serialize in
+   * @returns the serialized object
+   */
+  json serialize(json& parent) const override;
+
+private:
+  Sound* _sound;
+
+}; // end of class PlaySoundAction
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_ACTIONS_DIRECT_AUDIO_ACTIONS_PLAY_SOUND_ACTION_H
---
+++ src/BabylonCpp/include/babylon/actions/directaudioactions/stop_sound_action.h
@@ -0,0 +1,52 @@
+#ifndef BABYLON_ACTIONS_DIRECT_AUDIO_ACTIONS_STOP_SOUND_ACTION_H
+#define BABYLON_ACTIONS_DIRECT_AUDIO_ACTIONS_STOP_SOUND_ACTION_H
+
+#include <babylon/actions/action.h>
+#include <babylon/babylon_api.h>
+
+namespace BABYLON {
+
+class Sound;
+
+/**
+ * @brief This defines an action helpful to stop a defined sound on a triggered
+ * action.
+ */
+class BABYLON_SHARED_EXPORT StopSoundAction : public Action {
+
+public:
+  /**
+   * @brief Instantiate the action.
+   * @param triggerOptions defines the trigger options
+   * @param sound defines the sound to stop
+   * @param condition defines the trigger related conditions
+   */
+  StopSoundAction(unsigned int triggerOptions, Sound* sound,
+                  Condition* condition = nullptr);
+  ~StopSoundAction() override;
+
+  /**
+   * @brief Hidden
+   */
+  void _prepare() override;
+
+  /**
+   * @brief Execute the action and stop the sound.
+   */
+  void execute(const ActionEvent& evt) override;
+
+  /**
+   * @brief Serializes the actions and its related information.
+   * @param parent defines the object to serialize in
+   * @returns the serialized object
+   */
+  json serialize(json& parent) const override;
+
+private:
+  Sound* _sound;
+
+}; // end of class StopSoundAction
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_ACTIONS_DIRECT_AUDIO_ACTIONS_STOP_SOUND_ACTION_H
