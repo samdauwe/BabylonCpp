f4b2aa4c4431f1dbea8dd54802b2ec6cfdcdfa53
samdauwe <sam.dauwe@gmail.com>
2018-12-31 11:48:24 +0100

    Added Import BabylonJS Logo Scene

--- src/Samples/src/samples/loaders/_loaders_samples_index.cpp
+++ src/Samples/src/samples/loaders/_loaders_samples_index.cpp
@@ -1,5 +1,6 @@
 #include <babylon/samples/loaders/_loaders_samples_index.h>
 
+#include <babylon/samples/loaders/import_babylon_js_logo_scene.h>
 #include <babylon/samples/loaders/import_meshes_skull_scene.h>
 
 namespace BABYLON {
@@ -9,12 +10,18 @@ _LoadersSamplesIndex::_LoadersSamplesIndex()
 {
   // -- Loaders examples --
 
-  // Import Meshes Skull Scene
-  _samples["ImportMeshesSkullScene"] = std::make_tuple(
+  // Import Babylon JS Logo Scene
+  _samples["ImportBabylonJSLogoScene"] = std::make_tuple(
     true,                                                         //
     [](ICanvas* iCanvas) {                                        //
-      return std::make_unique<ImportMeshesSkullScene>(iCanvas); //
+      return std::make_unique<ImportBabylonJSLogoScene>(iCanvas); //
     });                                                           //
+  // Import Meshes Skull Scene
+  _samples["ImportMeshesSkullScene"] = std::make_tuple(
+    true,                                                       //
+    [](ICanvas* iCanvas) {                                      //
+      return std::make_unique<ImportMeshesSkullScene>(iCanvas); //
+    });                                                         //
 }
 
 _LoadersSamplesIndex::~_LoadersSamplesIndex()
