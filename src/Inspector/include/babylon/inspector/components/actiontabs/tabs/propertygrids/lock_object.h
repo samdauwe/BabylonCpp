83cd6074a016c2f545321c9e672374fb7b1ab306
samdauwe <sam.dauwe@gmail.com>
2019-02-14 21:30:52 +0100

    Added LockObject class

---
+++ src/Inspector/include/babylon/inspector/components/actiontabs/tabs/propertygrids/lock_object.h
@@ -0,0 +1,20 @@
+#ifndef BABYLON_INSPECTOR_COMPONENTS_ACTION_TABS_TABS_PROPERTY_GRIDS_LOCK_OBJECT_H
+#define BABYLON_INSPECTOR_COMPONENTS_ACTION_TABS_TABS_PROPERTY_GRIDS_LOCK_OBJECT_H
+
+#include <babylon/babylon_api.h>
+
+namespace BABYLON {
+
+struct BABYLON_SHARED_EXPORT LockObject {
+
+  /**
+   * Gets or set if the lock is engaged
+   */
+  bool lock = false;
+
+}; // end of struct LockObject
+
+} // end of namespace BABYLON
+
+#endif // end of
+       // BABYLON_INSPECTOR_COMPONENTS_ACTION_TABS_TABS_PROPERTY_GRIDS_LOCK_OBJECT_H
