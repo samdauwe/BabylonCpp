c40beb6aa58ff267111d93315a78c20ddcc4ce99
Pascal Thomet <pthomet@gmail.com>
2019-08-07 23:00:55 +0200

    argtable3.c : disable MSVC warnings

--- src/SampleLauncher/src/argtable3/argtable3.c
+++ src/SampleLauncher/src/argtable3/argtable3.c
@@ -27,6 +27,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
+#ifdef _MSC_VER
+#pragma warning(disable:4244)
+#pragma warning(disable:4267)
+#pragma warning(disable:4706)
+#pragma warning(disable:4702)
+#endif
 
 #include <argtable3/argtable3.h>
 
