a6c8b87aed4f0180abbd4f3ce0349abebcfbd447
Pascal Thomet <pthomet@gmail.com>
2019-08-27 17:49:07 +0200

    rtc : suppress warnings for clang

--- src/SamplesRunner/rtc/sandbox_autocompile.h
+++ src/SamplesRunner/rtc/sandbox_autocompile.h
@@ -4,9 +4,7 @@
 #include <SamplesRunner/rtc/iscene_producer.h>
 #include <SamplesRunner/rtc/interface_ids.h>
 
-using namespace BABYLON::rtc;
-
-class Sandbox : public TInterface<IID_RENDERABLESCENE_PRODUCER, ISceneProducer>
+class Sandbox : public TInterface<BABYLON::rtc::IID_RENDERABLESCENE_PRODUCER, BABYLON::rtc::ISceneProducer>
 {
 public:
   std::shared_ptr<BABYLON::IRenderableScene> MakeScene() override;
--- src/SamplesRunner/rtc/suppress_warnings.h
+++ src/SamplesRunner/rtc/suppress_warnings.h
@@ -1,4 +1,10 @@
+// The library RuntimeCompiledCplusplus has some warnings: hide them
 #ifdef __clang__
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
 #elif __GNUC__
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
