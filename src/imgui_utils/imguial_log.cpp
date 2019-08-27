1d2822dc47914dce96df6d6acea100517cddeeeb
Pascal Thomet <pthomet@gmail.com>
2019-08-27 19:04:04 +0200

    imguial_log : remove warnings

--- src/imgui_utils/imguial_log.cpp
+++ src/imgui_utils/imguial_log.cpp
@@ -39,23 +39,23 @@ bool ImGuiAl::Log::Init( unsigned flags, const char** more_actions )
   // Do compile-time checks for the line and buffer sizes.
   char check_line_size[ IMGUIAL_LOG_MAX_LINE_SIZE <= 65535 ? 1 : -1 ];
   char check_buffer_size[ IMGUIAL_LOG_MAX_BUFFER_SIZE >= IMGUIAL_LOG_MAX_LINE_SIZE + 3 ? 1 : -1 ];
-  
+
   (void)check_line_size;
   (void)check_buffer_size;
 
   m_Avail = IMGUIAL_LOG_MAX_BUFFER_SIZE;
   m_First = m_Last = 0;
-  
+
   m_Flags = flags;
   m_MoreActions = more_actions;
   m_Level = kDebug;
   m_Cumulative = true;
-  
+
   SetColor( kDebug, 0.0f, 0.6f, 1.0f );
   SetColor( kInfo,  0.0f, 1.0f, 0.4f );
   SetColor( kWarn,  0.8f, 0.8f, 0.0f );
   SetColor( kError, 1.0f, 0.3f, 0.3f );
-  
+
   m_Labels[ kDebug ]  = "Debug";
   m_Labels[ kInfo ]   = "Info";
   m_Labels[ kWarn ]   = "Warn";
@@ -63,7 +63,7 @@ bool ImGuiAl::Log::Init( unsigned flags, const char** more_actions )
   m_CumulativeLabel   = "Cumulative";
   m_FilterHeaderLabel = NULL;
   m_FilterLabel       = "Filter (inc,-exc)";
-  
+
   return true;
 }
 
@@ -71,9 +71,9 @@ void ImGuiAl::Log::SetColor( Level level, float r, float g, float b )
 {
   float h, s, v;
   ImGui::ColorConvertRGBtoHSV( r, g, b, h, s, v );
-  
+
   v -= 0.3f;
-  
+
   m_Colors[ level ][ 0 ] = ImColor( r, g, b );
   m_Colors[ level ][ 1 ] = ImColor::HSV( h, s, v );
   m_Colors[ level ][ 2 ] = ImColor::HSV( h, s, v + 0.1f );
@@ -104,15 +104,15 @@ void ImGuiAl::Log::VPrintf( Level level, const char* format, va_list args )
 {
   char line[ IMGUIAL_LOG_MAX_LINE_SIZE ];
   size_t length = vsnprintf( line, sizeof( line ), format, args );
-  
+
   if ( length > sizeof( line ) )
   {
     // Line size too small, truncated. Consider increasing the array size.
     length = sizeof( line );
   }
-  
+
   length += 3; // Add one byte for the level and two bytes for the length.
-  
+
   if ( length > m_Avail )
   {
     do
@@ -124,17 +124,17 @@ void ImGuiAl::Log::VPrintf( Level level, const char* format, va_list args )
     }
     while ( length > m_Avail );
   }
-  
+
   unsigned char meta[ 3 ];
   length -= 3;
-  
+
   meta[ 0 ] = static_cast<char>(level);
   meta[ 1 ] = length & 0xff;
   meta[ 2 ] = (unsigned char) (length >> 8);
-  
+
   Write( meta, 3 );
   Write( line, length );
-  
+
   m_ScrollToBottom = true;
 }
 
@@ -185,20 +185,20 @@ void ImGuiAl::Log::Message(const char * msg, Level level)
 void ImGuiAl::Log::Iterate( IterateFunc iterator, bool apply_filters, void* ud )
 {
   size_t pos = m_First;
-  
+
   while ( pos != m_Last )
   {
     char line[ IMGUIAL_LOG_MAX_LINE_SIZE + 1 ];
     unsigned char meta[ 3 ];
     pos = Peek( pos, meta, 3 );
-    
+
     Level level = (Level)meta[ 0 ];
     size_t length = meta[ 1 ] | meta[ 2 ] << 8;
     pos = Peek( pos, line, length );
     line[ length ] = 0;
-    
+
     bool show;
-    
+
     if ( apply_filters )
     {
       show = ( m_Cumulative && level >= m_Level ) || level == m_Level;
@@ -208,7 +208,7 @@ void ImGuiAl::Log::Iterate( IterateFunc iterator, bool apply_filters, void* ud )
     {
       show = true;
     }
-    
+
     if ( show )
     {
       if ( !iterator( level, line, ud ) )
@@ -222,7 +222,7 @@ void ImGuiAl::Log::Iterate( IterateFunc iterator, bool apply_filters, void* ud )
 int ImGuiAl::Log::Draw()
 {
   int action = 0;
-  
+
   if (m_MoreActions != nullptr)
   {
     for (int i = 0; m_MoreActions[i] != NULL; i++)
@@ -259,9 +259,9 @@ int ImGuiAl::Log::Draw()
       m_Level = kDebug;
       m_ScrollToBottom = true;
     }
-    
+
     ImGui::SameLine();
-    
+
     ImGui::PushStyleColor( ImGuiCol_Button, m_Colors[ kInfo ][ 1 ].Value );
     ImGui::PushStyleColor( ImGuiCol_ButtonHovered, m_Colors[ kInfo ][ 2 ].Value );
     ImGui::PushStyleColor( ImGuiCol_ButtonActive, m_Colors[ kInfo ][ 3 ].Value );
@@ -273,9 +273,9 @@ int ImGuiAl::Log::Draw()
       m_Level = kInfo;
       m_ScrollToBottom = true;
     }
-    
+
     ImGui::SameLine();
-    
+
     ImGui::PushStyleColor( ImGuiCol_Button, m_Colors[ kWarn ][ 1 ].Value );
     ImGui::PushStyleColor( ImGuiCol_ButtonHovered, m_Colors[ kWarn ][ 2 ].Value );
     ImGui::PushStyleColor( ImGuiCol_ButtonActive, m_Colors[ kWarn ][ 3 ].Value );
@@ -287,9 +287,9 @@ int ImGuiAl::Log::Draw()
       m_Level = kWarn;
       m_ScrollToBottom = true;
     }
-    
+
     ImGui::SameLine();
-    
+
     ImGui::PushStyleColor( ImGuiCol_Button, m_Colors[ kError ][ 1 ].Value );
     ImGui::PushStyleColor( ImGuiCol_ButtonHovered, m_Colors[ kError ][ 2 ].Value );
     ImGui::PushStyleColor( ImGuiCol_ButtonActive, m_Colors[ kError ][ 3 ].Value );
@@ -301,45 +301,45 @@ int ImGuiAl::Log::Draw()
       m_Level = kError;
       m_ScrollToBottom = true;
     }
-    
+
     ImGui::SameLine();
     ImGui::Checkbox( m_CumulativeLabel, &m_Cumulative );
     m_Filter.Draw( m_FilterLabel );
   }
-  
+
   // Do I need an unique id in BeginChild?
   char id[ 64 ];
-  snprintf( id, sizeof( id ), "console_%p", this );
+  snprintf( id, sizeof( id ), "console_%p", (void *)this );
   id[ sizeof( id ) - 1 ] = 0;
-  
+
   ImGui::BeginChild( id, ImVec2( 0, -ImGui::GetItemsLineHeightWithSpacing() ), false, ImGuiWindowFlags_HorizontalScrollbar );
   ImGui::PushStyleVar( ImGuiStyleVar_ItemSpacing, ImVec2( 4, 1 ) );
-  
+
   struct Iterator
   {
     static bool Func( Level level, const char* line, void *ud )
     {
       Log* self = (Log*)ud;
-      
+
       ImGui::PushStyleColor( ImGuiCol_Text, self->m_Colors[ level ][ 0 ].Value );
       ImGui::TextUnformatted( line );
       ImGui::PopStyleColor();
-      
+
       return true;
     }
   };
-  
+
   Iterate( Iterator::Func, true, this );
-  
+
   if ( m_ScrollToBottom )
   {
     ImGui::SetScrollHere();
     m_ScrollToBottom = false;
   }
-  
+
   ImGui::PopStyleVar();
   ImGui::EndChild();
-  
+
   return action;
 }
 
@@ -347,17 +347,17 @@ void ImGuiAl::Log::Write( const void* data, size_t size )
 {
   size_t first = size;
   size_t second = 0;
-  
+
   if ( first > IMGUIAL_LOG_MAX_BUFFER_SIZE - m_Last )
   {
     first = IMGUIAL_LOG_MAX_BUFFER_SIZE - m_Last;
     second = size - first;
   }
-  
+
   char* dest = m_Buffer + m_Last;
   memcpy( dest, data, first );
   memcpy( m_Buffer, (char*)data + first, second );
-  
+
   m_Last = ( m_Last + size ) % IMGUIAL_LOG_MAX_BUFFER_SIZE;
   m_Avail -= size;
 }
@@ -366,17 +366,17 @@ void ImGuiAl::Log::Read( void* data, size_t size )
 {
   size_t first = size;
   size_t second = 0;
-  
+
   if ( first > IMGUIAL_LOG_MAX_BUFFER_SIZE - m_First )
   {
     first = IMGUIAL_LOG_MAX_BUFFER_SIZE - m_First;
     second = size - first;
   }
-  
+
   char* src = m_Buffer + m_First;
   memcpy( data, src, first );
   memcpy( (char*)data + first, m_Buffer, second );
-  
+
   m_First = ( m_First + size ) % IMGUIAL_LOG_MAX_BUFFER_SIZE;
   m_Avail += size;
 }
@@ -385,17 +385,17 @@ size_t ImGuiAl::Log::Peek( size_t pos, void* data, size_t size )
 {
   size_t first = size;
   size_t second = 0;
-  
+
   if ( first > IMGUIAL_LOG_MAX_BUFFER_SIZE - pos )
   {
     first = IMGUIAL_LOG_MAX_BUFFER_SIZE - pos;
     second = size - first;
   }
-  
+
   char* src = m_Buffer + pos;
   memcpy( data, src, first );
   memcpy( (char*)data + first, m_Buffer, second );
-  
+
   return ( pos + size ) % IMGUIAL_LOG_MAX_BUFFER_SIZE;
 }
 
@@ -422,6 +422,6 @@ void ImGuiAl::Log::CopyToClipboard()
     ss << s << "\n";
 
   ImGui::LogToClipboard();
-  ImGui::LogText(ss.str().c_str());
+  ImGui::LogText("%s", ss.str().c_str());
   ImGui::LogFinish();
 }
