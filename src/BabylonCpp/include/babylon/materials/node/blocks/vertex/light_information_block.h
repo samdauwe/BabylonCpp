2161c9979f24488bc7afe49a66181ce83b97d066
samdauwe <sam.dauwe@gmail.com>
2019-09-22 18:36:00 +0200

    Added LightInformationBlock class

---
+++ src/BabylonCpp/include/babylon/materials/node/blocks/vertex/light_information_block.h
@@ -0,0 +1,114 @@
+#ifndef BABYLON_MATERIALS_NODE_BLOCKS_VERTEX_LIGHT_INFORMATION_BLOCK_H
+#define BABYLON_MATERIALS_NODE_BLOCKS_VERTEX_LIGHT_INFORMATION_BLOCK_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/materials/node/node_material_block.h>
+
+namespace BABYLON {
+
+class Light;
+class LightInformationBlock;
+using LightPtr                 = std::shared_ptr<Light>;
+using LightInformationBlockPtr = std::shared_ptr<LightInformationBlock>;
+
+/**
+ * @brief Block used to get data information from a light.
+ */
+class BABYLON_SHARED_EXPORT LightInformationBlock : public NodeMaterialBlock {
+
+public:
+  template <typename... Ts>
+  static LightInformationBlockPtr New(Ts&&... args)
+  {
+    return std::shared_ptr<LightInformationBlock>(
+      new LightInformationBlock(std::forward<Ts>(args)...));
+  }
+  ~LightInformationBlock() override;
+
+  /**
+   * @brief Gets the current class name.
+   * @returns the class name
+   */
+  const std::string getClassName() const override;
+
+  /**
+   * @brief Bind data to effect. Will only be called for blocks with isBindable
+   * === true.
+   * @param effect defines the effect to bind data to
+   * @param nodeMaterial defines the hosting NodeMaterial
+   * @param mesh defines the mesh that will be rendered
+   */
+  void bind(Effect* effect, const NodeMaterialPtr& nodeMaterial,
+            Mesh* mesh = nullptr) override;
+
+  /**
+   * @brief Serializes this block in a JSON representation.
+   * @returns the serialized block object
+   */
+  json serialize() const override;
+
+  /**
+   * @brief Hidden
+   */
+  void _deserialize(const json& serializationObject, Scene* scene,
+                    const std::string& rootUrl) override;
+
+protected:
+  /**
+   * @brief Creates a new LightInformationBlock.
+   * @param name defines the block name
+   */
+  LightInformationBlock(const std::string& iName);
+
+  /**
+   * @brief Gets the world position input component.
+   */
+  NodeMaterialConnectionPointPtr& get_worldPosition();
+
+  /**
+   * @brief Gets the direction output component.
+   */
+  NodeMaterialConnectionPointPtr& get_direction();
+
+  /**
+   * @brief Gets the color output component.
+   */
+  NodeMaterialConnectionPointPtr& get_color();
+
+  /**
+   * @brief Hidden
+   */
+  LightInformationBlock& _buildBlock(NodeMaterialBuildState& state) override;
+
+public:
+  /**
+   * Gets or sets the light associated with this block
+   */
+  LightPtr light;
+
+  /**
+   * Gets the world position input component
+   */
+  ReadOnlyProperty<LightInformationBlock, NodeMaterialConnectionPointPtr>
+    worldPosition;
+
+  /**
+   * Gets the direction output component
+   */
+  ReadOnlyProperty<LightInformationBlock, NodeMaterialConnectionPointPtr>
+    direction;
+
+  /**
+   * Gets the color output component
+   */
+  ReadOnlyProperty<LightInformationBlock, NodeMaterialConnectionPointPtr> color;
+
+private:
+  std::string _lightDataDefineName;
+  std::string _lightColorDefineName;
+
+}; // end of class LightInformationBlock
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_NODE_BLOCKS_VERTEX_LIGHT_INFORMATION_BLOCK_H
---
+++ src/BabylonCpp/src/materials/node/blocks/vertex/light_information_block.cpp
@@ -0,0 +1,123 @@
+﻿#include <babylon/materials/node/blocks/vertex/light_information_block.h>
+
+#include <babylon/core/json_util.h>
+#include <babylon/core/string.h>
+#include <babylon/lights/light.h>
+#include <babylon/materials/effect.h>
+#include <babylon/materials/node/node_material_build_state.h>
+#include <babylon/materials/node/node_material_build_state_shared_data.h>
+#include <babylon/materials/node/node_material_connection_point.h>
+
+namespace BABYLON {
+
+LightInformationBlock::LightInformationBlock(const std::string& iName)
+    : NodeMaterialBlock(iName, NodeMaterialBlockTargets::Vertex)
+    , worldPosition{this, &LightInformationBlock::get_worldPosition}
+    , direction{this, &LightInformationBlock::get_direction}
+    , color{this, &LightInformationBlock::get_color}
+{
+  registerInput("worldPosition", NodeMaterialBlockConnectionPointTypes::Vector4,
+                false, NodeMaterialBlockTargets::Vertex);
+  registerOutput("direction", NodeMaterialBlockConnectionPointTypes::Vector3);
+  registerOutput("color", NodeMaterialBlockConnectionPointTypes::Color3);
+}
+
+LightInformationBlock::~LightInformationBlock()
+{
+}
+
+const std::string LightInformationBlock::getClassName() const
+{
+  return "LightInformationBlock";
+}
+
+NodeMaterialConnectionPointPtr& LightInformationBlock::get_worldPosition()
+{
+  return _inputs[0];
+}
+
+NodeMaterialConnectionPointPtr& LightInformationBlock::get_direction()
+{
+  return _outputs[0];
+}
+
+NodeMaterialConnectionPointPtr& LightInformationBlock::get_color()
+{
+  return _outputs[1];
+}
+
+void LightInformationBlock::bind(Effect* effect,
+                                 const NodeMaterialPtr& /*nodeMaterial*/,
+                                 Mesh* mesh)
+{
+  if (!mesh) {
+    return;
+  }
+
+  if (!light || !light->isEnabled()) {
+    effect->setFloat3(_lightDataDefineName, 0.f, 0.f, 0.f);
+    effect->setFloat3(_lightColorDefineName, 0.f, 0.f, 0.f);
+    return;
+  }
+
+  light->transferToNodeMaterialEffect(effect, _lightDataDefineName);
+
+  effect->setColor3(_lightColorDefineName, light->diffuse);
+}
+
+LightInformationBlock&
+LightInformationBlock::_buildBlock(NodeMaterialBuildState& state)
+{
+  NodeMaterialBlock::_buildBlock(state);
+
+  state.sharedData->bindableBlocks.emplace_back(this);
+
+  const auto& _direction = direction();
+  const auto& _color     = color();
+
+  if (!light) {
+    state.compilationString
+      += _declareOutput(_direction, state) + " = vec3(0.);\r\n";
+    state.compilationString
+      += _declareOutput(_color, state) + " = vec3(0.);\r\n";
+  }
+  else {
+    _lightDataDefineName = state._getFreeDefineName("lightData");
+    state._emitUniformFromString(_lightDataDefineName, "vec3");
+
+    _lightColorDefineName = state._getFreeDefineName("lightColor");
+    state._emitUniformFromString(_lightColorDefineName, "vec3");
+
+    if (light->type() == Type::POINTLIGHT) {
+      state.compilationString
+        += _declareOutput(_direction, state)
+           + String::printf(" = normalize(%s - %s.xyz);\r\n)",
+                            _lightDataDefineName.c_str(),
+                            worldPosition()->associatedVariableName().c_str());
+    }
+    else {
+      state.compilationString
+        += _declareOutput(_direction, state)
+           + String::printf(" = %s;\r\n", _lightDataDefineName.c_str());
+    }
+
+    state.compilationString
+      += _declareOutput(_color, state)
+         + String::printf(" = %s;\r\n", _lightDataDefineName.c_str());
+  }
+
+  return *this;
+}
+
+json LightInformationBlock::serialize() const
+{
+  return nullptr;
+}
+
+void LightInformationBlock::_deserialize(const json& /*serializationObject*/,
+                                         Scene* /*scene*/,
+                                         const std::string& /*rootUrl*/)
+{
+}
+
+} // end of namespace BABYLON
