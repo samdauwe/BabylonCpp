337c9fc34c3a90dbb48ec8fc398aeeb95d49a08a
samdauwe <sam.dauwe@gmail.com>
2019-10-03 21:29:51 +0200

    Added IRenderableSceneWithHud class in the core library

---
+++ src/BabylonCpp/include/babylon/interfaces/irenderable_scene_with_hud.h
@@ -0,0 +1,21 @@
+#ifndef BABYLON_INTERFACES_IRENDERABLE_SCENE_WITH_HUD_H
+#define BABYLON_INTERFACES_IRENDERABLE_SCENE_WITH_HUD_H
+
+#include <functional>
+
+#include <babylon/interfaces/irenderable_scene.h>
+
+namespace BABYLON {
+
+class BABYLON_SHARED_EXPORT IRenderableSceneWithHud : public IRenderableScene {
+
+public:
+  IRenderableSceneWithHud(ICanvas* iCanvas = nullptr);
+  virtual ~IRenderableSceneWithHud() = default;
+
+  std::function<void(void)> hudGui = {};
+};
+
+} // end of namespace BABYLON
+
+#endif // BABYLON_INTERFACES_IRENDERABLE_SCENE_WITH_HUD_H
---
+++ src/BabylonCpp/src/interfaces/irenderable_scene_with_hud.cpp
@@ -0,0 +1,10 @@
+#include <babylon/interfaces/irenderable_scene_with_hud.h>
+
+namespace BABYLON {
+
+IRenderableSceneWithHud::IRenderableSceneWithHud(ICanvas* iCanvas /*= nullptr*/)
+    : IRenderableScene(iCanvas)
+{
+}
+
+} // end of namespace BABYLON
