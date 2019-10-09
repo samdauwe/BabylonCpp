ea3ef8b034003b8a80d21c3c1f2cc6a9ff0a9022
samdauwe <sam.dauwe@gmail.com>
2019-10-09 22:05:41 +0200

    Added missing constants implementation file

---
+++ src/BabylonCpp/src/engines/constants.cpp
@@ -0,0 +1,8 @@
+#include <babylon/engines/constants.h>
+
+namespace BABYLON {
+
+const char* Constants::PARTICLES_BaseAssetsUrl
+  = "https://assets.babylonjs.com/particles";
+
+} // end of namespace BABYLON
