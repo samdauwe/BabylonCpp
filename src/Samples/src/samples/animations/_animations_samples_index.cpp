2353a5ab050f0d39a2e156d342624438df3f27fc
Pascal Thomet <pthomet@gmail.com>
2019-11-07 23:00:12 +0100

    easing_functions_scene  & shape_easings_scene are headerless

--- src/Samples/src/samples/animations/_animations_samples_index.cpp
+++ src/Samples/src/samples/animations/_animations_samples_index.cpp
@@ -1,7 +1,5 @@
 #include <babylon/samples/animations/_animations_samples_index.h>
 
-#include <babylon/samples/animations/easing/easing_functions_scene.h>
-#include <babylon/samples/animations/easing/shape_easings_scene.h>
 
 namespace BABYLON {
 namespace Samples {
@@ -9,18 +7,6 @@ namespace Samples {
 
 _AnimationsSamplesIndex::_AnimationsSamplesIndex()
 {
-  // -- Animations examples --
-
-  // -- Easing function examples --
-
-  // Easing Functions Scene
-  _samples["EasingFunctionsScene"] = [](ICanvas* iCanvas) {
-    return std::make_unique<EasingFunctionsScene>(iCanvas);
-  };
-  // Shape Easings Scene
-  _samples["ShapeEasingsScene"] = [](ICanvas* iCanvas) {
-    return std::make_unique<ShapeEasingsScene>(iCanvas);
-  };
 }
 
 } // end of namespace Samples
