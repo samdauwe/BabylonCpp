28372d57a944af0d50dae61b89dd23ae5331ccbe
samdauwe <sam.dauwe@gmail.com>
2019-11-13 20:41:10 +0100

    Enabled mesh clone unit test

--- src/BabylonCpp/tests/mesh/mesh_test.cpp
+++ src/BabylonCpp/tests/mesh/mesh_test.cpp
@@ -3,37 +3,23 @@
 
 #include "../test_utils.h"
 
-#include <babylon/meshes/mesh.h>
 #include <babylon/engines/scene.h>
 #include <babylon/meshes/builders/mesh_builder_options.h>
+#include <babylon/meshes/mesh.h>
 #include <babylon/meshes/mesh_builder.h>
-
 
 TEST(Mesh, clone)
 {
   using namespace BABYLON;
   {
-    std::unique_ptr<BABYLON::Engine> engine = createSubject();
-    auto scene = BABYLON::Scene::New(engine.get());
+    auto engine = createSubject();
+    auto scene  = BABYLON::Scene::New(engine.get());
     SphereOptions sphereOptions;
     sphereOptions.diameter = 0.2f;
-    auto sphereLight = MeshBuilder::CreateSphere("sphere", sphereOptions, scene.get());
-
-
-    // This commented line will cause a segmentation fault (bad weak ptr)
-    // if uncommented!
-    //auto ss = sphereLight->clone("");
-
-    /*
-    will fail in
-    SubMeshPtr Mesh::_createGlobalSubMesh(bool force)
-      return SubMesh::New(0, 0, totalVertices, 0, getTotalIndices(),
-                    shared_from_base<Mesh>() <== fail here!
-                    );
-    */
-
-    //auto ss2 = SubMesh::New(0, 0, 0, 0, 0, sphereLight.shared_from_this<Mesh>());
-
+    auto sphere            = MeshBuilder::CreateSphere("sphere", sphereOptions, scene.get());
+    auto sphereClone       = sphere->clone("sphereClone");
+    EXPECT_EQ(sphere->name, "sphere");
+    EXPECT_EQ(sphereClone->name, "sphereClone");
+    EXPECT_NE(sphere->uniqueId, sphereClone->uniqueId);
   }
-
 }
