1a24b3f36527cf3feeb41476f64c44a4d6febd51
Pascal Thomet <pthomet@gmail.com>
2019-11-16 16:01:58 +0100

    edges_renderer: _checkEdge does not need to be virtual

--- src/BabylonCpp/include/babylon/rendering/edges_renderer.h
+++ src/BabylonCpp/include/babylon/rendering/edges_renderer.h
@@ -86,9 +86,9 @@ protected:
    * @param  p0
    * @param  p1
    */
-  virtual void _checkEdge(size_t faceIndex, int edge,
-                          const std::vector<Vector3>& faceNormals,
-                          const Vector3& p0, const Vector3& p1);
+  void _checkEdge(size_t faceIndex, int edge,
+                  const std::vector<Vector3>& faceNormals,
+                  const Vector3& p0, const Vector3& p1);
 
   /**
    * @brief Push line into the position, normal and index buffer.
