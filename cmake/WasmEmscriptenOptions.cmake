0d5963c70e1628e3ba91bee579aa05a4395d9d3a
Pascal Thomet <pthomet@gmail.com>
2019-12-10 13:32:03 +0100

    WasmEmscriptenOptions.cmake: simplify

--- cmake/WasmEmscriptenOptions.cmake
+++ cmake/WasmEmscriptenOptions.cmake
@@ -1,2 +1,1 @@
 set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-s USE_SDL=2 -s USE_WEBGL2=1 -s WASM=1 -s FULL_ES3=1 -s ALLOW_MEMORY_GROWTH=1")
-#set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-s --shell-file shell_minimal.html")
