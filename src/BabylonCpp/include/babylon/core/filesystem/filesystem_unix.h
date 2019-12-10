908d3ed34e18149500aa61badbba554d0b96b072
Pascal Thomet <pthomet@gmail.com>
2019-12-10 01:49:00 +0100

    Change emscripten detection mode

--- src/BabylonCpp/include/babylon/core/filesystem/filesystem_unix.h
+++ src/BabylonCpp/include/babylon/core/filesystem/filesystem_unix.h
@@ -1,7 +1,7 @@
 #ifndef BABYLON_CORE_FILESYSTEM_FILESYSTEM_UNIX_H
 #define BABYLON_CORE_FILESYSTEM_FILESYSTEM_UNIX_H
 
-#if (!defined(__APPLE__) && !defined(BABYLON_WASM))
+#if (!defined(__APPLE__) && !defined(__EMSCRIPTEN__))
 #include <linux/limits.h>
 #endif
 #include <string>
