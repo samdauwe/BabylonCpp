6ca31a1391506dd13dfce14eb5f32bafb79b9e9b
Pascal Thomet <pthomet@gmail.com>
2019-12-13 18:29:08 +0100

    WasmEmscriptenoptions.cmake: handle release build

--- cmake/WasmEmscriptenOptions.cmake
+++ cmake/WasmEmscriptenOptions.cmake
@@ -1,8 +1,17 @@
+# Standard flags for emscripten
 set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-s USE_SDL=2 -s USE_WEBGL2=1 -s WASM=1 -s FULL_ES3=1 -s ALLOW_MEMORY_GROWTH=1")
-#set(CMAKE_CXX_FLAGS -g4 ${CMAKE_CXX_FLAGS})
-#set(CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG} -O0 -g4)
-set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g4")
-#add_compile_options(-O0 -g4)
-#add_link_options(--source-map-base http://localhost:8001/src/ --preload-file build/assets@/)
+
+# Debug build is  -O0 -g4 in order to have the call stack + source map
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g4")
+
+    # source-map-base enables to be able to "kind of debug" from the js developper console
+    # Advice: always debug the same exact source code on desktop before even trying to run in the browser
+    # add_link_options(--source-map-base http://localhost:8001/src/)
+endif()
+
+#add_link_options(--preload-file build/assets@/)
+
+# Provide a default html shell
 set(link_options_shell_emscripten --shell-file ${CMAKE_SOURCE_DIR}/src/imgui_runner/runner_emscripten_shell.html)
 add_link_options(${link_options_shell_emscripten})
