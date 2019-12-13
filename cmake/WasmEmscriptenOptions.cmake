f32d012ad93c4515f049e1ed93e0b417b1ba7085
Pascal Thomet <pthomet@gmail.com>
2019-12-13 22:57:08 +0100

    Wasm.cmake: add exceptions in debug build

--- cmake/WasmEmscriptenOptions.cmake
+++ cmake/WasmEmscriptenOptions.cmake
@@ -3,11 +3,11 @@ set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-s USE_SDL=2 -s USE_WEBGL2=1 -s WASM=1 -
 
 # Debug build is  -O0 -g4 in order to have the call stack + source map
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g4")
+    set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g4 -s DISABLE_EXCEPTION_CATCHING=0")
 
     # source-map-base enables to be able to "kind of debug" from the js developper console
     # Advice: always debug the same exact source code on desktop before even trying to run in the browser
-    # add_link_options(--source-map-base http://localhost:8001/src/)
+    add_link_options(-O0 --source-map-base http://localhost:8000/src/)
 endif()
 
 #add_link_options(--preload-file build/assets@/)
