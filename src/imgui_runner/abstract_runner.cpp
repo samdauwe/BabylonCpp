d6b34ec37f1393788884e2c35f2f7d09ea23a85d
Pascal Thomet <pthomet@gmail.com>
2019-12-13 13:57:30 +0100

    AbstractRunner::SetupImgGuiContext(): ImGuiConfigFlags_ViewportsEnable false by default

--- src/imgui_runner/abstract_runner.cpp
+++ src/imgui_runner/abstract_runner.cpp
@@ -97,7 +97,8 @@ void AbstractRunner::SetupImgGuiContext()
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
   //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
   io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+  //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+
   //io.ConfigViewportsNoAutoMerge = true;
   //io.ConfigViewportsNoTaskBarIcon = true;
 
