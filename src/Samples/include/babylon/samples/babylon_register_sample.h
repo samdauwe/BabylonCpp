7880650d396c4f74f5638ddd420d5e6adbf376d0
Pascal Thomet <pthomet@gmail.com>
2019-12-15 00:26:14 +0100

    Change BABYLON_REGISTER_SAMPLE macro location

--- src/Samples/include/babylon/samples/babylon_register_sample.h
+++ src/Samples/include/babylon/samples/babylon_register_sample.h
@@ -5,4 +5,11 @@
 #ifndef BABYLONCPP_BABYLON_REGISTER_SAMPLE_H
 #define BABYLONCPP_BABYLON_REGISTER_SAMPLE_H
 
+// BABYLON_REGISTER_SAMPLE : registers an example in the Samples Index
+#define  BABYLON_REGISTER_SAMPLE(categoryName, sampleClassName)                          \
+std::shared_ptr<IRenderableScene> make_##sampleClassName(ICanvas* canvas)                \
+{                                                                                        \
+  return std::make_shared<sampleClassName>(canvas);                                      \
+}
+
 #endif // BABYLONCPP_BABYLON_REGISTER_SAMPLE_H
