f8725adb0652bf31e3e6231df48c17463b763dce
samdauwe <sam.dauwe@gmail.com>
2019-12-17 22:00:14 +0100

    Updated Path3D class to v4.1.0-beta.13

--- src/BabylonCpp/include/babylon/maths/path3d.h
+++ src/BabylonCpp/include/babylon/maths/path3d.h
@@ -6,6 +6,7 @@
 
 #include <babylon/babylon_api.h>
 #include <babylon/babylon_common.h>
+#include <babylon/maths/matrix.h>
 #include <babylon/maths/vector3.h>
 
 namespace BABYLON {
@@ -15,6 +16,20 @@ namespace BABYLON {
  */
 class BABYLON_SHARED_EXPORT Path3D {
 
+private:
+  // holds interpolated point data
+  struct PointAtData {
+    std::optional<float> id        = 0.f;
+    Vector3 point                  = Vector3::Zero();
+    size_t previousPointArrayIndex = 0;
+
+    float position    = 0.f;
+    float subPosition = 0.f;
+
+    bool interpolateReady      = false;
+    Matrix interpolationMatrix = Matrix::Identity();
+  }; // end of struct PointAtData
+
 public:
   Path3D();
   /**
@@ -23,13 +38,15 @@ public:
    * please read the description in the tutorial :
    * https://doc.babylonjs.com/how_to/how_to_use_path3d
    * @param path an array of Vector3, the curve axis of the Path3D
-   * @param firstNormal (options) Vector3, the first wanted normal to the curve.
-   * Ex (0, 1, 0) for a vertical normal.
-   * @param raw (optional, default false) : boolean, if true the returned Path3D
-   * isn't normalized. Useful to depict path acceleration or speed.
+   * @param firstNormal (options) Vector3, the first wanted normal to the curve. Ex (0, 1, 0) for a
+   * vertical normal.
+   * @param raw (optional, default false) : boolean, if true the returned Path3D isn't normalized.
+   * Useful to depict path acceleration or speed.
+   * @param alignTangentsWithPath (optional, default false) : boolean, if true the tangents will be
+   * aligned with the path.
    */
   Path3D(const std::vector<Vector3>& path, const std::optional<Vector3>& firstNormal = std::nullopt,
-         bool raw = false);
+         bool raw = false, bool alignTangentsWithPath = false);
   Path3D(const Path3D& otherPath);
   Path3D(Path3D&& otherPath);
   Path3D& operator=(const Path3D& otherPath);
@@ -46,70 +63,181 @@ public:
   std::vector<Vector3>& getCurve();
 
   /**
-   *@brief Returns an array populated with tangent vectors on each Path3D curve
-   *point.
-   * @returns an array populated with tangent vectors on each Path3D curve
-   *point.
+   * @brief Returns the Path3D array of successive Vector3 designing its curve.
+   * @returns the Path3D array of successive Vector3 designing its curve.
+   */
+  std::vector<Vector3>& getPoints();
+
+  /**
+   * @returns the computed length (float) of the path.
+   */
+  float length() const;
+
+  /**
+   *@brief Returns an array populated with tangent vectors on each Path3D curve point.
+   * @returns an array populated with tangent vectors on each Path3D curve point.
    */
   std::vector<Vector3>& getTangents();
 
   /**
-   * @brief Returns an array populated with normal vectors on each Path3D curve
-   * point.
+   * @brief Returns an array populated with normal vectors on each Path3D curve point.
    * @returns an array populated with normal vectors on each Path3D curve point.
    */
   std::vector<Vector3>& getNormals();
 
   /**
-   * @brief Returns an array populated with binormal vectors on each Path3D
-   * curve point.
-   * @returns an array populated with binormal vectors on each Path3D curve
-   * point.
+   * @brief Returns an array populated with binormal vectors on each Path3D curve point.
+   * @returns an array populated with binormal vectors on each Path3D curve point.
    */
   std::vector<Vector3>& getBinormals();
 
   /**
-   * @brief Returns an array populated with distances (float) of the i-th point
-   * from the first curve point.
-   * @returns an array populated with distances (float) of the i-th point from
-   * the first curve point.
+   * @brief Returns an array populated with distances (float) of the i-th point from the first curve
+   * point.
+   * @returns an array populated with distances (float) of the i-th point from the first curve
+   * point.
    */
   Float32Array& getDistances();
 
   /**
-   * @brief Forces the Path3D tangent, normal, binormal and distance
-   * recomputation.
+   * @brief Returns an interpolated point along this path
+   * @param position the position of the point along this path, from 0.0 to 1.0
+   * @returns a new Vector3 as the point
+   */
+  Vector3 getPointAt(float position);
+
+  /**
+   * @brief Returns the tangent vector of an interpolated Path3D curve point at the specified
+   * position along this path.
+   * @param position the position of the point along this path, from 0.0 to 1.0
+   * @param interpolated (optional, default false) : boolean, if true returns an interpolated
+   * tangent instead of the tangent of the previous path point.
+   * @returns a tangent vector corresponding to the interpolated Path3D curve point, if not
+   * interpolated, the tangent is taken from the precomputed tangents array.
+   */
+  Vector3 getTangentAt(float position, bool interpolated = false);
+
+  /**
+   * @brief Returns the tangent vector of an interpolated Path3D curve point at the specified
+   * position along this path.
+   * @param position the position of the point along this path, from 0.0 to 1.0
+   * @param interpolated (optional, default false) : boolean, if true returns an interpolated normal
+   * instead of the normal of the previous path point.
+   * @returns a normal vector corresponding to the interpolated Path3D curve point, if not
+   * interpolated, the normal is taken from the precomputed normals array.
+   */
+  Vector3 getNormalAt(float position, bool interpolated = false);
+
+  /**
+   * @brief Returns the binormal vector of an interpolated Path3D curve point at the specified
+   * position along this path.
+   * @param position the position of the point along this path, from 0.0 to 1.0
+   * @param interpolated (optional, default false) : boolean, if true returns an interpolated
+   * binormal instead of the binormal of the previous path point.
+   * @returns a binormal vector corresponding to the interpolated Path3D curve point, if not
+   * interpolated, the binormal is taken from the precomputed binormals array.
+   */
+  Vector3 getBinormalAt(float position, bool interpolated = false);
+
+  /**
+   * @brief Returns the distance (float) of an interpolated Path3D curve point at the specified
+   * position along this path.
+   * @param position the position of the point along this path, from 0.0 to 1.0
+   * @returns the distance of the interpolated Path3D curve point at the specified position along
+   * this path.
+   */
+  float getDistanceAt(float position) const;
+
+  /**
+   * @brief Returns the array index of the previous point of an interpolated point along this path
+   * @param position the position of the point to interpolate along this path, from 0.0 to 1.0
+   * @returns the array index
+   */
+  size_t getPreviousPointIndexAt(float position);
+
+  /**
+   * @brief Returns the position of an interpolated point relative to the two path points it lies
+   * between, from 0.0 (point A) to 1.0 (point B)
+   * @param position the position of the point to interpolate along this path, from 0.0 to 1.0
+   * @returns the sub position
+   */
+  float getSubPositionAt(float position);
+
+  /**
+   * @brief Returns the position of the closest virtual point on this path to an arbitrary Vector3,
+   * from 0.0 to 1.0
+   * @param target the vector of which to get the closest position to
+   * @returns the position of the closest virtual point on this path to the target vector
+   */
+  float getClosestPositionTo(const Vector3& target);
+
+  /**
+   * @brief Returns a sub path (slice) of this path
+   * @param start the position of the fist path point, from 0.0 to 1.0, or a negative value, which
+   * will get wrapped around from the end of the path to 0.0 to 1.0 values
+   * @param end the position of the last path point, from 0.0 to 1.0, or a negative value, which
+   * will get wrapped around from the end of the path to 0.0 to 1.0 values
+   * @returns a sub path (slice) of this path
+   */
+  Path3D slice(float start = 0.f, float end = 1.f);
+
+  /**
+   * @brief Forces the Path3D tangent, normal, binormal and distance recomputation.
    * @param path path which all values are copied into the curves points
    * @param firstNormal which should be projected onto the curve
+   * @param alignTangentsWithPath (optional, default false) : boolean, if true the tangents will be
+   * aligned with the path
    * @returns the same object updated.
    */
   Path3D& update(const std::vector<Vector3>& path,
-                 const std::optional<Vector3>& firstNormal = std::nullopt);
+                 const std::optional<Vector3>& firstNormal = std::nullopt,
+                 bool alignTangentsWithPath                = false);
 
 private:
   /**
    * @brief Computes tangents, normals and binormals.
    */
-  void _compute(const std::optional<Vector3>& firstNormal);
+  void _compute(const std::optional<Vector3>& firstNormal, bool alignTangentsWithPath = false);
 
   /**
-   * @brief Returns the first non null vector from index : curve[index +
-   * N].subtract(curve[index])
+   * @brief Returns the first non null vector from index : curve[index + N].subtract(curve[index])
    */
   Vector3 _getFirstNonNullVector(unsigned int index);
 
   /**
-   * @brief Returns the last non null vector from index :
-   * curve[index].subtract(curve[index - N]).
+   * @brief Returns the last non null vector from index : curve[index].subtract(curve[index - N]).
    */
   Vector3 _getLastNonNullVector(unsigned int index);
 
   /**
-   * @brief returns an arbitrary point in the plane defined by the point v0 and
-   * the vector vt orthogonal to this plane if va is passed, it returns the va
-   * projection on the plane orthogonal to vt at the point v0.
+   * @brief returns an arbitrary point in the plane defined by the point v0 and the vector vt
+   * orthogonal to this plane if va is passed, it returns the va projection on the plane orthogonal
+   * to vt at the point v0.
    */
   Vector3 _normalVector(const Vector3& vt, const std::optional<Vector3>& va);
+
+  /**
+   * @brief Updates the point at data for an interpolated point along this curve.
+   * @param position the position of the point along this curve, from 0.0 to 1.0
+   * @interpolateTNB wether to compute the interpolated tangent, normal and binormal
+   * @returns the (updated) point at data
+   */
+  PointAtData& _updatePointAtData(float position, bool interpolateTNB = false);
+
+  /**
+   * @brief Updates the point at data from the specified parameters.
+   * @param position where along the path the interpolated point is, from 0.0 to 1.0
+   * @param point the interpolated point
+   * @param parentIndex the index of an existing curve point that is on, or else positionally the
+   * first behind, the interpolated point
+   */
+  PointAtData& _setPointAtData(float position, float subPosition, const Vector3& point,
+                               size_t parentIndex, bool interpolateTNB);
+
+  /**
+   * @brief Updates the point at interpolation matrix for the tangents, normals and binormals.
+   */
+  void _updateInterpolationMatrix();
 
 public:
   /**
@@ -124,6 +252,8 @@ private:
   std::vector<Vector3> _normals;
   std::vector<Vector3> _binormals;
   bool _raw;
+  bool _alignTangentsWithPath;
+  PointAtData _pointAtData;
 
 }; // end of class Path3D
 
--- src/BabylonCpp/src/maths/path3d.cpp
+++ src/BabylonCpp/src/maths/path3d.cpp
@@ -8,18 +8,20 @@ namespace BABYLON {
 
 Path3D::Path3D() = default;
 
-Path3D::Path3D(const std::vector<Vector3>& iPath,
-               const std::optional<Vector3>& firstNormal, bool raw)
+Path3D::Path3D(const std::vector<Vector3>& iPath, const std::optional<Vector3>& firstNormal,
+               bool raw, bool alignTangentsWithPath)
     : path{iPath}, _raw{raw}
 {
   for (auto& vector : iPath) {
     _curve.emplace_back(vector);
   }
-  _compute(firstNormal);
+
+  _alignTangentsWithPath = alignTangentsWithPath;
+  _compute(firstNormal, alignTangentsWithPath);
 }
 
 Path3D::Path3D(const Path3D& otherPath) = default;
-Path3D::Path3D(Path3D&& otherPath) = default;
+Path3D::Path3D(Path3D&& otherPath)      = default;
 Path3D& Path3D::operator=(const Path3D& otherPath) = default;
 Path3D& Path3D::operator=(Path3D&& otherPath) = default;
 
@@ -82,6 +84,16 @@ std::vector<Vector3>& Path3D::getCurve()
   return _curve;
 }
 
+std::vector<Vector3>& Path3D::getPoints()
+{
+  return _curve;
+}
+
+float Path3D::length() const
+{
+  return _distances.back();
+}
+
 std::vector<Vector3>& Path3D::getTangents()
 {
   return _tangents;
@@ -102,8 +114,113 @@ Float32Array& Path3D::getDistances()
   return _distances;
 }
 
-Path3D& Path3D::update(const std::vector<Vector3>& iPath,
-                       const std::optional<Vector3>& firstNormal)
+Vector3 Path3D::getPointAt(float position)
+{
+  return _updatePointAtData(position).point;
+}
+
+Vector3 Path3D::getTangentAt(float position, bool interpolated)
+{
+  _updatePointAtData(position, interpolated);
+  return interpolated ?
+           Vector3::TransformCoordinates(Vector3::Forward(), _pointAtData.interpolationMatrix) :
+           _tangents[_pointAtData.previousPointArrayIndex];
+}
+
+Vector3 Path3D::getNormalAt(float position, bool interpolated)
+{
+  _updatePointAtData(position, interpolated);
+  return interpolated ?
+           Vector3::TransformCoordinates(Vector3::Right(), _pointAtData.interpolationMatrix) :
+           _normals[_pointAtData.previousPointArrayIndex];
+}
+
+Vector3 Path3D::getBinormalAt(float position, bool interpolated)
+{
+  _updatePointAtData(position, interpolated);
+  return interpolated ?
+           Vector3::TransformCoordinates(Vector3::UpReadOnly(), _pointAtData.interpolationMatrix) :
+           _binormals[_pointAtData.previousPointArrayIndex];
+}
+
+float Path3D::getDistanceAt(float position) const
+{
+  return length() * position;
+}
+
+size_t Path3D::getPreviousPointIndexAt(float position)
+{
+  _updatePointAtData(position);
+  return _pointAtData.previousPointArrayIndex;
+}
+
+float Path3D::getSubPositionAt(float position)
+{
+  _updatePointAtData(position);
+  return _pointAtData.subPosition;
+}
+
+float Path3D::getClosestPositionTo(const Vector3& target)
+{
+  auto smallestDistance = std::numeric_limits<float>::max();
+  auto closestPosition  = 0.f;
+  for (size_t i = 0; !_curve.empty() && i < _curve.size() - 1; ++i) {
+    const auto point     = _curve[i + 0];
+    const auto tangent   = _curve[i + 1].subtract(point).normalize();
+    const auto subLength = _distances[i + 1] - _distances[i + 0];
+    const auto subPosition
+      = std::min(std::max(Vector3::Dot(tangent, target.subtract(point).normalize()), 0.f)
+                   * Vector3::Distance(point, target) / subLength,
+                 1.f);
+    const auto distance
+      = Vector3::Distance(point.add(tangent.scale(subPosition * subLength)), target);
+
+    if (distance < smallestDistance) {
+      smallestDistance = distance;
+      closestPosition  = (_distances[i + 0] + subLength * subPosition) / length();
+    }
+  }
+  return closestPosition;
+}
+
+Path3D Path3D::slice(float start, float end)
+{
+  if (start < 0.f) {
+    start = 1.f - (start * -1.f) /* % 1.f */;
+  }
+  if (end < 0.f) {
+    end = 1.f - (end * -1.f) /* % 1.f */;
+  }
+  if (start > end) {
+    auto _start = start;
+    start       = end;
+    end         = _start;
+  }
+  auto curvePoints = getCurve();
+
+  auto startPoint = getPointAt(start);
+  auto startIndex = getPreviousPointIndexAt(start);
+
+  auto endPoint = getPointAt(end);
+  auto endIndex = getPreviousPointIndexAt(end) + 1;
+
+  std::vector<Vector3> slicePoints;
+  if (start != 0.f) {
+    startIndex++;
+    slicePoints.emplace_back(startPoint);
+  }
+
+  auto slice
+    = stl_util::slice(curvePoints, static_cast<int>(startIndex), static_cast<int>(endIndex));
+  stl_util::concat(slicePoints, slice);
+  if (end != 1.f || start == 1.f) {
+    slicePoints.emplace_back(endPoint);
+  }
+  return Path3D(slicePoints, getNormalAt(start), _raw, _alignTangentsWithPath);
+}
+
+Path3D& Path3D::update(const std::vector<Vector3>& iPath, const std::optional<Vector3>& firstNormal,
+                       bool alignTangentsWithPath)
 {
   if (_curve.size() < iPath.size()) {
     _curve.resize(iPath.size());
@@ -114,11 +231,11 @@ Path3D& Path3D::update(const std::vector<Vector3>& iPath,
     _curve[p].y = iPath[p].y;
     _curve[p].z = iPath[p].z;
   }
-  _compute(firstNormal);
+  _compute(firstNormal, alignTangentsWithPath);
   return *this;
 }
 
-void Path3D::_compute(const std::optional<Vector3>& firstNormal)
+void Path3D::_compute(const std::optional<Vector3>& firstNormal, bool alignTangentsWithPath)
 {
   const auto l = _curve.size();
 
@@ -152,10 +269,10 @@ void Path3D::_compute(const std::optional<Vector3>& firstNormal)
   _distances[0] = 0.f;
 
   // normals and binormals : next points
-  Vector3 prev;    // previous vector (segment)
-  Vector3 cur;     // current vector (segment)
-  Vector3 curTang; // current tangent
-  // previous normal
+  Vector3 prev;      // previous vector (segment)
+  Vector3 cur;       // current vector (segment)
+  Vector3 curTang;   // current tangent
+  Vector3 prevNor;   // previous normal
   Vector3 prevBinor; // previous binormal
 
   for (unsigned int i = 1; i < l; ++i) {
@@ -163,7 +280,7 @@ void Path3D::_compute(const std::optional<Vector3>& firstNormal)
     prev = _getLastNonNullVector(i);
     if (i < l - 1) {
       cur          = _getFirstNonNullVector(i);
-      _tangents[i] = prev.add(cur);
+      _tangents[i] = alignTangentsWithPath ? cur : prev.add(cur);
       _tangents[i].normalize();
     }
     _distances[i] = _distances[i - 1] + prev.length();
@@ -174,21 +291,27 @@ void Path3D::_compute(const std::optional<Vector3>& firstNormal)
     prevBinor   = _binormals[i - 1];
     _normals[i] = Vector3::Cross(prevBinor, curTang);
     if (!_raw) {
-      _normals[i].normalize();
+      if (_normals[i].length() == 0.f) {
+        prevNor     = _normals[i - 1];
+        _normals[i] = prevNor;
+      }
+      else {
+        _normals[i].normalize();
+      }
     }
     _binormals[i] = Vector3::Cross(curTang, _normals[i]);
     if (!_raw) {
       _binormals[i].normalize();
     }
   }
+  _pointAtData.id = std::nullopt;
 }
 
 Vector3 Path3D::_getFirstNonNullVector(unsigned int index)
 {
   unsigned int i   = 1;
   Vector3 nNVector = _curve[index + i].subtract(_curve[index]);
-  while (stl_util::almost_equal(nNVector.length(), 0.f)
-         && index + i + 1 < _curve.size()) {
+  while (stl_util::almost_equal(nNVector.length(), 0.f) && index + i + 1 < _curve.size()) {
     ++i;
     nNVector = _curve[index + i].subtract(_curve[index]);
   }
@@ -206,8 +329,7 @@ Vector3 Path3D::_getLastNonNullVector(unsigned int index)
   return nLVector;
 }
 
-Vector3 Path3D::_normalVector(const Vector3& vt,
-                              const std::optional<Vector3>& va)
+Vector3 Path3D::_normalVector(const Vector3& vt, const std::optional<Vector3>& va)
 {
   Vector3 normal0;
   float tgl = vt.length();
@@ -217,9 +339,8 @@ Vector3 Path3D::_normalVector(const Vector3& vt,
 
   if (va == std::nullopt) {
     Vector3 point;
-    if (!Scalar::WithinEpsilon(
-          std::abs(vt.y) / tgl, 1.f,
-          Math::Epsilon)) { // search for a point in the plane
+    if (!Scalar::WithinEpsilon(std::abs(vt.y) / tgl, 1.f,
+                               Math::Epsilon)) { // search for a point in the plane
       point = Vector3(0.f, -1.f, 0.f);
     }
     else if (!Scalar::WithinEpsilon(std::abs(vt.x) / tgl, 1.f, Math::Epsilon)) {
@@ -239,6 +360,92 @@ Vector3 Path3D::_normalVector(const Vector3& vt,
   }
   normal0.normalize();
   return normal0;
+}
+
+Path3D::PointAtData& Path3D::_updatePointAtData(float position, bool interpolateTNB)
+{
+  // set an id for caching the result
+  if (_pointAtData.id.has_value() && stl_util::almost_equal(_pointAtData.id.value(), position)) {
+    if (!_pointAtData.interpolateReady) {
+      _updateInterpolationMatrix();
+    }
+    return _pointAtData;
+  }
+  else {
+    _pointAtData.id = position;
+  }
+  const auto& curvePoints = getPoints();
+
+  // clamp position between 0.0 and 1.0
+  if (position <= 0.f) {
+    return _setPointAtData(0.f, 0.f, curvePoints[0], 0, interpolateTNB);
+  }
+  else if (position >= 1.f) {
+    return _setPointAtData(1.f, 1.f, curvePoints.back(), curvePoints.size() - 1, interpolateTNB);
+  }
+
+  auto previousPoint = curvePoints[0];
+  Vector3 currentPoint;
+  auto currentLength = 0.f;
+  auto targetLength  = position * length();
+
+  for (size_t i = 1; i < curvePoints.size(); ++i) {
+    currentPoint  = curvePoints[i];
+    auto distance = Vector3::Distance(previousPoint, currentPoint);
+    currentLength += distance;
+    if (stl_util::almost_equal(currentLength, targetLength)) {
+      return _setPointAtData(position, 1.f, currentPoint, i, interpolateTNB);
+    }
+    else if (currentLength > targetLength) {
+      auto toLength = currentLength - targetLength;
+      auto diff     = toLength / distance;
+      auto dir      = previousPoint.subtract(currentPoint);
+      auto point    = currentPoint.add(dir.scaleInPlace(diff));
+      return _setPointAtData(position, 1.f - diff, point, i - 1, interpolateTNB);
+    }
+    previousPoint = currentPoint;
+  }
+  return _pointAtData;
+}
+
+Path3D::PointAtData& Path3D::_setPointAtData(float position, float subPosition,
+                                             const Vector3& point, size_t parentIndex,
+                                             bool interpolateTNB)
+{
+  _pointAtData.point                   = point;
+  _pointAtData.position                = position;
+  _pointAtData.subPosition             = subPosition;
+  _pointAtData.previousPointArrayIndex = parentIndex;
+  _pointAtData.interpolateReady        = interpolateTNB;
+
+  if (interpolateTNB) {
+    _updateInterpolationMatrix();
+  }
+  return _pointAtData;
+}
+
+void Path3D::_updateInterpolationMatrix()
+{
+  _pointAtData.interpolationMatrix = Matrix::Identity();
+  auto parentIndex                 = _pointAtData.previousPointArrayIndex;
+
+  if (parentIndex != _tangents.size() - 1) {
+    auto index = parentIndex + 1;
+
+    auto tangentFrom  = _tangents[parentIndex];
+    auto normalFrom   = _normals[parentIndex];
+    auto binormalFrom = _binormals[parentIndex];
+
+    auto tangentTo  = _tangents[index];
+    auto normalTo   = _normals[index];
+    auto binormalTo = _binormals[index];
+
+    auto quatFrom = Quaternion::RotationQuaternionFromAxis(normalFrom, binormalFrom, tangentFrom);
+    auto quatTo   = Quaternion::RotationQuaternionFromAxis(normalTo, binormalTo, tangentTo);
+    auto quatAt   = Quaternion::Slerp(quatFrom, quatTo, _pointAtData.subPosition);
+
+    quatAt.toRotationMatrix(_pointAtData.interpolationMatrix);
+  }
 }
 
 } // end of namespace BABYLON
