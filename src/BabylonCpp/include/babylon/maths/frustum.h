298958566443dd4ebfd5c15be44638782f828d95
samdauwe <sam.dauwe@gmail.com>
2019-12-17 20:12:34 +0100

    Fixed type and applied formatting

--- src/BabylonCpp/include/babylon/maths/frustum.h
+++ src/BabylonCpp/include/babylon/maths/frustum.h
@@ -11,7 +11,7 @@ class Matrix;
 class Plane;
 
 /**
- * @brief Reprasents a camera frustum.
+ * @brief Represents a camera frustum.
  */
 class BABYLON_SHARED_EXPORT Frustum {
 
@@ -19,70 +19,61 @@ public:
   /** Statics **/
 
   /**
-   * @brief Gets the planes representing the frustum.
+   * @brief Gets the planes representing the frustum
    * @param transform matrix to be applied to the returned planes
-   * @returns a new array of 6 Frustum planes computed by the given
-   * transformation matrix.
+   * @returns a new array of 6 Frustum planes computed by the given transformation matrix.
    */
   static std::array<Plane, 6> GetPlanes(const Matrix& transform);
 
   /**
-   * @brief Gets the near frustum plane transformed by the transform matrix.
-   * @param transform transformation matrix to be applied to the resulting
-   * frustum plane
+   * @brief Gets the near frustum plane transformed by the transform matrix
+   * @param transform transformation matrix to be applied to the resulting frustum plane
    * @param frustumPlane the resuling frustum plane
    */
   static void GetNearPlaneToRef(const Matrix& transform, Plane& frustumPlane);
 
   /**
    * @brief Gets the far frustum plane transformed by the transform matrix.
-   * @param transform transformation matrix to be applied to the resulting
-   * frustum plane
+   * @param transform transformation matrix to be applied to the resulting frustum plane
    * @param frustumPlane the resuling frustum plane
    */
   static void GetFarPlaneToRef(const Matrix& transform, Plane& frustumPlane);
 
   /**
    * @brief Gets the left frustum plane transformed by the transform matrix.
-   * @param transform transformation matrix to be applied to the resulting
-   * frustum plane
+   * @param transform transformation matrix to be applied to the resulting frustum plane
    * @param frustumPlane the resuling frustum plane
    */
   static void GetLeftPlaneToRef(const Matrix& transform, Plane& frustumPlane);
 
   /**
    * @brief Gets the right frustum plane transformed by the transform matrix.
-   * @param transform transformation matrix to be applied to the resulting
-   * frustum plane
+   * @param transform transformation matrix to be applied to the resulting frustum plane
    * @param frustumPlane the resuling frustum plane
    */
   static void GetRightPlaneToRef(const Matrix& transform, Plane& frustumPlane);
 
   /**
    * @brief Gets the top frustum plane transformed by the transform matrix.
-   * @param transform transformation matrix to be applied to the resulting
-   * frustum plane
+   * @param transform transformation matrix to be applied to the resulting frustum plane
    * @param frustumPlane the resuling frustum plane
    */
   static void GetTopPlaneToRef(const Matrix& transform, Plane& frustumPlane);
 
   /**
    * @brief Gets the bottom frustum plane transformed by the transform matrix.
-   * @param transform transformation matrix to be applied to the resulting
-   * frustum plane
+   * @param transform transformation matrix to be applied to the resulting frustum plane
    * @param frustumPlane the resuling frustum plane
    */
   static void GetBottomPlaneToRef(const Matrix& transform, Plane& frustumPlane);
 
   /**
-   * @brief Sets the given array "frustumPlanes" with the 6 Frustum planes
-   * computed by the given transformation matrix.
-   * @param transform transformation matrix to be applied to the resulting
-   * frustum planes
+   * @brief Sets the given array "frustumPlanes" with the 6 Frustum planes computed by the given
+   * transformation matrix.
+   * @param transform transformation matrix to be applied to the resulting frustum planes
    * @param frustumPlanes the resuling frustum planes
    */
-  static void GetPlanesToRef(const Matrix& transform,
-                             std::array<Plane, 6>& frustumPlanes);
+  static void GetPlanesToRef(const Matrix& transform, std::array<Plane, 6>& frustumPlanes);
 
 }; // end of class Frustum
 
