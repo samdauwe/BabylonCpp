c8f905273b9fe2cdb0449fcdb0408a0d2b322cbf
Pascal Thomet <pthomet@gmail.com>
2019-12-22 23:50:41 +0100

    asio_emscripten: better logs

--- src/BabylonCpp/src/asio/internal/asio_emscripten.cpp
+++ src/BabylonCpp/src/asio/internal/asio_emscripten.cpp
@@ -42,55 +42,44 @@ DownloadId storeDownloadInfo(
   std::function<void(const ArrayBuffer& data)> onSuccessFunction,
   std::function<void(const std::string& message)> onErrorFunction)
 {
-  std::cout << "storeDownloadInfo 1 \n";
   static int id = 0;
   ++id;
   DownloadInfo info {url, onSuccessFunction, onErrorFunction};
   gDownloadInfos[id] = info;
-  std::cout << "storeDownloadInfo 2 returned id " << id << " gDownloadInfos size=" << gDownloadInfos.size() << "\n";
   return id;
 }
 
 DownloadInfo consumeDownloadInfo(DownloadId id)
 {
-  std::cout << "consumeDownloadInfo 1 \n";
   if (gDownloadInfos.find(id) == gDownloadInfos.end()) {
-    std::cout << "consumeDownloadInfo; bad id !!! -> " << id << "\n";
     throw std::runtime_error("consumeDownloadInfo; bad id");
   }
-  std::cout << "consumeDownloadInfo 2 \n";
   DownloadInfo r = gDownloadInfos.at(id);
   gDownloadInfos.erase(id);
-  std::cout << "consumeDownloadInfo 3 gDownloadInfos size=" << gDownloadInfos.size() << "\n";
   return r;
 }
 
 // See https://emscripten.org/docs/api_reference/emscripten.h.html#c.emscripten_async_wget_data
 void babylon_emscripten_onLoad(void *arg_downloadId, void *bufferData, int bufferSize)
 {
-  std::cout << "babylon_emscripten_onLoad 1 bufferSize=" << bufferSize <<  "arg_downloadId=" << (int)arg_downloadId << "\n";
   uint8_t* bufferDataAsUint8 = static_cast<uint8_t *>(bufferData);
   ArrayBuffer arrayBuffer(bufferDataAsUint8, bufferDataAsUint8 + bufferSize); // this makes a copy
-  std::cout << "babylon_emscripten_onLoad 2 \n";
 
   DownloadInfo info = consumeDownloadInfo((DownloadId)arg_downloadId);
-  std::cout << "babylon_emscripten_onLoad 3 \n";
+  BABYLON_LOG_DEBUG("babylon_emscripten_onLoad", info.url.c_str(), " Success!");
   info.onSuccessFunction(arrayBuffer);
-  std::cout << "babylon_emscripten_onLoad 4 \n";
 }
 
 void babylon_emscripten_onError(void *arg_downloadId)
 {
-  std::cout << "babylon_emscripten_onError 1 " <<  "arg_downloadId=" << (int)arg_downloadId << "\n";
   DownloadInfo info = consumeDownloadInfo((DownloadId)arg_downloadId);
-  std::cout << "babylon_emscripten_onError 2 \n";
   std::string errorMessage = std::string("Error while downloading ") + info.url;
+  BABYLON_LOG_DEBUG("babylon_emscripten_onError", info.url.c_str(), " Failure!");
   info.onErrorFunction(errorMessage);
-  std::cout << "babylon_emscripten_onError 3 \n";
 }
 
 static std::string BaseUrl() {
-  return "../assets/";
+  return "./emscripten_http_assets/assets/";
 }
 
 } // anonymous namespace
@@ -108,15 +97,12 @@ void LoadUrlAsync_Text(
   const OnProgressFunction& onProgressFunction
 )
 {
-  std::cout << "LoadUrlAsync_Text 1 \n";
   std::string fullUrl = BaseUrl() + url;
   auto onSuccessFunctionArrayBuffer = [onSuccessFunction](const ArrayBuffer& dataUint8) {
     onSuccessFunction(ArrayBufferToString(dataUint8));
   };
   auto downloadId = storeDownloadInfo(fullUrl.c_str(), onSuccessFunctionArrayBuffer, onErrorFunction);
-  std::cout << "LoadUrlAsync_Text 2 \n";
   emscripten_async_wget_data(fullUrl.c_str(), (void*)downloadId, babylon_emscripten_onLoad, babylon_emscripten_onError);
-  std::cout << "LoadUrlAsync_Text 3 \n";
 }
 
 void LoadUrlAsync_Binary(
@@ -126,12 +112,9 @@ void LoadUrlAsync_Binary(
   const OnProgressFunction& onProgressFunction
 )
 {
-  std::cout << "LoadUrlAsync_Binary 1 \n";
   std::string fullUrl = BaseUrl() + url;
   auto downloadId = storeDownloadInfo(fullUrl.c_str(), onSuccessFunction, onErrorFunction);
-  std::cout << "LoadUrlAsync_Binary 2 \n";
   emscripten_async_wget_data(fullUrl.c_str(), (void*)downloadId, babylon_emscripten_onLoad, babylon_emscripten_onError);
-  std::cout << "LoadUrlAsync_Binary 3 \n";
 }
 
 // Call this in the app's main loop: it will run the callbacks synchronously
