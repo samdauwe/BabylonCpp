f47363c1b5d0a787686f65e70e28c1431d498f63
Pascal Thomet <pthomet@gmail.com>
2020-01-09 15:01:32 +0100

    gl_rendering_context: correct glGetError call

--- src/BabylonImGui/src/GL/gl_rendering_context.cpp
+++ src/BabylonImGui/src/GL/gl_rendering_context.cpp
@@ -130,7 +130,7 @@ std::string GlErrorCodeStr(GLenum error_code)
 void glad_post_call_callback(const char* name, void* /*funcptr*/, int /*len_args*/, ...)
 {
   GLenum error_code;
-  error_code = glad_glGetError();
+  error_code = glGetError();
 
   std::stringstream msg_str;
   msg_str << "ERROR " << GlErrorCodeStr(error_code) << "(" << error_code << ") in " << name << "\n";
