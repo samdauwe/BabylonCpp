f196783c947d958171616f796c0cb3ed7204217f
Pascal Thomet <pthomet@gmail.com>
2020-01-09 17:23:02 +0100

    CmakeListss: use babylon_add_library
    this leads to massive simplifications

---
+++ cmake/BabylonAddLibrary.cmake
@@ -0,0 +1,38 @@
+function(babylon_add_library TARGET)
+    message("babylon_add_library ${TARGET}")
+
+    add_library(${TARGET} ${ARGN})
+    babylon_target_clang_tidy(${TARGET})
+
+    # Create namespaced alias
+    add_library(${META_PROJECT_NAME}::${TARGET} ALIAS ${TARGET})
+
+    # Create API export header
+    generate_export_header(${TARGET}
+        EXPORT_FILE_NAME  ${EXPORT_FILE}
+        EXPORT_MACRO_NAME ${EXPORT_MACRO}
+        )
+
+    # Set library ouput name
+    set_target_properties(${TARGET}
+        PROPERTIES  PREFIX "${CMAKE_SHARED_LIBRARY_PREFIX}"
+        OUTPUT_NAME $<LOWER_CASE:${TARGET}>
+        VERSION ${META_VERSION}
+        SOVERSION ${META_VERSION}
+        )
+
+    # Compile definitions
+    target_compile_definitions(${TARGET}
+        PUBLIC
+        $<$<NOT:$<BOOL:${BUILD_SHARED_LIBS}>>:${BABYLON_UPPER}_STATIC_DEFINE>
+        )
+
+endfunction()
+
+
+function(babylon_add_executable TARGET)
+    message("babylon_add_executable ${TARGET}")
+
+    add_executable(${TARGET} ${ARGN})
+    babylon_target_clang_tidy(${TARGET})
+endfunction()
