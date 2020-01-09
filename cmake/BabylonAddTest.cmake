80e9ce81fa1d6fc8b024f2894c62d0559e459a74
Pascal Thomet <pthomet@gmail.com>
2020-01-09 16:57:26 +0100

    Add cmake/BabylonAddTest.cmake

---
+++ cmake/BabylonAddTest.cmake
@@ -0,0 +1,9 @@
+macro(babylon_add_test TESTNAME)
+    message("babylon_add_test ${TESTNAME}")
+    add_executable(${TESTNAME} ${ARGN})
+    # Create namespaced alias
+    add_executable(${META_PROJECT_NAME}::${TESTNAME} ALIAS ${TESTNAME})
+
+    target_link_libraries(${TESTNAME} PRIVATE gtest gmock gtest_main)
+    add_test(NAME ${TESTNAME} COMMAND ${TESTNAME})
+endmacro()
