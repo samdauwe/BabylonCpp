862199774c81060254c60f0733cd017d3d50f48b
samdauwe <sam.dauwe@gmail.com>
2020-02-02 13:30:33 +0100

    Formatted DecalBuilder class

--- src/BabylonCpp/include/babylon/meshes/builders/decal_builder.h
+++ src/BabylonCpp/include/babylon/meshes/builders/decal_builder.h
@@ -22,24 +22,22 @@ struct BABYLON_SHARED_EXPORT DecalBuilder {
 
   /**
    * @brief Creates a decal mesh.
-   * A decal is a mesh usually applied as a model onto the surface of another
-   * mesh. So don't forget the parameter `sourceMesh` depicting the decal
-   * * The parameter `position` (Vector3, default `(0, 0, 0)`) sets the position
-   * of the decal in World coordinates
-   * * The parameter `normal` (Vector3, default `Vector3.Up`) sets the normal of
-   * the mesh where the decal is applied onto in World coordinates
-   * * The parameter `size` (Vector3, default `(1, 1, 1)`) sets the decal
-   * scaling
-   * * The parameter `angle` (float in radian, default 0) sets the angle to
-   * rotate the decal
+   * A decal is a mesh usually applied as a model onto the surface of another mesh. So don't forget
+   * the parameter `sourceMesh` depicting the decal
+   * * The parameter `position` (Vector3, default `(0, 0, 0)`) sets the position of the decal in
+   * World coordinates
+   * * The parameter `normal` (Vector3, default `Vector3.Up`) sets the normal of the mesh where the
+   * decal is applied onto in World coordinates
+   * * The parameter `size` (Vector3, default `(1, 1, 1)`) sets the decal scaling
+   * * The parameter `angle` (float in radian, default 0) sets the angle to rotate the decal
    * @param name defines the name of the mesh
    * @param sourceMesh defines the mesh where the decal must be applied
    * @param options defines the options used to create the mesh
+   * @param scene defines the hosting scene
    * @returns the decal mesh
    * @see https://doc.babylonjs.com/how_to/decals
    */
-  static MeshPtr CreateDecal(const std::string& name,
-                             const AbstractMeshPtr& sourceMesh,
+  static MeshPtr CreateDecal(const std::string& name, const AbstractMeshPtr& sourceMesh,
                              DecalOptions& options);
 
 }; // end of struct DecalBuilder
