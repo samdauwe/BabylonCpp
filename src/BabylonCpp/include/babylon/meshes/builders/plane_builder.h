c342556db0e01b2efa4f2af57086feb452c03693
samdauwe <sam.dauwe@gmail.com>
2020-02-06 20:33:08 +0100

    Formatted PlaneBuilder class

--- src/BabylonCpp/include/babylon/meshes/builders/plane_builder.h
+++ src/BabylonCpp/include/babylon/meshes/builders/plane_builder.h
@@ -20,21 +20,18 @@ struct BABYLON_SHARED_EXPORT PlaneBuilder {
 
   /**
    * @brief Creates a plane mesh.
-   * * The parameter `size` sets the size (float) of both sides of the plane at
-   * once (default 1)
-   * * You can set some different plane dimensions by using the parameters
-   * `width` and `height` (both by default have the same value of `size`)
-   * * The parameter `sourcePlane` is a Plane instance. It builds a mesh plane
-   * from a Math plane
-   * * You can also set the mesh side orientation with the values :
-   * BABYLON.Mesh.FRONTSIDE (default), BABYLON.Mesh.BACKSIDE or
-   * BABYLON.Mesh.DOUBLESIDE
-   * * If you create a double-sided mesh, you can choose what parts of the
-   * texture image to crop and stick respectively on the front and the back
-   * sides with the parameters `frontUVs` and `backUVs` (Vector4). Detail here :
+   * * The parameter `size` sets the size (float) of both sides of the plane at once (default 1)
+   * * You can set some different plane dimensions by using the parameters `width` and `height`
+   * (both by default have the same value of `size`)
+   * * The parameter `sourcePlane` is a Plane instance. It builds a mesh plane from a Math plane
+   * * You can also set the mesh side orientation with the values : BABYLON.Mesh.FRONTSIDE
+   * (default), BABYLON.Mesh.BACKSIDE or BABYLON.Mesh.DOUBLESIDE
+   * * If you create a double-sided mesh, you can choose what parts of the texture image to crop and
+   * stick respectively on the front and the back sides with the parameters `frontUVs` and `backUVs`
+   * (Vector4). Detail here :
    * https://doc.babylonjs.com/babylon101/discover_basic_elements#side-orientation
-   * * The mesh can be set to updatable with the boolean parameter `updatable`
-   * (default false) if its internal geometry is supposed to change once created
+   * * The mesh can be set to updatable with the boolean parameter `updatable` (default false) if
+   * its internal geometry is supposed to change once created
    * @param name defines the name of the mesh
    * @param options defines the options used to create the mesh
    * @param scene defines the hosting scene
