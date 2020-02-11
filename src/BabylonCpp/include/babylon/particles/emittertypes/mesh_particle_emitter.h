cedb7ae18399c3209ec330b1b66437e4154f4472
samdauwe <sam.dauwe@gmail.com>
2020-02-11 21:15:13 +0100

    Added MeshParticleEmitter class

---
+++ src/BabylonCpp/include/babylon/particles/emittertypes/mesh_particle_emitter.h
@@ -0,0 +1,116 @@
+#ifndef BABYLON_PARTICLES_EMITTER_TYPES_MESH_PARTICLE_EMITTER_H
+#define BABYLON_PARTICLES_EMITTER_TYPES_MESH_PARTICLE_EMITTER_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/babylon_common.h>
+#include <babylon/maths/vector3.h>
+#include <babylon/particles/emittertypes/iparticle_emitter_type.h>
+
+namespace BABYLON {
+
+class AbstractMesh;
+class Scene;
+using AbstractMeshPtr = std::shared_ptr<AbstractMesh>;
+
+/**
+ * @brief Particle emitter emitting particles from the inside of a box.
+ * It emits the particles randomly between 2 given directions.
+ */
+class BABYLON_SHARED_EXPORT MeshParticleEmitter : public IParticleEmitterType {
+
+public:
+  /**
+   * @brief Creates a new instance MeshParticleEmitter.
+   * @param mesh defines the mesh to use as source
+   */
+  MeshParticleEmitter(const AbstractMeshPtr& mesh = nullptr);
+  ~MeshParticleEmitter() override; // = default
+
+  /**
+   * @brief Called by the particle System when the direction is computed for the created particle.
+   * @param worldMatrix is the world matrix of the particle system
+   * @param directionToUpdate is the direction vector to update with the result
+   * @param particle is the particle we are computed the direction for
+   */
+  void startDirectionFunction(const Matrix& worldMatrix, Vector3& directionToUpdate,
+                              Particle* particle) override;
+
+  /**
+   * @brief Called by the particle System when the position is computed for the created particle.
+   * @param worldMatrix is the world matrix of the particle system
+   * @param positionToUpdate is the position vector to update with the result
+   * @param particle is the particle we are computed the position for
+   */
+  void startPositionFunction(const Matrix& worldMatrix, Vector3& positionToUpdate,
+                             Particle* particle) override;
+
+  /**
+   * @brief Clones the current emitter and returns a copy of it.
+   * @returns the new emitter
+   */
+  std::unique_ptr<IParticleEmitterType> clone() const override;
+
+  /**
+   * @brief Called by the GPUParticleSystem to setup the update shader
+   * @param effect defines the update shader
+   */
+  void applyToShader(Effect* effect) override;
+
+  /**
+   * @brief Returns a string to use to update the GPU particles update shader.
+   * @returns a string containng the defines string
+   */
+  std::string getEffectDefines() const override;
+
+  /**
+   * @brief Returns the string "ConeParticleEmitter".
+   * @returns a string containing the class name
+   */
+  std::string getClassName() const override;
+
+  /**
+   * @brief Serializes the particle system to a JSON object.
+   * @returns the JSON object
+   */
+  json serialize() const override;
+
+  /**
+   * @brief Parse properties from a JSON object.
+   * @param serializationObject defines the JSON object
+   * @param scene defines the hosting scene
+   */
+  void parse(const json& serializationObject, Scene* scene) override;
+
+public:
+  /**
+   * Random direction of each particle after it has been emitted, between direction1 and direction2
+   * vectors.
+   */
+  Vector3 direction1;
+  /**
+   * Random direction of each particle after it has been emitted, between direction1 and direction2
+   * vectors.
+   */
+  Vector3 direction2;
+
+  /**
+   * Gets or sets a boolean indicating that particle directions must be built from mesh face normals
+   */
+  bool useMeshNormalsForDirection;
+
+  /**
+   * Defines the mesh to use as source
+   */
+  AbstractMeshPtr mesh;
+
+private:
+  IndicesArray _indices;
+  Float32Array _positions;
+  Float32Array _normals;
+  Vector3 _storedNormal;
+
+}; // end of class MeshParticleEmitter
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_PARTICLES_EMITTER_TYPES_MESH_PARTICLE_EMITTER_H
---
+++ src/BabylonCpp/src/particles/emittertypes/mesh_particle_emitter.cpp
@@ -0,0 +1,130 @@
+#include <babylon/particles/emittertypes/mesh_particle_emitter.h>
+
+#include <babylon/core/json_util.h>
+#include <babylon/core/random.h>
+#include <babylon/engines/scene.h>
+#include <babylon/materials/effect.h>
+#include <babylon/maths/scalar.h>
+#include <babylon/maths/tmp_vectors.h>
+#include <babylon/meshes/abstract_mesh.h>
+#include <babylon/meshes/vertex_buffer.h>
+
+namespace BABYLON {
+
+MeshParticleEmitter::MeshParticleEmitter(const AbstractMeshPtr& iMesh)
+    : direction1{Vector3(0.f, 1.f, 0.f)}
+    , direction2{Vector3(0.f, 1.f, 0.f)}
+    , useMeshNormalsForDirection{true}
+    , mesh{iMesh}
+    , _storedNormal{Vector3::Zero()}
+{
+  if (mesh) {
+    _indices   = mesh->getIndices();
+    _positions = mesh->getVerticesData(VertexBuffer::PositionKind);
+    _normals   = mesh->getVerticesData(VertexBuffer::NormalKind);
+  }
+}
+
+MeshParticleEmitter::~MeshParticleEmitter() = default;
+
+void MeshParticleEmitter::startDirectionFunction(const Matrix& worldMatrix,
+                                                 Vector3& directionToUpdate, Particle* /*particle*/)
+{
+  if (useMeshNormalsForDirection && !_normals.empty()) {
+    Vector3::TransformNormalToRef(_storedNormal, worldMatrix, directionToUpdate);
+    return;
+  }
+
+  const auto randX = Scalar::RandomRange(direction1.x, direction2.x);
+  const auto randY = Scalar::RandomRange(direction1.y, direction2.y);
+  const auto randZ = Scalar::RandomRange(direction1.z, direction2.z);
+
+  Vector3::TransformNormalFromFloatsToRef(randX, randY, randZ, worldMatrix, directionToUpdate);
+}
+
+void MeshParticleEmitter::startPositionFunction(const Matrix& worldMatrix,
+                                                Vector3& positionToUpdate, Particle* /*particle*/)
+{
+  if (_indices.empty() || _positions.empty()) {
+    return;
+  }
+
+  const auto randomFaceIndex = static_cast<size_t>(3 * Math::random() * (_indices.size() / 3));
+  const auto bu              = Math::random();
+  const auto bv              = Math::random() * (1.f - bu);
+  const auto bw              = 1.f - bu - bv;
+
+  const auto faceIndexA = _indices[randomFaceIndex];
+  const auto faceIndexB = _indices[randomFaceIndex + 1];
+  const auto faceIndexC = _indices[randomFaceIndex + 2];
+  auto& vertexA         = TmpVectors::Vector3Array[0];
+  auto& vertexB         = TmpVectors::Vector3Array[1];
+  auto& vertexC         = TmpVectors::Vector3Array[2];
+  auto& randomVertex    = TmpVectors::Vector3Array[3];
+
+  Vector3::FromArrayToRef(_positions, faceIndexA * 3, vertexA);
+  Vector3::FromArrayToRef(_positions, faceIndexB * 3, vertexB);
+  Vector3::FromArrayToRef(_positions, faceIndexC * 3, vertexC);
+
+  randomVertex.x = bu * vertexA.x + bv * vertexB.x + bw * vertexC.x;
+  randomVertex.y = bu * vertexA.y + bv * vertexB.y + bw * vertexC.y;
+  randomVertex.z = bu * vertexA.z + bv * vertexB.z + bw * vertexC.z;
+
+  Vector3::TransformCoordinatesFromFloatsToRef(randomVertex.x, randomVertex.y, randomVertex.z,
+                                               worldMatrix, positionToUpdate);
+
+  if (useMeshNormalsForDirection && !_normals.empty()) {
+    Vector3::FromArrayToRef(_normals, faceIndexA * 3, vertexA);
+    Vector3::FromArrayToRef(_normals, faceIndexB * 3, vertexB);
+    Vector3::FromArrayToRef(_normals, faceIndexC * 3, vertexC);
+
+    _storedNormal.x = bu * vertexA.x + bv * vertexB.x + bw * vertexC.x;
+    _storedNormal.y = bu * vertexA.y + bv * vertexB.y + bw * vertexC.y;
+    _storedNormal.z = bu * vertexA.z + bv * vertexB.z + bw * vertexC.z;
+  }
+}
+
+std::unique_ptr<IParticleEmitterType> MeshParticleEmitter::clone() const
+{
+  auto newOne = std::make_unique<MeshParticleEmitter>();
+
+  return newOne;
+}
+
+void MeshParticleEmitter::applyToShader(Effect* effect)
+{
+  effect->setVector3("direction1", direction1);
+  effect->setVector3("direction2", direction2);
+}
+
+std::string MeshParticleEmitter::getEffectDefines() const
+{
+  return "";
+}
+
+std::string MeshParticleEmitter::getClassName() const
+{
+  return "MeshParticleEmitter";
+}
+
+json MeshParticleEmitter::serialize() const
+{
+  return nullptr;
+}
+
+void MeshParticleEmitter::parse(const json& serializationObject, Scene* scene)
+{
+  Vector3::FromArrayToRef(json_util::get_array<float>(serializationObject, "direction1"), 0,
+                          direction1);
+  Vector3::FromArrayToRef(json_util::get_array<float>(serializationObject, "direction2"), 0,
+                          direction2);
+
+  if (json_util::has_valid_key_value(serializationObject, "meshId")) {
+    mesh = scene->getLastMeshByID(json_util::get_string(serializationObject, "meshId"));
+  }
+
+  useMeshNormalsForDirection
+    = json_util::get_bool(serializationObject, "useMeshNormalsForDirection");
+}
+
+} // end of namespace BABYLON
