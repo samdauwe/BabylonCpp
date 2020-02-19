a1a27243b2e7414b83dca34d044964dfd4085df0
samdauwe <sam.dauwe@gmail.com>
2020-02-19 20:51:11 +0100

    Added StereoscopicInterlacePostProcessI class

---
+++ src/BabylonCpp/include/babylon/postprocesses/stereoscopic_interlace_post_process_i.h
@@ -0,0 +1,56 @@
+#ifndef BABYLON_POSTPROCESSES_STEREOSCOPIC_INTERLACE_POST_PROCESS_I_H
+#define BABYLON_POSTPROCESSES_STEREOSCOPIC_INTERLACE_POST_PROCESS_I_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/maths/vector2.h>
+#include <babylon/postprocesses/post_process.h>
+
+namespace BABYLON {
+
+class StereoscopicInterlacePostProcessI;
+using StereoscopicInterlacePostProcessIPtr = std::shared_ptr<StereoscopicInterlacePostProcessI>;
+
+/**
+ * @brief StereoscopicInterlacePostProcessI used to render stereo views from a rigged camera with
+ * support for alternate line interlacing
+ */
+class BABYLON_SHARED_EXPORT StereoscopicInterlacePostProcessI : public PostProcess {
+
+public:
+  template <typename... Ts>
+  static StereoscopicInterlacePostProcessIPtr New(Ts&&... args)
+  {
+    auto postProcess = std::shared_ptr<StereoscopicInterlacePostProcessI>(
+      new StereoscopicInterlacePostProcessI(std::forward<Ts>(args)...));
+    postProcess->add(postProcess);
+
+    return postProcess;
+  }
+  ~StereoscopicInterlacePostProcessI() override; // = default
+
+protected:
+  /**
+   * @brief Initializes a StereoscopicInterlacePostProcessI.
+   * @param name The name of the effect.
+   * @param rigCameras The rig cameras to be appled to the post process
+   * @param isStereoscopicHoriz If the rendered results are horizontal or vertical
+   * @param isStereoscopicInterlaced If the rendered results are alternate line interlaced
+   * @param samplingMode The sampling mode to be used when computing the pass. (default: 0)
+   * @param engine The engine which the post process will be applied. (default: current engine)
+   * @param reusable If the post process can be reused on the same frame. (default: false)
+   */
+  StereoscopicInterlacePostProcessI(const std::string& name,
+                                    const std::vector<CameraPtr>& rigCameras,
+                                    bool isStereoscopicHoriz, bool isStereoscopicInterlaced,
+                                    const std::optional<unsigned int>& samplingMode = std::nullopt,
+                                    Engine* engine = nullptr, bool reusable = false);
+
+private:
+  Vector2 _stepSize;
+  PostProcessPtr _passedProcess;
+
+}; // end of class StereoscopicInterlacePostProcessI
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_POSTPROCESSES_STEREOSCOPIC_INTERLACE_POST_PROCESS_I_H
---
+++ src/BabylonCpp/src/postprocesses/stereoscopic_interlace_post_process_i.cpp
@@ -0,0 +1,32 @@
+#include <babylon/postprocesses/stereoscopic_interlace_post_process_i.h>
+
+#include <babylon/cameras/camera.h>
+#include <babylon/materials/effect.h>
+
+namespace BABYLON {
+
+StereoscopicInterlacePostProcessI::StereoscopicInterlacePostProcessI(
+  const std::string& iName, const std::vector<CameraPtr>& rigCameras, bool isStereoscopicHoriz,
+  bool isStereoscopicInterlaced, const std::optional<unsigned int>& samplingMode, Engine* engine,
+  bool reusable)
+    : PostProcess(iName, "stereoscopicInterlace", {"stepSize"}, {"camASampler"}, 1.f, rigCameras[1],
+                  samplingMode, engine, reusable,
+                  isStereoscopicInterlaced ?
+                    "#define IS_STEREOSCOPIC_INTERLACED 1" :
+                    isStereoscopicHoriz ? "#define IS_STEREOSCOPIC_HORIZ 1" : "")
+{
+  _passedProcess = rigCameras[0]->_rigPostProcess;
+  _stepSize      = Vector2(1.f / static_cast<float>(width), 1.f / static_cast<float>(height));
+
+  onSizeChangedObservable.add([&](PostProcess*, EventState&) {
+    _stepSize = Vector2(1.f / static_cast<float>(width), 1.f / static_cast<float>(height));
+  });
+  onApplyObservable.add([&](Effect* effect, EventState&) {
+    effect->setTextureFromPostProcess("camASampler", _passedProcess);
+    effect->setFloat2("stepSize", _stepSize.x, _stepSize.y);
+  });
+}
+
+StereoscopicInterlacePostProcessI::~StereoscopicInterlacePostProcessI() = default;
+
+} // end of namespace BABYLON
