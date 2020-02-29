c214ca9824d6cbd6167cb43957d7564636b1fa26
samdauwe <sam.dauwe@gmail.com>
2020-02-29 19:18:47 +0100

    Added MinMaxReducer class

---
+++ src/BabylonCpp/include/babylon/misc/min_max_reducer.h
@@ -0,0 +1,132 @@
+#ifndef BABYLON_MISC_MIN_MAX_REDUCER_H
+#define BABYLON_MISC_MIN_MAX_REDUCER_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/core/structs.h>
+#include <babylon/engines/constants.h>
+#include <babylon/misc/observable.h>
+#include <babylon/misc/observer.h>
+
+namespace BABYLON {
+
+class Camera;
+class PostProcess;
+class PostProcessManager;
+class RenderTargetTexture;
+using CameraPtr              = std::shared_ptr<Camera>;
+using PostProcessPtr         = std::shared_ptr<PostProcess>;
+using PostProcessManagerPtr  = std::shared_ptr<PostProcessManager>;
+using RenderTargetTexturePtr = std::shared_ptr<RenderTargetTexture>;
+
+/**
+ * @brief This class computes a min/max reduction from a texture: it means it computes the minimum
+ * and maximum values from all values of the texture.
+ * It is performed on the GPU for better performances, thanks to a succession of post processes.
+ * The source values are read from the red channel of the texture.
+ */
+class BABYLON_SHARED_EXPORT MinMaxReducer {
+
+public:
+  /**
+   * @brief Creates a min/max reducer.
+   * @param camera The camera to use for the post processes
+   */
+  MinMaxReducer(const CameraPtr& camera);
+  virtual ~MinMaxReducer(); // = default
+
+  /**
+   * @brief Sets the source texture to read the values from.
+   * One must indicate if the texture is a depth texture or not through the depthRedux parameter
+   * because in such textures '1' value must not be taken into account to compute the maximum
+   * as this value is used to clear the texture.
+   * Note that the computation is not activated by calling this function, you must call activate()
+   * for that!
+   * @param sourceTexture The texture to read the values from. The values should be in the red
+   * channel.
+   * @param depthRedux Indicates if the texture is a depth texture or not
+   * @param type The type of the textures created for the reduction (defaults to
+   * TEXTURETYPE_HALF_FLOAT)
+   * @param forceFullscreenViewport Forces the post processes used for the reduction to be applied
+   * without taking into account viewport (defaults to true)
+   */
+  virtual void setSourceTexture(const RenderTargetTexturePtr& sourceTexture, bool depthRedux,
+                                unsigned int type            = Constants::TEXTURETYPE_HALF_FLOAT,
+                                bool forceFullscreenViewport = true);
+
+  /**
+   * @brief Activates the reduction computation.
+   * When activated, the observers registered in onAfterReductionPerformed are
+   * called after the compuation is performed
+   */
+  virtual void activate();
+
+  /**
+   * @brief Deactivates the reduction computation.
+   */
+  virtual void deactivate();
+
+  /**
+   * @brief Disposes the min/max reducer.
+   * @param disposeAll true to dispose all the resources. You should always call this function with
+   * true as the parameter (or without any parameter as it is the default one). This flag is meant
+   * to be used internally.
+   */
+  virtual void dispose(bool disposeAll = true);
+
+protected:
+  /**
+   * @brief Gets the texture used to read the values from.
+   */
+  RenderTargetTexturePtr& get_sourceTexture();
+
+  /**
+   * @brief Gets the refresh rate of the computation.
+   * Use 0 to compute just once, 1 to compute on every frame, 2 to compute every two frames and so
+   * on...
+   */
+  int get_refreshRate() const;
+
+  /**
+   * @brief Sets the refresh rate of the computation.
+   * Use 0 to compute just once, 1 to compute on every frame, 2 to compute every two frames and so
+   * on...
+   */
+  void set_refreshRate(int value);
+
+  /**
+   * @brief Gets the activation status of the reducer.
+   */
+  bool get_activated() const;
+
+public:
+  /**
+   * Observable triggered when the computation has been performed
+   */
+  Observable<MinMaxFloats> onAfterReductionPerformed;
+
+  /**
+   * Defines the refresh rate of the computation.
+   * Use 0 to compute just once, 1 to compute on every frame, 2 to compute every two frames and so
+   * on...
+   */
+  Property<MinMaxReducer, int> refreshRate;
+
+  /**
+   * Gets the activation status of the reducer
+   */
+  ReadOnlyProperty<MinMaxReducer, bool> activated;
+
+protected:
+  CameraPtr _camera;
+  RenderTargetTexturePtr _sourceTexture;
+  std::vector<PostProcessPtr> _reductionSteps;
+  std::unique_ptr<PostProcessManager> _postProcessManager;
+  Observer<RenderTargetTexture>::Ptr _onAfterUnbindObserver;
+  bool _forceFullscreenViewport;
+  bool _activated;
+
+}; // end of class MinMaxReducer
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MISC_MIN_MAX_REDUCER_H
---
+++ src/BabylonCpp/src/misc/min_max_reducer.cpp
@@ -0,0 +1,217 @@
+#include <babylon/misc/min_max_reducer.h>
+
+#include <babylon/cameras/camera.h>
+#include <babylon/engines/engine.h>
+#include <babylon/engines/scene.h>
+#include <babylon/materials/effect.h>
+#include <babylon/materials/textures/render_target_texture.h>
+#include <babylon/postprocesses/post_process.h>
+#include <babylon/postprocesses/post_process_manager.h>
+
+namespace BABYLON {
+
+MinMaxReducer::MinMaxReducer(const CameraPtr& camera)
+    : refreshRate{this, &MinMaxReducer::get_refreshRate, &MinMaxReducer::set_refreshRate}
+    , activated{this, &MinMaxReducer::get_activated}
+    , _sourceTexture{nullptr}
+    , _postProcessManager{nullptr}
+    , _onAfterUnbindObserver{nullptr}
+    , _forceFullscreenViewport{true}
+    , _activated{false}
+{
+  _camera             = camera;
+  _postProcessManager = std::make_unique<PostProcessManager>(camera->getScene());
+}
+
+MinMaxReducer::~MinMaxReducer() = default;
+
+RenderTargetTexturePtr& MinMaxReducer::get_sourceTexture()
+{
+  return _sourceTexture;
+}
+
+void MinMaxReducer::setSourceTexture(const RenderTargetTexturePtr& sourceTexture, bool depthRedux,
+                                     unsigned int type, bool forceFullscreenViewport)
+{
+  if (sourceTexture == _sourceTexture) {
+    return;
+  }
+
+  dispose(false);
+
+  _sourceTexture           = sourceTexture;
+  _reductionSteps          = {};
+  _forceFullscreenViewport = forceFullscreenViewport;
+
+  auto scene = _camera->getScene();
+
+  // create the first step
+  auto reductionInitial = PostProcess::New(
+    "Initial reduction phase",                                                  // name
+    "minmaxRedux",                                                              // shader
+    {"texSize"},                                                                // parameters
+    {"sourceTexture"},                                                          // textures
+    1.f,                                                                        // options
+    nullptr,                                                                    // camera
+    Constants::TEXTURE_NEAREST_NEAREST,                                         // sampling
+    scene->getEngine(),                                                         // engine
+    false,                                                                      // reusable
+    "#define INITIAL" + std::string(depthRedux ? "\n#define DEPTH_REDUX" : ""), // defines
+    type,                                                                       // textureType
+    "",                                                                         // vertexUrl
+    {},                                                                         // indexParameters
+    false,                                                                      // blockCompilation
+    Constants::TEXTUREFORMAT_RG                                                 // textureFormat
+  );
+
+  reductionInitial->autoClear               = false;
+  reductionInitial->forceFullscreenViewport = forceFullscreenViewport;
+
+  auto w = static_cast<float>(_sourceTexture->getRenderWidth()),
+       h = static_cast<float>(_sourceTexture->getRenderHeight());
+
+  reductionInitial->onApply = [this, w, h](Effect* effect, EventState & /*es*/) -> void {
+    effect->setTexture("sourceTexture", _sourceTexture);
+    effect->setFloatArray2("texSize", {w, h});
+  };
+
+  _reductionSteps.emplace_back(reductionInitial);
+
+  unsigned int index = 1;
+
+  // create the additional steps
+  while (w > 1 || h > 1) {
+    w = std::max(std::round(w / 2.f), 1.f);
+    h = std::max(std::round(h / 2.f), 1.f);
+
+    const auto _w = static_cast<int>(w);
+    const auto _h = static_cast<int>(h);
+
+    auto reduction = PostProcess::New(
+      "Reduction phase " + std::to_string(index), // name
+      "minmaxRedux",                              // shader
+      {"texSize"},                                // parameters
+      {},                                         // textures
+      PostProcessOptions{_w, _h},                 // options
+      nullptr,                                    // camera
+      Constants::TEXTURE_NEAREST_NEAREST,         // sampling
+      scene->getEngine(),                         // engine
+      false,                                      // reusable
+      "#define "
+        + std::string((_w == 1 && _h == 1) ?
+                        "LAST" :
+                        (_w == 1 || _h == 1) ? "ONEBEFORELAST" : "MAIN"), // defines
+      type,                                                               // textureType
+      "",                                                                 // vertexUrl
+      {},                                                                 // indexParameters
+      false,                                                              // blockCompilation
+      Constants::TEXTUREFORMAT_RG                                         // textureFormat
+    );
+
+    reduction->autoClear               = false;
+    reduction->forceFullscreenViewport = forceFullscreenViewport;
+
+    reduction->onApply = [_w, _h](Effect* effect, EventState & /*es*/) -> void {
+      if (_w == 1 || _h == 1) {
+        effect->setIntArray2("texSize", {_w, _h});
+      }
+      else {
+        effect->setFloatArray2("texSize", {static_cast<float>(_w), static_cast<float>(_h)});
+      }
+    };
+
+    _reductionSteps.emplace_back(reduction);
+
+    ++index;
+
+    if (_w == 1 && _h == 1) {
+      const auto func = [this, scene](int w, int h, const PostProcessPtr& reduction) {
+        const auto func
+          = [this, scene, reduction, w, h](Effect* /*effect*/, EventState /*es*/) -> void {
+          Float32Array buffer(4 * static_cast<size_t>(w * h));
+          MinMaxFloats minmax{0.f, 0.f};
+          scene->getEngine()->_readTexturePixels(reduction->inputTexture, w, h, -1, 0, buffer);
+          minmax.min = buffer[0];
+          minmax.max = buffer[1];
+          onAfterReductionPerformed.notifyObservers(&minmax);
+        };
+        return func;
+      };
+      reduction->onAfterRenderObservable.add(func(_w, _h, reduction));
+    }
+  }
+}
+
+int MinMaxReducer::get_refreshRate() const
+{
+  return _sourceTexture ? _sourceTexture->refreshRate() : -1;
+}
+
+void MinMaxReducer::set_refreshRate(int value)
+{
+  if (_sourceTexture) {
+    _sourceTexture->refreshRate = value;
+  }
+}
+
+bool MinMaxReducer::get_activated() const
+{
+  return _activated;
+}
+
+void MinMaxReducer::activate()
+{
+  if (_onAfterUnbindObserver || !_sourceTexture) {
+    return;
+  }
+
+  _onAfterUnbindObserver = _sourceTexture->onAfterUnbindObservable.add(
+    [this](RenderTargetTexture* /*texture*/, EventState & /*es*/) -> void {
+      if (_reductionSteps.empty() || !_reductionSteps[0] || !_postProcessManager || !_camera) {
+        return;
+      }
+
+      _reductionSteps[0]->activate(_camera);
+      _postProcessManager->directRender(_reductionSteps, _reductionSteps[0]->inputTexture(),
+                                        _forceFullscreenViewport);
+      _camera->getScene()->getEngine()->unBindFramebuffer(_reductionSteps[0]->inputTexture(),
+                                                          false);
+    });
+
+  _activated = true;
+}
+
+void MinMaxReducer::deactivate()
+{
+  if (!_onAfterUnbindObserver || !_sourceTexture) {
+    return;
+  }
+
+  _sourceTexture->onAfterUnbindObservable.remove(_onAfterUnbindObserver);
+  _onAfterUnbindObserver = nullptr;
+  _activated             = false;
+}
+
+void MinMaxReducer::dispose(bool disposeAll)
+{
+  if (disposeAll) {
+    onAfterReductionPerformed.clear();
+  }
+
+  deactivate();
+
+  if (!_reductionSteps.empty()) {
+    for (const auto& reductionStep : _reductionSteps) {
+      reductionStep->dispose();
+    }
+    _reductionSteps.clear();
+  }
+
+  if (_postProcessManager && disposeAll) {
+    _postProcessManager->dispose();
+  }
+
+  _sourceTexture = nullptr;
+}
+
+} // end of namespace BABYLON
