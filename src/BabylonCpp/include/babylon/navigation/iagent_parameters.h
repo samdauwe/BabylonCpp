f91724ab44c9e43b0114bf0f093744b52b9644ea
samdauwe <sam.dauwe@gmail.com>
2020-03-01 17:13:28 +0100

    Setting default IAgentParameters properties to invalid values

--- src/BabylonCpp/include/babylon/navigation/iagent_parameters.h
+++ src/BabylonCpp/include/babylon/navigation/iagent_parameters.h
@@ -10,38 +10,38 @@ struct IAgentParameters {
   /**
    *  Agent radius. [Limit: >= 0]
    */
-  float radius;
+  float radius = -1.f;
 
   /**
    * Agent height. [Limit: > 0]
    */
-  float height;
+  float height = 0.f;
 
   /**
    *  Maximum allowed acceleration. [Limit: >= 0]
    */
-  float maxAcceleration;
+  float maxAcceleration = -1.f;
 
   /**
    * Maximum allowed speed. [Limit: >= 0]
    */
-  float maxSpeed;
+  float maxSpeed = -1.f;
 
   /**
    * Defines how close a collision element must be before it is considered for steering behaviors.
    * [Limits: > 0]
    */
-  float collisionQueryRange;
+  float collisionQueryRange = 0.f;
 
   /**
    * The path visibility optimization range. [Limit: > 0]
    */
-  float pathOptimizationRange;
+  float pathOptimizationRange = 0.f;
 
   /**
    * How aggresive the agent manager should be at avoiding collisions with this agent. [Limit: >= 0]
    */
-  float separationWeight;
+  float separationWeight = -1.f;
 }; // end of struct IAgentParameters
 
 } // end of namespace BABYLON
