7fb107a3c104b1dbc12f739d4aee18bae2ae2684
samdauwe <sam.dauwe@gmail.com>
2020-04-11 20:55:55 +0200

    Updating random id generator

--- src/BabylonCpp/src/misc/guid.cpp
+++ src/BabylonCpp/src/misc/guid.cpp
@@ -13,14 +13,14 @@ std::string GUID::RandomId()
   std::string randomId = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
   const std::array<unsigned int, 16> yconv{
     {8, 9, 10, 11, 8, 9, 10, 11, 8, 9, 10, 11, 8, 9, 10, 11}};
-  const std::array<char, 16> hex{{'0', '1', '2', '3', '4', '5', '6', '7', '8',
-                                  '9', 'a', 'b', 'c', 'd', 'e', 'f'}};
+  const std::array<char, 16> hex{
+    {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'}};
   const auto randomFloats = Math::randomList(0.f, 1.f, randomId.size());
   for (unsigned int i = 0; i < randomId.size(); ++i) {
-    const char c = randomId[i];
+    const auto c = randomId[i];
     if (c == 'x' || c == 'y') {
-      const auto r         = static_cast<unsigned int>(randomFloats[i] * 16);
-      randomId[i]          = (c == 'x') ? hex[r] : hex[yconv[r]];
+      const auto r = static_cast<unsigned int>(randomFloats[i] * 16);
+      randomId[i]  = (c == 'x') ? hex[r] : hex[yconv[r]];
     }
   }
   return randomId;
