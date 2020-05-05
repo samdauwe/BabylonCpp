624acdd67fe98378d26c141d80d678b3ca12ad91
samdauwe <sam.dauwe@gmail.com>
2020-05-05 22:14:48 +0200

    Formatted SimplePineGenerator class

--- src/Extensions/src/extensions/treegenerators/simple_pine_generator.cpp
+++ src/Extensions/src/extensions/treegenerators/simple_pine_generator.cpp
@@ -7,11 +7,9 @@
 namespace BABYLON {
 namespace Extensions {
 
-MeshPtr SimplePineGenerator::CreateTree(Scene* scene,
-                                        const MaterialPtr& trunkMaterial,
-                                        const MaterialPtr& leafMaterial,
-                                        unsigned int canopies, float baseRadius,
-                                        float height, unsigned int tessellation,
+MeshPtr SimplePineGenerator::CreateTree(Scene* scene, const MaterialPtr& trunkMaterial,
+                                        const MaterialPtr& leafMaterial, unsigned int canopies,
+                                        float baseRadius, float height, unsigned int tessellation,
                                         float twist)
 {
   if (twist < 0.f || twist > 1.f) {
@@ -27,10 +25,10 @@ MeshPtr SimplePineGenerator::CreateTree(Scene* scene,
         path.emplace_back(Vector3(0.f, i, 0.f));
       }
       else {
-        path.emplace_back(Vector3(Math::randomNumber(-twist, twist), i,
-                                  Math::randomNumber(-twist, twist)));
-        path.emplace_back(Vector3(Math::randomNumber(-twist, twist), i,
-                                  Math::randomNumber(-twist, twist)));
+        path.emplace_back(
+          Vector3(Math::randomNumber(-twist, twist), i, Math::randomNumber(-twist, twist)));
+        path.emplace_back(
+          Vector3(Math::randomNumber(-twist, twist), i, Math::randomNumber(-twist, twist)));
       }
     }
     return path;
@@ -49,13 +47,12 @@ MeshPtr SimplePineGenerator::CreateTree(Scene* scene,
     return (nbL * 2.f - i - 1.f) * fact;
   };
 
-  auto leaves = Mesh::CreateTube("leaves", curve, 0.f, tessellation,
-                                 radiusFunction, Mesh::CAP_START, scene);
+  auto leaves
+    = Mesh::CreateTube("leaves", curve, 0.f, tessellation, radiusFunction, Mesh::CAP_START, scene);
   // leaves->convertToFlatShadedMesh();
 
-  auto trunk
-    = Mesh::CreateCylinder("trunk", height / nbL, nbL * 1.5f - nbL / 2.f - 1.f,
-                           nbL * 1.5f - nbL / 2.f - 1.f, 12, 1, scene);
+  auto trunk = Mesh::CreateCylinder("trunk", height / nbL, nbL * 1.5f - nbL / 2.f - 1.f,
+                                    nbL * 1.5f - nbL / 2.f - 1.f, 12, 1, scene);
   // trunk->convertToFlatShadedMesh();
 
   leaves->material = leafMaterial;
