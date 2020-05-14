e846c6c8b7e07d4c11b65e45cb0f8b7789330af4
samdauwe <sam.dauwe@gmail.com>
2020-05-14 21:22:47 +0200

    Formatted noise generation extension

--- src/Extensions/include/babylon/extensions/noisegeneration/simplex_noise.h
+++ src/Extensions/include/babylon/extensions/noisegeneration/simplex_noise.h
@@ -34,30 +34,28 @@ public:
 
 public:
   /*
-   * Gradient tables. These could be programmed the Ken Perlin way with
-   * some clever bit-twiddling, but this is more clear, and not really slower.
+   * Gradient tables. These could be programmed the Ken Perlin way with some clever bit-twiddling,
+   * but this is more clear, and not really slower.
    */
   static std::array<std::array<float, 2>, 8> grad2lut;
 
   /*
    * Gradient directions for 3D.
    * These vectors are based on the midpoints of the 12 edges of a cube.
-   * A larger array of random unit length vectors would also do the job,
-   * but these 12 (including 4 repeats to make the array length a power
-   * of two) work better. They are not random, they are carefully chosen
-   * to represent a small, isotropic set of directions.
+   * A larger array of random unit length vectors would also do the job, but these 12 (including 4
+   * repeats to make the array length a power of two) work better. They are not random, they are
+   * carefully chosen to represent a small, isotropic set of directions.
    */
   static std::array<std::array<float, 3>, 16> grad3lut;
   static std::array<std::array<float, 4>, 32> grad4lut;
 
   /*
    * For 3D, we define two orthogonal vectors in the desired rotation plane.
-   * These vectors are based on the midpoints of the 12 edges of a cube,
-   * they all rotate in their own plane and are never coincident or collinear.
-   * A larger array of random vectors would also do the job, but these 12
-   * (including 4 repeats to make the array length a power of two) work better.
-   * They are not random, they are carefully chosen to represent a small
-   * isotropic set of directions for any rotation angle.
+   * These vectors are based on the midpoints of the 12 edges of a cube, they all rotate in their
+   * own plane and are never coincident or collinear. A larger array of random vectors would also do
+   * the job, but these 12 (including 4 repeats to make the array length a power of two) work
+   * better. They are not random, they are carefully chosen to represent a small isotropic set of
+   * directions for any rotation angle.
    */
   static constexpr float a = 0.81649658f; // a = sqrt(2)/sqrt(3) = 0.816496580
   static std::array<std::array<float, 3>, 16> grad3u;
@@ -87,7 +85,7 @@ public:
   SimplexNoise();
   ~SimplexNoise(); // = default
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Returns a 1D simplex noise.
@@ -109,7 +107,7 @@ public:
    */
   float noise(const Vector4& v);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   template <typename T>
   inline float ridgedNoise_t(const T& input)
@@ -137,7 +135,7 @@ public:
    */
   float ridgedNoise(const Vector4& v);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Returns a 1D simplex noise with analytical derivative.
@@ -159,7 +157,7 @@ public:
    */
   vec5 dnoise(const Vector4& v);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Returns a 2D simplex noise with rotating gradients.
@@ -171,7 +169,7 @@ public:
    */
   float flowNoise(const Vector3& v, float angle);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Returns a 2D simplex noise with rotating gradients and analytical
@@ -185,7 +183,7 @@ public:
    */
   Vector4 dFlowNoise(const Vector3& v, float angle);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Returns the curl of a 2D simplex noise.
@@ -218,7 +216,7 @@ public:
    */
   Vector3 curlNoise(const Vector3& v, uint8_t octaves, float lacunarity, float gain);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Returns the curl of a custom 2D potential using finite difference
@@ -234,7 +232,7 @@ public:
   Vector3 curl(const Vector3& v, const std::function<Vector3(const Vector3&)>& potential,
                float delta = 1e-4f);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   template <typename T>
   float fBm_t(const T& input, uint8_t octaves, float lacunarity, float gain)
@@ -273,7 +271,7 @@ public:
    */
   float fBm(const Vector4& v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Returns a 1D simplex noise fractal brownian motion sum with
@@ -299,7 +297,7 @@ public:
    */
   vec5 dfBm(const Vector4& v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   float ridge(float h, float offset)
   {
@@ -349,7 +347,7 @@ public:
   float ridgedMF(const Vector4& v, float ridgeOffset = 1.0f, uint8_t octaves = 4,
                  float lacunarity = 2.0f, float gain = 0.5f);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Returns the 2D simplex noise fractal brownian motion sum variation
@@ -363,7 +361,7 @@ public:
    */
   float iqfBm(const Vector3& v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f);
 
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
 
   /**
    * @brief Seeds the permutation table with new random values.
@@ -373,15 +371,13 @@ public:
 private:
   /*
    * Helper functions to compute gradients-dot-residualvectors (1D to 4D)
-   * Note that these generate gradients of more than unit length. To make
-   * a close match with the value range of classic Perlin noise, the final
-   * noise values need to be rescaled to fit nicely within [-1,1].
-   * (The simplex noise functions as such also have different scaling.)
-   * Note also that these noise functions are the most practical and useful
-   * signed version of Perlin noise. To return values according to the
-   * RenderMan specification from the SL noise() and pnoise() functions,
-   * the noise values need to be scaled and offset to [0,1], like this:
-   * float SLnoise = (SimplexNoise1234::noise(x,y,z) + 1.0) * 0.5;
+   * Note that these generate gradients of more than unit length. To make a close match with the
+   * value range of classic Perlin noise, the final noise values need to be rescaled to fit nicely
+   * within [-1,1]. (The simplex noise functions as such also have different scaling.) Note also
+   * that these noise functions are the most practical and useful signed version of Perlin noise. To
+   * return values according to the RenderMan specification from the SL noise() and pnoise()
+   * functions, the noise values need to be scaled and offset to [0,1], like this: float SLnoise =
+   * (SimplexNoise1234::noise(x,y,z) + 1.0) * 0.5;
    */
   [[nodiscard]] float grad(int hash, float x) const;
   [[nodiscard]] float grad(int hash, float x, float y) const;
@@ -389,8 +385,8 @@ private:
   [[nodiscard]] float grad(int hash, float x, float y, float z, float t) const;
 
   /*
-   * Helper functions to compute gradients in 1D to 4D
-   * and gradients-dot-residualvectors in 2D to 4D.
+   * Helper functions to compute gradients in 1D to 4D and gradients-dot-residualvectors in 2D to
+   * 4D.
    */
   void grad1(int hash, float* gx);
   void grad2(int hash, float* gx, float* gy);
@@ -398,8 +394,7 @@ private:
   void grad4(int hash, float* gx, float* gy, float* gz, float* gw);
 
   /*
-   * Helper functions to compute rotated gradients and
-   * gradients-dot-residualvectors in 2D and 3D.
+   * Helper functions to compute rotated gradients and gradients-dot-residualvectors in 2D and 3D.
    */
   void gradrot2(int hash, float sin_t, float cos_t, float* gx, float* gy);
   void gradrot3(int hash, float sin_t, float cos_t, float* gx, float* gy, float* gz);
@@ -408,21 +403,18 @@ private:
 
 private:
   /*
-   * Permutation table. This is just a random jumble of all numbers 0-255,
-   * repeated twice to avoid wrapping the index at 255 for each lookup.
-   * This needs to be exactly the same for all instances on all platforms,
-   * so it's easiest to just keep it as static explicit data.
-   * This also removes the need for any initialisation of this class.
+   * Permutation table. This is just a random jumble of all numbers 0-255, repeated twice to avoid
+   * wrapping the index at 255 for each lookup. This needs to be exactly the same for all instances
+   * on all platforms, so it's easiest to just keep it as static explicit data. This also removes
+   * the need for any initialisation of this class.
    *
-   * Note that making this an int[] instead of a char[] might make the
-   * code run faster on platforms with a high penalty for unaligned single
-   * byte addressing. Intel x86 is generally single-byte-friendly, but
-   * some other CPUs are faster with 4-aligned reads.
-   * However, a char[] is smaller, which avoids cache trashing, and that
-   * is probably the most important aspect on most architectures.
-   * This array is accessed a *lot* by the noise functions.
-   * A vector-valued noise over 3D accesses it 96 times, and a
-   * float-valued 4D noise 64 times. We want this to fit in the cache!
+   * Note that making this an int[] instead of a char[] might make the code run faster on platforms
+   * with a high penalty for unaligned single byte addressing. Intel x86 is generally
+   * single-byte-friendly, but some other CPUs are faster with 4-aligned reads. However, a char[] is
+   * smaller, which avoids cache trashing, and that is probably the most important aspect on most
+   * architectures. This array is accessed a *lot* by the noise functions. A vector-valued noise
+   * over 3D accesses it 96 times, and a float-valued 4D noise 64 times. We want this to fit in the
+   * cache!
    */
   std::array<unsigned char, 512> perm;
 
