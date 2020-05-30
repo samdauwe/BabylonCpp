81cb9c0dc0bf71c3c5ea09c2fbf58f9de52132ab
samdauwe <sam.dauwe@gmail.com>
2020-05-30 20:26:08 +0200

    Formatted RawTexture3D class

--- src/BabylonCpp/src/materials/textures/raw_texture_3d.cpp
+++ src/BabylonCpp/src/materials/textures/raw_texture_3d.cpp
@@ -6,11 +6,9 @@
 
 namespace BABYLON {
 
-RawTexture3D::RawTexture3D(const ArrayBufferView& data, int width, int height,
-                           int depth, unsigned int iFormat, Scene* scene,
-                           bool generateMipMaps, bool iInvertY,
-                           unsigned int iSamplingMode,
-                           unsigned int iTextureType)
+RawTexture3D::RawTexture3D(const ArrayBufferView& data, int width, int height, int depth,
+                           unsigned int iFormat, Scene* scene, bool generateMipMaps, bool iInvertY,
+                           unsigned int iSamplingMode, unsigned int iTextureType)
     : Texture{nullptr, scene, !generateMipMaps, iInvertY}, format{iFormat}
 {
   _engine = scene->getEngine();
@@ -37,8 +35,8 @@ void RawTexture3D::update(const ArrayBufferView& data)
   if (!_texture) {
     return;
   }
-  _engine->updateRawTexture3D(_texture, data, _texture->format,
-                              _texture->invertY, "", _texture->type);
+  _engine->updateRawTexture3D(_texture, data, _texture->format, _texture->invertY, "",
+                              _texture->type);
 }
 
 } // end of namespace BABYLON
