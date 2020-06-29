13eb226f553b53a2fc3235ebddc4f20f03889847
samdauwe <sam.dauwe@gmail.com>
2020-06-29 20:19:00 +0200

    Updated comment

--- src/BabylonCpp/include/babylon/misc/guid.h
+++ src/BabylonCpp/include/babylon/misc/guid.h
@@ -16,9 +16,8 @@ struct BABYLON_SHARED_EXPORT GUID {
    * @brief Implementation from
    * http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#answer-2117523
    * Be aware Math.random() could cause collisions, but:
-   * "All but 6 of the 128 bits of the ID are randomly generated, which means
-   * that for any two ids, there's a 1 in 2^^122 (or 5.3x10^^36) chance they'll
-   * collide"
+   * "All but 6 of the 128 bits of the ID are randomly generated, which means that for any two ids,
+   * there's a 1 in 2^^122 (or 5.3x10^^36) chance they'll collide"
    * @returns a pseudo random id
    */
   static std::string RandomId();
