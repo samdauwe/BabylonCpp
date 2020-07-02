0a34116e9a0fe8b66dac9b3db201585201f95117
samdauwe <sam.dauwe@gmail.com>
2020-07-02 21:02:05 +0200

    Implemented ParticleTextureBlock class

---
+++ src/BabylonCpp/include/babylon/materials/node/blocks/particle/particle_texture_block.h
@@ -0,0 +1,192 @@
+#ifndef BABYLON_MATERIALS_NODE_BLOCKS_PARTICLE_PARTICLE_TEXTURE_BLOCK_H
+#define BABYLON_MATERIALS_NODE_BLOCKS_PARTICLE_PARTICLE_TEXTURE_BLOCK_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/materials/node/node_material_block.h>
+
+namespace BABYLON {
+
+class BaseTexture;
+class ParticleTextureBlock;
+using BaseTexturePtr          = std::shared_ptr<BaseTexture>;
+using ParticleTextureBlockPtr = std::shared_ptr<ParticleTextureBlock>;
+
+/**
+ * @brief Base block used for the particle texture.
+ */
+class BABYLON_SHARED_EXPORT ParticleTextureBlock : public NodeMaterialBlock {
+
+public:
+  template <typename... Ts>
+  static ParticleTextureBlockPtr New(Ts&&... args)
+  {
+    return std::shared_ptr<ParticleTextureBlock>(
+      new ParticleTextureBlock(std::forward<Ts>(args)...));
+  }
+  ~ParticleTextureBlock() override;
+
+  /**
+   * @brief Gets the current class name.
+   * @returns the class name
+   */
+  std::string getClassName() const override;
+
+  /**
+   * @brief Initialize the block and prepare the context for build.
+   * @param state defines the state that will be used for the build
+   */
+  void initialize(NodeMaterialBuildState& state) override;
+
+  /**
+   * @brief Lets the block try to connect some inputs automatically.
+   * @param material defines the hosting NodeMaterial
+   */
+  void autoConfigure(const NodeMaterialPtr& nodeMaterial) override;
+
+  /**
+   * @brief Update defines for shader compilation.
+   * @param mesh defines the mesh to be rendered
+   * @param nodeMaterial defines the node material requesting the update
+   * @param defines defines the material defines to update
+   * @param useInstances specifies that instances should be used
+   */
+  void prepareDefines(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+                      NodeMaterialDefines& defines, bool useInstances = false,
+                      const SubMeshPtr& subMesh = nullptr) override;
+
+  /**
+   * @brief Checks if the block is ready.
+   * @param mesh defines the mesh to be rendered
+   * @param nodeMaterial defines the node material requesting the update
+   * @param defines defines the material defines to update
+   * @param useInstances specifies that instances should be used
+   * @returns true if the block is ready
+   */
+  bool isReady(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+               const NodeMaterialDefines& defines, bool useInstances = false) override;
+
+  /**
+   * @brief Serializes this block in a JSON representation.
+   * @returns the serialized block object
+   */
+  json serialize() const override;
+
+  /**
+   * @brief Hidden
+   */
+  void _deserialize(const json& serializationObject, Scene* scene,
+                    const std::string& rootUrl) override;
+
+protected:
+  /**
+   * @brief Creates a new ParticleTextureBlock.
+   * @param name defines the block name
+   */
+  ParticleTextureBlock(const std::string& name);
+
+  /**
+   * @brief Gets the uv input component.
+   */
+  NodeMaterialConnectionPointPtr& get_uv();
+
+  /**
+   * @brief Gets the rgba output component.
+   */
+  NodeMaterialConnectionPointPtr& get_rgba();
+
+  /**
+   * @brief Gets the rgb output component.
+   */
+  NodeMaterialConnectionPointPtr& get_rgb();
+
+  /**
+   * @brief Gets the r output component.
+   */
+  NodeMaterialConnectionPointPtr& get_r();
+
+  /**
+   * @brief Gets the g output component.
+   */
+  NodeMaterialConnectionPointPtr& get_g();
+
+  /**
+   * @brief Gets the b output component.
+   */
+  NodeMaterialConnectionPointPtr& get_b();
+
+  /**
+   * @brief Gets the a output component.
+   */
+  NodeMaterialConnectionPointPtr& get_a();
+
+  /**
+   * @brief Hidden
+   */
+  ParticleTextureBlock& _buildBlock(NodeMaterialBuildState& state) override;
+
+private:
+  void _writeOutput(NodeMaterialBuildState& state, const NodeMaterialConnectionPointPtr& output,
+                    const std::string& swizzle);
+
+public:
+  /**
+   * Gets or sets the texture associated with the node
+   */
+  BaseTexturePtr texture;
+
+  /**
+   * Gets or sets a boolean indicating if content needs to be converted to gamma space
+   */
+  bool convertToGammaSpace;
+
+  /**
+   * Gets or sets a boolean indicating if content needs to be converted to linear space
+   */
+  bool convertToLinearSpace;
+
+  /**
+   * Gets the uv input component
+   */
+  ReadOnlyProperty<ParticleTextureBlock, NodeMaterialConnectionPointPtr> uv;
+
+  /**
+   * Gets the rgba output component
+   */
+  ReadOnlyProperty<ParticleTextureBlock, NodeMaterialConnectionPointPtr> rgba;
+
+  /**
+   * Gets the rgb output component
+   */
+  ReadOnlyProperty<ParticleTextureBlock, NodeMaterialConnectionPointPtr> rgb;
+
+  /**
+   * Gets the r output component
+   */
+  ReadOnlyProperty<ParticleTextureBlock, NodeMaterialConnectionPointPtr> r;
+
+  /**
+   * Gets the g output component
+   */
+  ReadOnlyProperty<ParticleTextureBlock, NodeMaterialConnectionPointPtr> g;
+
+  /**
+   * Gets the b output component
+   */
+  ReadOnlyProperty<ParticleTextureBlock, NodeMaterialConnectionPointPtr> b;
+
+  /**
+   * Gets the a output component
+   */
+  ReadOnlyProperty<ParticleTextureBlock, NodeMaterialConnectionPointPtr> a;
+
+private:
+  std::string _samplerName;
+  std::string _linearDefineName;
+  std::string _gammaDefineName;
+  std::string _tempTextureRead;
+
+}; // end of class ParticleTextureBlock
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_NODE_BLOCKS_PARTICLE_PARTICLE_TEXTURE_BLOCK_H
---
+++ src/BabylonCpp/src/materials/node/blocks/particle/particle_texture_block.cpp
@@ -0,0 +1,202 @@
+#include <babylon/materials/node/blocks/particle/particle_texture_block.h>
+
+#include <babylon/core/json_util.h>
+#include <babylon/materials/node/blocks/input/input_block.h>
+#include <babylon/materials/node/enums/node_material_block_connection_point_types.h>
+#include <babylon/materials/node/enums/node_material_block_targets.h>
+#include <babylon/materials/node/node_material.h>
+#include <babylon/materials/node/node_material_build_state.h>
+#include <babylon/materials/node/node_material_build_state_shared_data.h>
+#include <babylon/materials/node/node_material_connection_point.h>
+#include <babylon/materials/node/node_material_defines.h>
+#include <babylon/materials/textures/base_texture.h>
+#include <babylon/misc/string_tools.h>
+
+namespace BABYLON {
+
+ParticleTextureBlock::ParticleTextureBlock(const std::string& iName)
+    : NodeMaterialBlock{iName, NodeMaterialBlockTargets::Fragment}
+    , texture{nullptr}
+    , convertToGammaSpace{false}
+    , convertToLinearSpace{false}
+    , uv{this, &ParticleTextureBlock::get_uv}
+    , rgba{this, &ParticleTextureBlock::get_rgba}
+    , rgb{this, &ParticleTextureBlock::get_rgb}
+    , r{this, &ParticleTextureBlock::get_r}
+    , g{this, &ParticleTextureBlock::get_g}
+    , b{this, &ParticleTextureBlock::get_b}
+    , a{this, &ParticleTextureBlock::get_a}
+    , _samplerName{"diffuseSampler"}
+{
+  _isUnique = true;
+
+  registerInput("uv", NodeMaterialBlockConnectionPointTypes::Vector2, false,
+                NodeMaterialBlockTargets::VertexAndFragment);
+
+  registerOutput("rgba", NodeMaterialBlockConnectionPointTypes::Color4,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("rgb", NodeMaterialBlockConnectionPointTypes::Color3,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("r", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("g", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("b", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("a", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Neutral);
+
+  _inputs[0]->acceptedConnectionPointTypes.emplace_back(
+    NodeMaterialBlockConnectionPointTypes::Vector3);
+  _inputs[0]->acceptedConnectionPointTypes.emplace_back(
+    NodeMaterialBlockConnectionPointTypes::Vector4);
+}
+
+ParticleTextureBlock::~ParticleTextureBlock() = default;
+
+std::string ParticleTextureBlock::getClassName() const
+{
+  return "ParticleTextureBlock";
+}
+
+NodeMaterialConnectionPointPtr& ParticleTextureBlock::ParticleTextureBlock::get_uv()
+{
+  return _inputs[0];
+}
+
+NodeMaterialConnectionPointPtr& ParticleTextureBlock::ParticleTextureBlock::get_rgba()
+{
+  return _outputs[0];
+}
+
+NodeMaterialConnectionPointPtr& ParticleTextureBlock::ParticleTextureBlock::get_rgb()
+{
+  return _outputs[1];
+}
+
+NodeMaterialConnectionPointPtr& ParticleTextureBlock::ParticleTextureBlock::get_r()
+{
+  return _outputs[2];
+}
+
+NodeMaterialConnectionPointPtr& ParticleTextureBlock::ParticleTextureBlock::get_g()
+{
+  return _outputs[3];
+}
+
+NodeMaterialConnectionPointPtr& ParticleTextureBlock::ParticleTextureBlock::get_b()
+{
+  return _outputs[4];
+}
+
+NodeMaterialConnectionPointPtr& ParticleTextureBlock::ParticleTextureBlock::get_a()
+{
+  return _outputs[5];
+}
+
+void ParticleTextureBlock::initialize(NodeMaterialBuildState& state)
+{
+  state._excludeVariableName("diffuseSampler");
+}
+
+void ParticleTextureBlock::autoConfigure(const NodeMaterialPtr& material)
+{
+  if (!uv()->isConnected()) {
+    auto uvInput = material->getInputBlockByPredicate(
+      [](const InputBlockPtr& b) -> bool { return b->isAttribute() && b->name == "particle_uv"; });
+
+    if (!uvInput) {
+      uvInput = InputBlock::New("uv");
+      uvInput->setAsAttribute("particle_uv");
+    }
+    uvInput->output()->connectTo(uv);
+  }
+}
+
+void ParticleTextureBlock::prepareDefines(AbstractMesh* /*mesh*/,
+                                          const NodeMaterialPtr& /*nodeMaterial*/,
+                                          NodeMaterialDefines& defines, bool /*useInstances*/,
+                                          const SubMeshPtr& /*subMesh*/)
+{
+  defines.setValue(_linearDefineName, convertToGammaSpace, true);
+  defines.setValue(_gammaDefineName, convertToLinearSpace, true);
+}
+
+bool ParticleTextureBlock::isReady(AbstractMesh* /*mesh*/, const NodeMaterialPtr& /*nodeMaterial*/,
+                                   const NodeMaterialDefines& /*defines*/, bool /*useInstances*/)
+{
+  if (texture && !texture->isReadyOrNotBlocking()) {
+    return false;
+  }
+
+  return true;
+}
+
+void ParticleTextureBlock::_writeOutput(NodeMaterialBuildState& state,
+                                        const NodeMaterialConnectionPointPtr& output,
+                                        const std::string& swizzle)
+{
+  state.compilationString
+    += StringTools::printf("%s = %s.%s;\r\n", _declareOutput(output, state).c_str(),
+                           _tempTextureRead.c_str(), swizzle.c_str());
+
+  state.compilationString += StringTools::printf("#ifdef %s\r\n", _linearDefineName.c_str());
+  state.compilationString
+    += StringTools::printf("%s = toGammaSpace(%s);\r\n", output->associatedVariableName().c_str(),
+                           output->associatedVariableName().c_str());
+  state.compilationString += "#endif\r\n";
+
+  state.compilationString += StringTools::printf("#ifdef %s\r\n", _gammaDefineName.c_str());
+  state.compilationString
+    += StringTools::printf("%s = toLinearSpace(%s);\r\n", output->associatedVariableName().c_str(),
+                           output->associatedVariableName().c_str());
+  state.compilationString += "#endif\r\n";
+}
+
+ParticleTextureBlock& ParticleTextureBlock::_buildBlock(NodeMaterialBuildState& state)
+{
+  NodeMaterialBlock::_buildBlock(state);
+
+  if (state.target == NodeMaterialBlockTargets::Vertex) {
+    return *this;
+  }
+
+  _tempTextureRead = state._getFreeVariableName("tempTextureRead");
+
+  state._emit2DSampler(_samplerName);
+
+  state.sharedData->blockingBlocks.emplace_back(shared_from_this());
+  state.sharedData->textureBlocks.emplace_back(
+    std::static_pointer_cast<ParticleTextureBlock>(shared_from_this()));
+  state.sharedData->blocksWithDefines.emplace_back(shared_from_this());
+
+  _linearDefineName = state._getFreeDefineName("ISLINEAR");
+  _gammaDefineName  = state._getFreeDefineName("ISGAMMA");
+
+  const auto comments = StringTools::printf("//%s", name.c_str());
+  state._emitFunctionFromInclude("helperFunctions", comments);
+
+  state.compilationString
+    += StringTools::printf("vec4 %s = texture2D(%s, %s);\r\n", _tempTextureRead.c_str(),
+                           _samplerName.c_str(), uv()->associatedVariableName().c_str());
+
+  for (const auto& output : _outputs) {
+    if (output->hasEndpoints()) {
+      _writeOutput(state, output, output->name);
+    }
+  }
+
+  return *this;
+}
+
+json ParticleTextureBlock::serialize() const
+{
+  return nullptr;
+}
+
+void ParticleTextureBlock::_deserialize(const json& /*serializationObject*/, Scene* /*scene*/,
+                                        const std::string& /*rootUrl*/)
+{
+}
+
+} // end of namespace BABYLON
