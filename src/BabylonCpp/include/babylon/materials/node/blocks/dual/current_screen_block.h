18c3835e94ab7d9cc09411b2ce07bf1bde6da799
samdauwe <sam.dauwe@gmail.com>
2020-07-04 20:47:15 +0200

    Implemented CurrentScreenBlock class

---
+++ src/BabylonCpp/include/babylon/materials/node/blocks/dual/current_screen_block.h
@@ -0,0 +1,194 @@
+#ifndef BABYLON_MATERIALS_NODE_BLOCKS_DUAL_CURRENT_SHEEN_BLOCK_H
+#define BABYLON_MATERIALS_NODE_BLOCKS_DUAL_CURRENT_SHEEN_BLOCK_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/materials/node/node_material_block.h>
+
+namespace BABYLON {
+
+class BaseTexture;
+class CurrentScreenBlock;
+using BaseTexturePtr        = std::shared_ptr<BaseTexture>;
+using CurrentScreenBlockPtr = std::shared_ptr<CurrentScreenBlock>;
+
+/**
+ * @brief Base block used as input for post process.
+ */
+class BABYLON_SHARED_EXPORT CurrentScreenBlock : public NodeMaterialBlock {
+
+public:
+  template <typename... Ts>
+  static CurrentScreenBlockPtr New(Ts&&... args)
+  {
+    return std::shared_ptr<CurrentScreenBlock>(new CurrentScreenBlock(std::forward<Ts>(args)...));
+  }
+  ~CurrentScreenBlock() override;
+
+  /**
+   * @brief Gets the current class name.
+   * @returns the class name
+   */
+  std::string getClassName() const override;
+
+  /**
+   * @brief Initialize the block and prepare the context for build.
+   * @param state defines the state that will be used for the build
+   */
+  void initialize(NodeMaterialBuildState& state) override;
+
+  /**
+   * @brief Update defines for shader compilation.
+   * @param mesh defines the mesh to be rendered
+   * @param nodeMaterial defines the node material requesting the update
+   * @param defines defines the material defines to update
+   * @param useInstances specifies that instances should be used
+   */
+  void prepareDefines(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+                      NodeMaterialDefines& defines, bool useInstances = false,
+                      const SubMeshPtr& subMesh = nullptr) override;
+
+  /**
+   * @brief Checks if the block is ready.
+   * @param mesh defines the mesh to be rendered
+   * @param nodeMaterial defines the node material requesting the update
+   * @param defines defines the material defines to update
+   * @param useInstances specifies that instances should be used
+   * @returns true if the block is ready
+   */
+  bool isReady(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+               const NodeMaterialDefines& defines, bool useInstances = false) override;
+
+  /**
+   * @brief Serializes this block in a JSON representation.
+   * @returns the serialized block object
+   */
+  json serialize() const override;
+
+  /**
+   * @brief Hidden
+   */
+  void _deserialize(const json& serializationObject, Scene* scene,
+                    const std::string& rootUrl) override;
+
+protected:
+  /**
+   * @brief Create a new CurrentScreenBlock.
+   * @param name defines the block name
+   */
+  CurrentScreenBlock(const std::string& name);
+
+  /**
+   * @brief Gets the uv input component.
+   */
+  NodeMaterialConnectionPointPtr& get_uv();
+
+  /**
+   * @brief Gets the rgba output component.
+   */
+  NodeMaterialConnectionPointPtr& get_rgba();
+
+  /**
+   * @brief Gets the rgb output component.
+   */
+  NodeMaterialConnectionPointPtr& get_rgb();
+
+  /**
+   * @brief Gets the r output component.
+   */
+  NodeMaterialConnectionPointPtr& get_r();
+
+  /**
+   * @brief Gets the g output component.
+   */
+  NodeMaterialConnectionPointPtr& get_g();
+
+  /**
+   * @brief Gets the b output component.
+   */
+  NodeMaterialConnectionPointPtr& get_b();
+
+  /**
+   * @brief Gets the a output component.
+   */
+  NodeMaterialConnectionPointPtr& get_a();
+
+  /**
+   * @brief Gets the target of the block.
+   */
+  NodeMaterialBlockTargets& get_target() override;
+
+  /**
+   * @brief Hidden
+   */
+  CurrentScreenBlock& _buildBlock(NodeMaterialBuildState& state) override;
+
+private:
+  void _injectVertexCode(NodeMaterialBuildState& state);
+  void _writeTextureRead(NodeMaterialBuildState& state, bool vertexMode = false);
+  void _writeOutput(NodeMaterialBuildState& state, const NodeMaterialConnectionPointPtr& output,
+                    const std::string& swizzle, bool vertexMode = false);
+
+public:
+  /**
+   * Gets or sets the texture associated with the node
+   */
+  BaseTexturePtr texture;
+
+  /**
+   * Gets or sets a boolean indicating if content needs to be converted to gamma space
+   */
+  bool convertToGammaSpace;
+
+  /**
+   * Gets or sets a boolean indicating if content needs to be converted to linear space
+   */
+  bool convertToLinearSpace;
+
+  /**
+   * Gets the uv input component
+   */
+  ReadOnlyProperty<CurrentScreenBlock, NodeMaterialConnectionPointPtr> uv;
+
+  /**
+   * Gets the rgba output component
+   */
+  ReadOnlyProperty<CurrentScreenBlock, NodeMaterialConnectionPointPtr> rgba;
+
+  /**
+   * Gets the rgb output component
+   */
+  ReadOnlyProperty<CurrentScreenBlock, NodeMaterialConnectionPointPtr> rgb;
+
+  /**
+   * Gets the r output component
+   */
+  ReadOnlyProperty<CurrentScreenBlock, NodeMaterialConnectionPointPtr> r;
+
+  /**
+   * Gets the g output component
+   */
+  ReadOnlyProperty<CurrentScreenBlock, NodeMaterialConnectionPointPtr> g;
+
+  /**
+   * Gets the b output component
+   */
+  ReadOnlyProperty<CurrentScreenBlock, NodeMaterialConnectionPointPtr> b;
+
+  /**
+   * Gets the a output component
+   */
+  ReadOnlyProperty<CurrentScreenBlock, NodeMaterialConnectionPointPtr> a;
+
+private:
+  std::string _samplerName;
+  std::string _linearDefineName;
+  std::string _gammaDefineName;
+  std::string _mainUVName;
+  std::string _tempTextureRead;
+  NodeMaterialBlockTargets _currentTarget;
+
+}; // end of class CurrentScreenBlock
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_NODE_BLOCKS_DUAL_CURRENT_SHEEN_BLOCK_H
---
+++ src/BabylonCpp/src/materials/node/blocks/dual/current_screen_block.cpp
@@ -0,0 +1,324 @@
+#include <babylon/materials/node/blocks/dual/current_screen_block.h>
+
+#include <babylon/core/json_util.h>
+#include <babylon/materials/node/blocks/input/input_block.h>
+#include <babylon/materials/node/node_material_build_state.h>
+#include <babylon/materials/node/node_material_build_state_shared_data.h>
+#include <babylon/materials/node/node_material_connection_point.h>
+#include <babylon/materials/node/node_material_defines.h>
+#include <babylon/materials/textures/base_texture.h>
+#include <babylon/misc/string_tools.h>
+
+namespace BABYLON {
+
+CurrentScreenBlock::CurrentScreenBlock(const std::string& iName)
+    : NodeMaterialBlock{iName, NodeMaterialBlockTargets::VertexAndFragment}
+    , texture{nullptr}
+    , convertToGammaSpace{false}
+    , convertToLinearSpace{false}
+    , uv{this, &CurrentScreenBlock::get_uv}
+    , rgba{this, &CurrentScreenBlock::get_rgba}
+    , rgb{this, &CurrentScreenBlock::get_rgb}
+    , r{this, &CurrentScreenBlock::get_r}
+    , g{this, &CurrentScreenBlock::get_g}
+    , b{this, &CurrentScreenBlock::get_b}
+    , a{this, &CurrentScreenBlock::get_a}
+    , _samplerName{"textureSampler"}
+{
+  _isUnique = true;
+
+  registerInput("uv", NodeMaterialBlockConnectionPointTypes::Vector2, false,
+                NodeMaterialBlockTargets::VertexAndFragment);
+
+  registerOutput("rgba", NodeMaterialBlockConnectionPointTypes::Color4,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("rgb", NodeMaterialBlockConnectionPointTypes::Color3,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("r", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("g", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("b", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Neutral);
+  registerOutput("a", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Neutral);
+
+  _inputs[0]->acceptedConnectionPointTypes.emplace_back(
+    NodeMaterialBlockConnectionPointTypes::Vector3);
+  _inputs[0]->acceptedConnectionPointTypes.emplace_back(
+    NodeMaterialBlockConnectionPointTypes::Vector4);
+
+  _inputs[0]->_prioritizeVertex = true;
+}
+
+CurrentScreenBlock::~CurrentScreenBlock() = default;
+
+std::string CurrentScreenBlock::getClassName() const
+{
+  return "CurrentScreenBlock";
+}
+
+NodeMaterialConnectionPointPtr& CurrentScreenBlock::get_uv()
+{
+  return _inputs[0];
+}
+
+NodeMaterialConnectionPointPtr& CurrentScreenBlock::get_rgba()
+{
+  return _outputs[0];
+}
+
+NodeMaterialConnectionPointPtr& CurrentScreenBlock::get_rgb()
+{
+  return _outputs[1];
+}
+
+NodeMaterialConnectionPointPtr& CurrentScreenBlock::get_r()
+{
+  return _outputs[2];
+}
+
+NodeMaterialConnectionPointPtr& CurrentScreenBlock::get_g()
+{
+  return _outputs[3];
+}
+
+NodeMaterialConnectionPointPtr& CurrentScreenBlock::get_b()
+{
+  return _outputs[4];
+}
+
+NodeMaterialConnectionPointPtr& CurrentScreenBlock::get_a()
+{
+  return _outputs[5];
+}
+
+void CurrentScreenBlock::initialize(NodeMaterialBuildState& state)
+{
+  state._excludeVariableName("textureSampler");
+}
+
+NodeMaterialBlockTargets& CurrentScreenBlock::get_target()
+{
+  // TextureBlock has a special optimizations for uvs that come from the vertex shaders as they can
+  // be packed into a single varyings. But we need to detect uvs coming from fragment then
+  if (!uv()->isConnected()) {
+    _currentTarget = NodeMaterialBlockTargets::VertexAndFragment;
+    return _currentTarget;
+  }
+
+  if (uv()->sourceBlock()->isInput()) {
+    _currentTarget = NodeMaterialBlockTargets::VertexAndFragment;
+    return _currentTarget;
+  }
+
+  auto parent = uv()->connectedPoint();
+
+  while (parent) {
+    if (parent->target() == NodeMaterialBlockTargets::Fragment) {
+      _currentTarget = NodeMaterialBlockTargets::Fragment;
+      return _currentTarget;
+    }
+
+    if (parent->target() == NodeMaterialBlockTargets::Vertex) {
+      _currentTarget = NodeMaterialBlockTargets::VertexAndFragment;
+      return _currentTarget;
+    }
+
+    if (parent->target() == NodeMaterialBlockTargets::Neutral
+        || parent->target() == NodeMaterialBlockTargets::VertexAndFragment) {
+      auto parentBlock = parent->ownerBlock();
+
+      parent = nullptr;
+      for (const auto& input : parentBlock->inputs()) {
+        if (input->connectedPoint()) {
+          parent = input->connectedPoint();
+          break;
+        }
+      }
+    }
+  }
+
+  _currentTarget = NodeMaterialBlockTargets::VertexAndFragment;
+  return _currentTarget;
+}
+
+void CurrentScreenBlock::prepareDefines(AbstractMesh* /*mesh*/,
+                                        const NodeMaterialPtr& /*nodeMaterial*/,
+                                        NodeMaterialDefines& defines, bool /*useInstances*/,
+                                        const SubMeshPtr& /*subMesh*/)
+{
+  defines.setValue(_linearDefineName, convertToGammaSpace, true);
+  defines.setValue(_gammaDefineName, convertToLinearSpace, true);
+}
+
+bool CurrentScreenBlock::isReady(AbstractMesh* /*mesh*/, const NodeMaterialPtr& /*nodeMaterial*/,
+                                 const NodeMaterialDefines& /*defines*/, bool /*useInstances*/)
+{
+  if (texture && !texture->isReadyOrNotBlocking()) {
+    return false;
+  }
+
+  return true;
+}
+
+void CurrentScreenBlock::_injectVertexCode(NodeMaterialBuildState& state)
+{
+  auto uvInput = uv();
+
+  if (uvInput->connectedPoint()->ownerBlock()->isInput()) {
+    auto uvInputOwnerBlock
+      = std::static_pointer_cast<InputBlock>(uvInput->connectedPoint()->ownerBlock());
+
+    if (!uvInputOwnerBlock->isAttribute()) {
+      state._emitUniformFromString(uvInput->associatedVariableName, "vec2");
+    }
+  }
+
+  _mainUVName = "vMain" + uvInput->associatedVariableName();
+
+  state._emitVaryingFromString(_mainUVName, "vec2");
+
+  state.compilationString += StringTools::printf("%s = %s.xy;\r\n", _mainUVName.c_str(),
+                                                 uvInput->associatedVariableName().c_str());
+
+  for (const auto& output : _outputs) {
+    if (!output->isConnectedInVertexShader()) {
+      return;
+    }
+  }
+
+  _writeTextureRead(state, true);
+
+  for (const auto& output : _outputs) {
+    if (output->hasEndpoints()) {
+      _writeOutput(state, output, output->name, true);
+    }
+  }
+}
+
+void CurrentScreenBlock::_writeTextureRead(NodeMaterialBuildState& state, bool vertexMode)
+{
+  auto uvInput = uv();
+
+  if (vertexMode) {
+    if (state.target == NodeMaterialBlockTargets::Fragment) {
+      return;
+    }
+
+    state.compilationString
+      += StringTools::printf("vec4 %s = texture2D(%s, %s);\r\n", _tempTextureRead.c_str(),
+                             _samplerName.c_str(), uvInput->associatedVariableName().c_str());
+    return;
+  }
+
+  if (uv()->ownerBlock()->target() == NodeMaterialBlockTargets::Fragment) {
+    state.compilationString
+      += StringTools::printf("vec4 %s = texture2D(%s, %s);\r\n", _tempTextureRead.c_str(),
+                             _samplerName.c_str(), uvInput->associatedVariableName().c_str());
+    return;
+  }
+
+  state.compilationString
+    += StringTools::printf("vec4 %s = texture2D(%s, %s);\r\n", _tempTextureRead.c_str(),
+                           _samplerName.c_str(), _mainUVName.c_str());
+}
+
+void CurrentScreenBlock::_writeOutput(NodeMaterialBuildState& state,
+                                      const NodeMaterialConnectionPointPtr& output,
+                                      const std::string& swizzle, bool vertexMode)
+{
+  if (vertexMode) {
+    if (state.target == NodeMaterialBlockTargets::Fragment) {
+      return;
+    }
+
+    state.compilationString
+      += StringTools::printf("%s = %s.%s;\r\n", _declareOutput(output, state).c_str(),
+                             _tempTextureRead.c_str(), swizzle.c_str());
+
+    return;
+  }
+
+  if (uv()->ownerBlock()->target() == NodeMaterialBlockTargets::Fragment) {
+    state.compilationString
+      += StringTools::printf("%s = %s.%s;\r\n", _declareOutput(output, state).c_str(),
+                             _tempTextureRead.c_str(), swizzle.c_str());
+    return;
+  }
+
+  state.compilationString
+    += StringTools::printf("%s = %s.%s;\r\n", _declareOutput(output, state).c_str(),
+                           _tempTextureRead.c_str(), swizzle.c_str());
+
+  state.compilationString += StringTools::printf("#ifdef %s\r\n", _linearDefineName.c_str());
+  state.compilationString
+    += StringTools::printf("%s = toGammaSpace(%s);\r\n", output->associatedVariableName().c_str(),
+                           output->associatedVariableName().c_str());
+  state.compilationString += "#endif\r\n";
+
+  state.compilationString += StringTools::printf("#ifdef %s\r\n", _gammaDefineName.c_str());
+  state.compilationString
+    += StringTools::printf("%s = toLinearSpace(%s);\r\n", output->associatedVariableName().c_str(),
+                           output->associatedVariableName().c_str());
+  state.compilationString += "#endif\r\n";
+}
+
+CurrentScreenBlock& CurrentScreenBlock::_buildBlock(NodeMaterialBuildState& state)
+{
+  NodeMaterialBlock::_buildBlock(state);
+
+  if (state.target == NodeMaterialBlockTargets::Vertex) {
+    _tempTextureRead = state._getFreeVariableName("tempTextureRead");
+
+    state._emit2DSampler(_samplerName);
+
+    state.sharedData->blockingBlocks.emplace_back(shared_from_this());
+    state.sharedData->textureBlocks.emplace_back(
+      std::static_pointer_cast<CurrentScreenBlock>(shared_from_this()));
+    state.sharedData->blocksWithDefines.emplace_back(shared_from_this());
+  }
+
+  if (state.target != NodeMaterialBlockTargets::Fragment) {
+    // Vertex
+    _injectVertexCode(state);
+    return *this;
+  }
+
+  // Fragment
+  for (const auto& output : _outputs) {
+    if (!output->isConnectedInFragmentShader()) {
+      return *this;
+    }
+  }
+
+  state._emit2DSampler(_samplerName);
+
+  _linearDefineName = state._getFreeDefineName("ISLINEAR");
+  _gammaDefineName  = state._getFreeDefineName("ISGAMMA");
+
+  const auto comments = StringTools::printf("//%s", name.c_str());
+  state._emitFunctionFromInclude("helperFunctions", comments);
+
+  _writeTextureRead(state);
+
+  for (const auto& output : _outputs) {
+    if (output->hasEndpoints()) {
+      _writeOutput(state, output, output->name);
+    }
+  }
+
+  return *this;
+}
+
+json CurrentScreenBlock::serialize() const
+{
+  return nullptr;
+}
+
+void CurrentScreenBlock::_deserialize(const json& /*serializationObject*/, Scene* /*scene*/,
+                                      const std::string& /*rootUrl*/)
+{
+}
+
+} // end of namespace BABYLON
