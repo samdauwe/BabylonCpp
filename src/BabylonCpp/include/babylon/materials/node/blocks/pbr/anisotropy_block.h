251fcea9650346f02ae6b50eba0c9c3517a6161c
samdauwe <sam.dauwe@gmail.com>
2020-07-06 21:00:00 +0200

    Implemented AnisotropyBlock class

---
+++ src/BabylonCpp/include/babylon/materials/node/blocks/pbr/anisotropy_block.h
@@ -0,0 +1,159 @@
+#ifndef BABYLON_MATERIALS_NODE_BLOCKS_PBR_ANISOTROPY_BLOCK_H
+#define BABYLON_MATERIALS_NODE_BLOCKS_PBR_ANISOTROPY_BLOCK_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/materials/node/node_material_block.h>
+
+namespace BABYLON {
+
+class AnisotropyBlock;
+class NodeMaterialConnectionPoint;
+using AnisotropyBlockPtr             = std::shared_ptr<AnisotropyBlock>;
+using NodeMaterialConnectionPointPtr = std::shared_ptr<NodeMaterialConnectionPoint>;
+
+/**
+ * @brief Block used to implement the reflectivity module of the PBR material.
+ */
+class BABYLON_SHARED_EXPORT AnisotropyBlock : public NodeMaterialBlock {
+
+public:
+  template <typename... Ts>
+  static AnisotropyBlockPtr New(Ts&&... args)
+  {
+    auto anisotropyBlock
+      = std::shared_ptr<AnisotropyBlock>(new AnisotropyBlock(std::forward<Ts>(args)...));
+    AnisotropyBlock::RegisterConnections(anisotropyBlock);
+
+    return anisotropyBlock;
+  }
+  ~AnisotropyBlock() override;
+
+  /**
+   * @brief Hidden
+   */
+  static void RegisterConnections(const AnisotropyBlockPtr& anisotropyBlock);
+
+  /**
+   * @brief Initialize the block and prepare the context for build.
+   * @param state defines the state that will be used for the build
+   */
+  void initialize(NodeMaterialBuildState& state) override;
+
+  /**
+   * @brief Gets the current class name.
+   * @returns the class name
+   */
+  std::string getClassName() const override;
+
+  /**
+   * @brief Gets the main code of the block (fragment side)
+   * @param state current state of the node material building
+   * @param generateTBNSpace if true, the code needed to create the TBN coordinate space is
+   * generated
+   * @returns the shader code
+   */
+  std::string getCode(NodeMaterialBuildState& state, bool generateTBNSpace = false) const;
+
+  /**
+   * @brief Update defines for shader compilation.
+   * @param mesh defines the mesh to be rendered
+   * @param nodeMaterial defines the node material requesting the update
+   * @param defines defines the material defines to update
+   * @param useInstances specifies that instances should be used
+   */
+  void prepareDefines(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+                      NodeMaterialDefines& defines, bool useInstances = false,
+                      const SubMeshPtr& subMesh = nullptr) override;
+
+protected:
+  /**
+   * @brief Creates a new AnisotropyBlock.
+   * @param name defines the block name
+   */
+  AnisotropyBlock(const std::string& name);
+
+  /**
+   * @brief Gets the intensity input component
+   */
+  NodeMaterialConnectionPointPtr& get_intensity();
+
+  /**
+   * @brief Gets the direction input component
+   */
+  NodeMaterialConnectionPointPtr& get_direction();
+
+  /**
+   * @brief Gets the texture input component
+   */
+  NodeMaterialConnectionPointPtr& get_texture();
+
+  /**
+   * @brief Gets the uv input component
+   */
+  NodeMaterialConnectionPointPtr& get_uv();
+
+  /**
+   * @brief Gets the worldTangent input component
+   */
+  NodeMaterialConnectionPointPtr& get_worldTangent();
+
+  /**
+   * @brief Gets the anisotropy object output component
+   */
+  NodeMaterialConnectionPointPtr& get_anisotropy();
+
+  /**
+   * @brief Hidden
+   */
+  AnisotropyBlock& _buildBlock(NodeMaterialBuildState& state) override;
+
+private:
+  std::string _generateTBNSpace(NodeMaterialBuildState& state) const;
+
+public:
+  /**
+   * The two properties below are set by the main PBR block prior to calling methods of this class.
+   * This is to avoid having to add them as inputs here whereas they are already inputs of the main
+   * block, so already known. It's less burden on the user side in the editor part.
+   */
+
+  /** @hidden */
+  NodeMaterialConnectionPointPtr worldPositionConnectionPoint;
+  /** @hidden */
+  NodeMaterialConnectionPointPtr worldNormalConnectionPoint;
+
+  /**
+   * Gets the intensity input component
+   */
+  ReadOnlyProperty<AnisotropyBlock, NodeMaterialConnectionPointPtr> intensity;
+
+  /**
+   * Gets the direction input component
+   */
+  ReadOnlyProperty<AnisotropyBlock, NodeMaterialConnectionPointPtr> direction;
+
+  /**
+   * Gets the texture input component
+   */
+  ReadOnlyProperty<AnisotropyBlock, NodeMaterialConnectionPointPtr> texture;
+
+  /**
+   * Gets the uv input component
+   */
+  ReadOnlyProperty<AnisotropyBlock, NodeMaterialConnectionPointPtr> uv;
+
+  /**
+   * Gets the worldTangent input component
+   */
+  ReadOnlyProperty<AnisotropyBlock, NodeMaterialConnectionPointPtr> worldTangent;
+
+  /**
+   * Gets the anisotropy object output component
+   */
+  ReadOnlyProperty<AnisotropyBlock, NodeMaterialConnectionPointPtr> anisotropy;
+
+}; // end of class AnisotropyBlock
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_NODE_BLOCKS_PBR_ANISOTROPY_BLOCK_H
---
+++ src/BabylonCpp/src/materials/node/blocks/pbr/anisotropy_block.cpp
@@ -0,0 +1,195 @@
+#include <babylon/materials/node/blocks/pbr/anisotropy_block.h>
+
+#include <babylon/core/logging.h>
+#include <babylon/materials/node/node_material_build_state.h>
+#include <babylon/materials/node/node_material_build_state_shared_data.h>
+#include <babylon/materials/node/node_material_connection_point.h>
+#include <babylon/materials/node/node_material_connection_point_custom_object.h>
+#include <babylon/materials/node/node_material_defines.h>
+#include <babylon/misc/string_tools.h>
+
+namespace BABYLON {
+
+AnisotropyBlock::AnisotropyBlock(const std::string& iName)
+    : NodeMaterialBlock{iName, NodeMaterialBlockTargets::Fragment}
+    , worldPositionConnectionPoint{nullptr}
+    , worldNormalConnectionPoint{nullptr}
+    , intensity{this, &AnisotropyBlock::get_intensity}
+    , direction{this, &AnisotropyBlock::get_direction}
+    , texture{this, &AnisotropyBlock::get_texture}
+    , uv{this, &AnisotropyBlock::get_uv}
+    , worldTangent{this, &AnisotropyBlock::get_worldTangent}
+    , anisotropy{this, &AnisotropyBlock::get_anisotropy}
+{
+}
+
+AnisotropyBlock::~AnisotropyBlock() = default;
+
+void AnisotropyBlock::RegisterConnections(const AnisotropyBlockPtr& anisotropyBlock)
+{
+  anisotropyBlock->registerInput("intensity", NodeMaterialBlockConnectionPointTypes::Float, true,
+                                 NodeMaterialBlockTargets::Fragment);
+  anisotropyBlock->registerInput("direction", NodeMaterialBlockConnectionPointTypes::Vector2, true,
+                                 NodeMaterialBlockTargets::Fragment);
+  anisotropyBlock->registerInput("texture", NodeMaterialBlockConnectionPointTypes::Color3, true,
+                                 NodeMaterialBlockTargets::Fragment);
+  anisotropyBlock->registerInput("uv", NodeMaterialBlockConnectionPointTypes::Vector2,
+                                 true); // need this property and the next one in case there's no
+                                        // PerturbNormal block connected to the main PBR block
+  anisotropyBlock->registerInput("worldTangent", NodeMaterialBlockConnectionPointTypes::Vector4,
+                                 true);
+
+  anisotropyBlock->registerOutput(
+    "anisotropy", NodeMaterialBlockConnectionPointTypes::Object, NodeMaterialBlockTargets::Fragment,
+    NodeMaterialConnectionPointCustomObject::New(
+      "anisotropy", anisotropyBlock, NodeMaterialConnectionPointDirection::Output,
+      [](const std::string& iName) -> AnisotropyBlockPtr { return AnisotropyBlock::New(iName); },
+      "AnisotropyBlock"));
+}
+
+void AnisotropyBlock::initialize(NodeMaterialBuildState& state)
+{
+  state._excludeVariableName("anisotropicOut");
+  state._excludeVariableName("TBN");
+}
+
+std::string AnisotropyBlock::getClassName() const
+{
+  return "AnisotropyBlock";
+}
+
+NodeMaterialConnectionPointPtr& AnisotropyBlock::get_intensity()
+{
+  return _inputs[0];
+}
+
+NodeMaterialConnectionPointPtr& AnisotropyBlock::get_direction()
+{
+  return _inputs[1];
+}
+
+NodeMaterialConnectionPointPtr& AnisotropyBlock::get_texture()
+{
+  return _inputs[2];
+}
+
+NodeMaterialConnectionPointPtr& AnisotropyBlock::get_uv()
+{
+  return _inputs[3];
+}
+
+NodeMaterialConnectionPointPtr& AnisotropyBlock::get_worldTangent()
+{
+  return _inputs[4];
+}
+
+NodeMaterialConnectionPointPtr& AnisotropyBlock::get_anisotropy()
+{
+  return _outputs[0];
+}
+
+std::string AnisotropyBlock::_generateTBNSpace(NodeMaterialBuildState& state) const
+{
+  std::string code = "";
+
+  auto iComments     = StringTools::printf("//%s", name.c_str());
+  auto iUv           = uv();
+  auto worldPosition = worldPositionConnectionPoint;
+  auto worldNormal   = worldNormalConnectionPoint;
+  auto iWorldTangent = worldTangent();
+
+  if (!iUv->isConnected()) {
+    // we must set the uv input as optional because we may not end up in this method (in case a
+    // PerturbNormal block is linked to the PBR material) in which case uv is not required. But if
+    // we do come here, we do need the uv, so we have to raise an error but not with throw, else it
+    // will stop the building of the node material and will lead to errors in the editor!
+    BABYLON_LOG_ERROR("AnisotropyBlock", "You must connect the 'uv' input of the Anisotropy block!")
+  }
+
+  state._emitExtension("derivatives", "#extension GL_OES_standard_derivatives : enable");
+
+  EmitFunctionFromIncludeOptions tangentReplaceStringOptions;
+  tangentReplaceStringOptions.replaceStrings
+    = {{R"(defined\(TANGENT\)/)",
+        iWorldTangent->isConnected() ? "defined(TANGENT)" : "defined(IGNORE)"}};
+
+  if (iWorldTangent->isConnected()) {
+    code += StringTools::printf("vec3 tbnNormal = normalize(%s.xyz);\r\n",
+                                worldNormal->associatedVariableName().c_str());
+    code += StringTools::printf("vec3 tbnTangent = normalize(%s.xyz);\r\n",
+                                iWorldTangent->associatedVariableName().c_str());
+    code += "vec3 tbnBitangent = cross(tbnNormal, tbnTangent);\r\n";
+    code += "mat3 vTBN = mat3(tbnTangent, tbnBitangent, tbnNormal);\r\n";
+  }
+
+  code += StringTools::printf(
+    R"(#if defined(${worldTangent.isConnected ? "TANGENT" : "IGNORE"}) && defined(NORMAL)
+            mat3 TBN = vTBN;
+        #else
+            mat3 TBN = cotangent_frame(%s, %s, %s, vec2(1., 1.));
+        #endif\r\n
+      )",
+    StringTools::printf("%s.xyz", worldNormal->associatedVariableName().c_str()).c_str(),
+    StringTools::printf("v_%s.xyz", worldPosition->associatedVariableName().c_str()).c_str(),
+    StringTools::printf("%s", iUv->isConnected ? iUv->associatedVariableName().c_str() : "vec2(0.)")
+      .c_str());
+
+  state._emitFunctionFromInclude("bumpFragmentMainFunctions", iComments,
+                                 tangentReplaceStringOptions);
+
+  return code;
+}
+
+std::string AnisotropyBlock::getCode(NodeMaterialBuildState& state, bool generateTBNSpace) const
+{
+  std::string code = "";
+
+  if (generateTBNSpace) {
+    code += _generateTBNSpace(state);
+  }
+
+  const auto iIntensity
+    = intensity()->isConnected() ? intensity()->associatedVariableName() : "1.0";
+  const auto iDirection
+    = direction()->isConnected() ? direction()->associatedVariableName() : "vec2(1., 0.)";
+  const auto iTexture = texture()->isConnected() ? texture()->associatedVariableName() : "vec3(0.)";
+
+  code += StringTools::printf(
+    R"(anisotropicOutParams anisotropicOut;
+        anisotropicBlock(
+            vec3(%s, %s),
+        #ifdef ANISOTROPIC_TEXTURE
+            %s,
+        #endif
+            TBN,
+            normalW,
+            viewDirectionW,
+            anisotropicOut
+        );\r\n
+      )",
+    iDirection.c_str(), iIntensity.c_str(), //
+    iTexture.c_str());
+
+  return code;
+}
+
+void AnisotropyBlock::prepareDefines(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+                                     NodeMaterialDefines& defines, bool /*useInstances*/,
+                                     const SubMeshPtr& /*subMesh*/)
+{
+  NodeMaterialBlock::prepareDefines(mesh, nodeMaterial, defines);
+
+  defines.setValue("ANISOTROPIC", true);
+  defines.setValue("ANISOTROPIC_TEXTURE", texture()->isConnected(), true);
+}
+
+AnisotropyBlock& AnisotropyBlock::_buildBlock(NodeMaterialBuildState& state)
+{
+  if (state.target == NodeMaterialBlockTargets::Fragment) {
+    state.sharedData->blocksWithDefines.emplace_back(shared_from_this());
+  }
+
+  return *this;
+}
+
+} // end of namespace BABYLON
