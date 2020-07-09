82cb43ffa7a852ec9f095fb3cbbfd9d29e19d89d
samdauwe <sam.dauwe@gmail.com>
2020-07-09 20:24:00 +0200

    Implemented RefractionBlock class

---
+++ src/BabylonCpp/include/babylon/materials/node/blocks/pbr/refraction_block.h
@@ -0,0 +1,228 @@
+#ifndef BABYLON_MATERIALS_NODE_BLOCKS_PBR_REFRACTION_BLOCK_H
+#define BABYLON_MATERIALS_NODE_BLOCKS_PBR_REFRACTION_BLOCK_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/materials/node/node_material_block.h>
+
+namespace BABYLON {
+
+class BaseTexture;
+class RefractionBlock;
+using BaseTexturePtr     = std::shared_ptr<BaseTexture>;
+using RefractionBlockPtr = std::shared_ptr<RefractionBlock>;
+
+/**
+ * @brief Block used to implement the refraction part of the sub surface module of the PBR material.
+ */
+class BABYLON_SHARED_EXPORT RefractionBlock : public NodeMaterialBlock {
+
+public:
+  template <typename... Ts>
+  static RefractionBlockPtr New(Ts&&... args)
+  {
+    auto refractionBlock
+      = std::shared_ptr<RefractionBlock>(new RefractionBlock(std::forward<Ts>(args)...));
+    RefractionBlock::RegisterConnections(refractionBlock);
+
+    return refractionBlock;
+  }
+  ~RefractionBlock() override;
+
+  /**
+   * @brief Hidden
+   */
+  static void RegisterConnections(const RefractionBlockPtr& refractionBlock);
+
+  /**
+   * @brief Gets the current class name.
+   * @returns the class name
+   */
+  std::string getClassName() const override;
+
+  /**
+   * @brief Lets the block try to connect some inputs automatically.
+   * @param material defines the hosting NodeMaterial
+   */
+  void autoConfigure(const NodeMaterialPtr& material) override;
+
+  /**
+   * @brief Update defines for shader compilation.
+   * @param mesh defines the mesh to be rendered
+   * @param nodeMaterial defines the node material requesting the update
+   * @param defines defines the material defines to update
+   * @param useInstances specifies that instances should be used
+   */
+  void prepareDefines(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+                      NodeMaterialDefines& defines, bool useInstances = false,
+                      const SubMeshPtr& subMesh = nullptr) override;
+
+  /**
+   * @brief Checks if the block is ready.
+   * @param mesh defines the mesh to be rendered
+   * @param nodeMaterial defines the node material requesting the update
+   * @param defines defines the material defines to update
+   * @param useInstances specifies that instances should be used
+   * @returns true if the block is ready
+   */
+  bool isReady(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+               const NodeMaterialDefines& defines, bool useInstances = false) override;
+
+  /**
+   * @brief Bind data to effect. Will only be called for blocks with isBindable === true.
+   * @param effect defines the effect to bind data to
+   * @param nodeMaterial defines the hosting NodeMaterial
+   * @param mesh defines the mesh that will be rendered
+   */
+  void bind(const EffectPtr& effect, const NodeMaterialPtr& nodeMaterial, Mesh* mesh = nullptr,
+            SubMesh* subMesh = nullptr) override;
+
+  /**
+   * @brief Gets the main code of the block (fragment side)
+   * @param state current state of the node material building
+   * @returns the shader code
+   */
+  std::string getCode(NodeMaterialBuildState& state);
+
+  /**
+   * @brief Serializes this block in a JSON representation.
+   * @returns the serialized block object
+   */
+  json serialize() const override;
+
+  /**
+   * @brief Hidden
+   */
+  void _deserialize(const json& serializationObject, Scene* scene,
+                    const std::string& rootUrl) override;
+
+protected:
+  /**
+   * @brief Creates a new RefractionBlock.
+   * @param name defines the block name
+   */
+  RefractionBlock(const std::string& name);
+
+  /**
+   * @brief Gets the intensity input component.
+   */
+  NodeMaterialConnectionPointPtr& get_intensity();
+
+  /**
+   * @brief Gets the index of refraction input component.
+   */
+  NodeMaterialConnectionPointPtr& get_indexOfRefraction();
+
+  /**
+   * @brief Gets the tint at distance input component.
+   */
+  NodeMaterialConnectionPointPtr& get_tintAtDistance();
+
+  /**
+   * @brief Gets the view input component.
+   */
+  NodeMaterialConnectionPointPtr& get_view();
+
+  /**
+   * @brief Gets the refraction object output component.
+   */
+  NodeMaterialConnectionPointPtr& get_refraction();
+
+  /**
+   * @brief Returns true if the block has a texture.
+   */
+  bool get_hasTexture() const;
+
+  /**
+   * @brief Hidden
+   */
+  BaseTexturePtr _getTexture() const;
+
+  /**
+   * @brief Hidden
+   */
+  RefractionBlock& _buildBlock(NodeMaterialBuildState& state) override;
+
+  /**
+   * @brief Hidden
+   */
+  std::string _dumpPropertiesCode() override;
+
+public:
+  /** @hidden */
+  std::string _define3DName;
+  /** @hidden */
+  std::string _refractionMatrixName;
+  /** @hidden */
+  std::string _defineLODRefractionAlpha;
+  /** @hidden */
+  std::string _defineLinearSpecularRefraction;
+  /** @hidden */
+  std::string _defineOppositeZ;
+  /** @hidden */
+  std::string _cubeSamplerName;
+  /** @hidden */
+  std::string _2DSamplerName;
+  /** @hidden */
+  std::string _vRefractionMicrosurfaceInfosName;
+  /** @hidden */
+  std::string _vRefractionInfosName;
+
+  /**
+   * This parameters will make the material used its opacity to control how much it is refracting
+   * aginst not. Materials half opaque for instance using refraction could benefit from this
+   * control.
+   */
+  // @editableInPropertyPage("Link refraction to transparency", PropertyTypeForEdition.Boolean,
+  // "ADVANCED", { "notifiers": { "update": true }})
+  bool linkRefractionWithTransparency;
+
+  /**
+   * Controls if refraction needs to be inverted on Y. This could be useful for procedural texture.
+   */
+  // @editableInPropertyPage("Invert refraction Y", PropertyTypeForEdition.Boolean, "ADVANCED", {
+  // "notifiers": { "update": true }})
+  bool invertRefractionY;
+
+  /**
+   * Gets or sets the texture associated with the node
+   */
+  BaseTexturePtr texture;
+
+  /**
+   * Gets the intensity input component
+   */
+  ReadOnlyProperty<RefractionBlock, NodeMaterialConnectionPointPtr> intensity;
+
+  /**
+   * Gets the index of refraction input component
+   */
+  ReadOnlyProperty<RefractionBlock, NodeMaterialConnectionPointPtr> indexOfRefraction;
+
+  /**
+   * Gets the tint at distance input component
+   */
+  ReadOnlyProperty<RefractionBlock, NodeMaterialConnectionPointPtr> tintAtDistance;
+
+  /**
+   * Gets the view input component
+   */
+  ReadOnlyProperty<RefractionBlock, NodeMaterialConnectionPointPtr> view;
+
+  /**
+   * Gets the refraction object output component
+   */
+  ReadOnlyProperty<RefractionBlock, NodeMaterialConnectionPointPtr> refraction;
+
+  /**
+   * Returns true if the block has a texture
+   */
+  ReadOnlyProperty<RefractionBlock, bool> hasTexture;
+
+private:
+  Scene* _scene;
+
+}; // end of class RefractionBlock
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_NODE_BLOCKS_PBR_REFRACTION_BLOCK_H
---
+++ src/BabylonCpp/src/materials/node/blocks/pbr/refraction_block.cpp
@@ -0,0 +1,319 @@
+#include <babylon/materials/node/blocks/pbr/refraction_block.h>
+
+#include <babylon/core/json_util.h>
+#include <babylon/engines/scene.h>
+#include <babylon/materials/effect.h>
+#include <babylon/materials/node/blocks/input/input_block.h>
+#include <babylon/materials/node/node_material.h>
+#include <babylon/materials/node/node_material_build_state.h>
+#include <babylon/materials/node/node_material_build_state_shared_data.h>
+#include <babylon/materials/node/node_material_connection_point.h>
+#include <babylon/materials/node/node_material_connection_point_custom_object.h>
+#include <babylon/materials/node/node_material_defines.h>
+#include <babylon/materials/textures/base_texture.h>
+#include <babylon/misc/string_tools.h>
+
+namespace BABYLON {
+
+RefractionBlock::RefractionBlock(const std::string& iName)
+    : NodeMaterialBlock{iName, NodeMaterialBlockTargets::Fragment}
+    , linkRefractionWithTransparency{false}
+    , invertRefractionY{false}
+    , texture{nullptr}
+    , intensity{this, &RefractionBlock::get_intensity}
+    , indexOfRefraction{this, &RefractionBlock::get_indexOfRefraction}
+    , tintAtDistance{this, &RefractionBlock::get_tintAtDistance}
+    , view{this, &RefractionBlock::get_view}
+    , refraction{this, &RefractionBlock::get_refraction}
+    , hasTexture{this, &RefractionBlock::get_hasTexture}
+    , _scene{nullptr}
+{
+  _isUnique = true;
+}
+
+RefractionBlock::~RefractionBlock() = default;
+
+void RefractionBlock::RegisterConnections(const RefractionBlockPtr& refractionBlock)
+{
+  refractionBlock->registerInput("intensity", NodeMaterialBlockConnectionPointTypes::Float, false,
+                                 NodeMaterialBlockTargets::Fragment);
+  refractionBlock->registerInput("indexOfRefraction", NodeMaterialBlockConnectionPointTypes::Float,
+                                 true, NodeMaterialBlockTargets::Fragment);
+  refractionBlock->registerInput("tintAtDistance", NodeMaterialBlockConnectionPointTypes::Float,
+                                 true, NodeMaterialBlockTargets::Fragment);
+  refractionBlock->registerInput("view", NodeMaterialBlockConnectionPointTypes::Matrix, false,
+                                 NodeMaterialBlockTargets::Fragment);
+
+  refractionBlock->registerOutput(
+    "refraction", NodeMaterialBlockConnectionPointTypes::Object, NodeMaterialBlockTargets::Fragment,
+    NodeMaterialConnectionPointCustomObject::New(
+      "refraction", refractionBlock, NodeMaterialConnectionPointDirection::Output,
+      [](const std::string& iName) -> RefractionBlockPtr { return RefractionBlock::New(iName); },
+      "RefractionBlock"));
+}
+
+std::string RefractionBlock::getClassName() const
+{
+  return "RefractionBlock";
+}
+
+NodeMaterialConnectionPointPtr& RefractionBlock::get_intensity()
+{
+  return _inputs[0];
+}
+
+NodeMaterialConnectionPointPtr& RefractionBlock::get_indexOfRefraction()
+{
+  return _inputs[1];
+}
+
+NodeMaterialConnectionPointPtr& RefractionBlock::get_tintAtDistance()
+{
+  return _inputs[2];
+}
+
+NodeMaterialConnectionPointPtr& RefractionBlock::get_view()
+{
+  return _inputs[3];
+}
+
+NodeMaterialConnectionPointPtr& RefractionBlock::get_refraction()
+{
+  return _outputs[0];
+}
+
+bool RefractionBlock::get_hasTexture() const
+{
+  return _getTexture() != nullptr;
+}
+
+BaseTexturePtr RefractionBlock::_getTexture() const
+{
+  if (texture) {
+    return texture;
+  }
+
+  return _scene->environmentTexture();
+}
+
+void RefractionBlock::autoConfigure(const NodeMaterialPtr& material)
+{
+  if (!intensity()->isConnected()) {
+    auto intensityInput
+      = InputBlock::New("Refraction intensity", NodeMaterialBlockTargets::Fragment,
+                        NodeMaterialBlockConnectionPointTypes::Float);
+    intensityInput->value = std::make_shared<AnimationValue>(1.f);
+    intensityInput->output()->connectTo(intensity);
+  }
+
+  if (!view()->isConnected()) {
+    auto viewInput = material->getInputBlockByPredicate([](const InputBlockPtr& b) -> bool {
+      return b->systemValue() == NodeMaterialSystemValues::View;
+    });
+
+    if (!viewInput) {
+      viewInput = InputBlock::New("view");
+      viewInput->setAsSystemValue(NodeMaterialSystemValues::View);
+    }
+    viewInput->output()->connectTo(view);
+  }
+}
+
+void RefractionBlock::prepareDefines(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+                                     NodeMaterialDefines& defines, bool /*useInstances*/,
+                                     const SubMeshPtr& /*subMesh*/)
+{
+  NodeMaterialBlock::prepareDefines(mesh, nodeMaterial, defines);
+
+  const auto refractionTexture = _getTexture();
+  const auto refraction        = refractionTexture && refractionTexture->getTextureMatrix();
+
+  defines.setValue("SS_REFRACTION", refraction, true);
+
+  if (!refraction) {
+    return;
+  }
+
+  defines.setValue(_define3DName, refractionTexture->isCube(), true);
+  defines.setValue(_defineLODRefractionAlpha, refractionTexture->lodLevelInAlpha, true);
+  defines.setValue(_defineLinearSpecularRefraction, refractionTexture->linearSpecularLOD(), true);
+  defines.setValue(_defineOppositeZ,
+                   _scene->useRightHandedSystem() ? !refractionTexture->invertZ :
+                                                    refractionTexture->invertZ,
+                   true);
+
+  defines.setValue("SS_LINKREFRACTIONTOTRANSPARENCY", linkRefractionWithTransparency, true);
+  defines.setValue("SS_GAMMAREFRACTION", refractionTexture->gammaSpace(), true);
+  defines.setValue("SS_RGBDREFRACTION", refractionTexture->isRGBD(), true);
+}
+
+bool RefractionBlock::isReady(AbstractMesh* /*mesh*/, const NodeMaterialPtr& /*nodeMaterial*/,
+                              const NodeMaterialDefines& /*defines*/, bool /*useInstances*/)
+{
+  const auto texture = _getTexture();
+
+  if (texture && !texture->isReadyOrNotBlocking()) {
+    return false;
+  }
+
+  return true;
+}
+
+void RefractionBlock::bind(const EffectPtr& effect, const NodeMaterialPtr& nodeMaterial, Mesh* mesh,
+                           SubMesh* /*subMesh*/)
+{
+  NodeMaterialBlock::bind(effect, nodeMaterial, mesh);
+
+  const auto refractionTexture = _getTexture();
+
+  if (!refractionTexture) {
+    return;
+  }
+
+  if (refractionTexture->isCube()) {
+    effect->setTexture(_cubeSamplerName, refractionTexture);
+  }
+  else {
+    effect->setTexture(_2DSamplerName, refractionTexture);
+  }
+
+  effect->setMatrix(_refractionMatrixName, *refractionTexture->getReflectionTextureMatrix());
+
+  auto depth = 1.f;
+  if (!refractionTexture->isCube()) {
+    /*if (refractionTexture.depth) {
+        depth = (<any>refractionTexture).depth;
+    }*/
+  }
+
+  auto iIndexOfRefraction = 1.f;
+  indexOfRefraction()->connectInputBlock()->value() ?
+    indexOfRefraction()->connectInputBlock()->value()->get<float>() :
+    1.f;
+
+  effect->setFloat4(_vRefractionInfosName, refractionTexture->level, 1.f / iIndexOfRefraction,
+                    depth, invertRefractionY ? -1.f : 1.f);
+
+  effect->setFloat3(_vRefractionMicrosurfaceInfosName, refractionTexture->getSize().width,
+                    refractionTexture->lodGenerationScale(),
+                    refractionTexture->lodGenerationOffset());
+}
+
+std::string RefractionBlock::getCode(NodeMaterialBuildState& state)
+{
+  std::string code = "";
+
+  state.sharedData->blockingBlocks.emplace_back(shared_from_this());
+  state.sharedData->textureBlocks.emplace_back(
+    std::static_pointer_cast<RefractionBlock>(shared_from_this()));
+
+  // Samplers
+  _cubeSamplerName = state._getFreeVariableName(name + "CubeSampler");
+  state.samplers.emplace_back(_cubeSamplerName);
+
+  _2DSamplerName = state._getFreeVariableName(name + "2DSampler");
+  state.samplers.emplace_back(_2DSamplerName);
+
+  _define3DName = state._getFreeDefineName("SS_REFRACTIONMAP_3D");
+
+  state._samplerDeclaration += StringTools::printf("#ifdef %s\r\n", _define3DName.c_str());
+  state._samplerDeclaration
+    += StringTools::printf("uniform samplerCube %s;\r\n", _cubeSamplerName.c_str());
+  state._samplerDeclaration += "#else\r\n";
+  state._samplerDeclaration
+    += StringTools::printf("uniform sampler2D %s;\r\n", _2DSamplerName.c_str());
+  state._samplerDeclaration += "#endif\r\n";
+
+  // Fragment
+  state.sharedData->blocksWithDefines.emplace_back(shared_from_this());
+  state.sharedData->bindableBlocks.emplace_back(shared_from_this());
+
+  _defineLODRefractionAlpha       = state._getFreeDefineName("SS_LODINREFRACTIONALPHA");
+  _defineLinearSpecularRefraction = state._getFreeDefineName("SS_LINEARSPECULARREFRACTION");
+  _defineOppositeZ                = state._getFreeDefineName("SS_REFRACTIONMAP_OPPOSITEZ");
+
+  _refractionMatrixName = state._getFreeVariableName("refractionMatrix");
+
+  state._emitUniformFromString(_refractionMatrixName, "mat4");
+
+  const auto iComment = StringTools::printf("//%s", name.c_str());
+
+  state._emitFunction("sampleRefraction",
+                      StringTools::printf(
+                        R"(
+                           #ifdef %s
+                               #define sampleRefraction(s, c) textureCube(s, c)
+                           #else
+                               #define sampleRefraction(s, c) texture2D(s, c)
+                           #endif\r\n
+                          )",
+                        _define3DName.c_str()),
+                      iComment.c_str());
+
+  state._emitFunction("sampleRefractionLod",
+                      StringTools::printf(
+                        R"(
+                           #ifdef %s
+                               #define sampleRefractionLod(s, c, l) textureCubeLodEXT(s, c, l)
+                           #else
+                               #define sampleRefractionLod(s, c, l) texture2DLodEXT(s, c, l)
+                           #endif\r\n
+                          )",
+                        _define3DName.c_str()),
+                      iComment.c_str());
+
+  _vRefractionMicrosurfaceInfosName = state._getFreeVariableName("vRefractionMicrosurfaceInfos");
+
+  state._emitUniformFromString(_vRefractionMicrosurfaceInfosName, "vec3");
+
+  _vRefractionInfosName = state._getFreeVariableName("vRefractionInfos");
+
+  state._emitUniformFromString(_vRefractionInfosName, "vec4");
+
+  return code;
+}
+
+RefractionBlock& RefractionBlock::_buildBlock(NodeMaterialBuildState& state)
+{
+  _scene = state.sharedData->scene;
+
+  return *this;
+}
+
+std::string RefractionBlock::_dumpPropertiesCode()
+{
+  auto codeString = NodeMaterialBlock::_dumpPropertiesCode();
+
+  if (texture) {
+    if (texture->isCube()) {
+      codeString = StringTools::printf("%s.texture = CubeTexture::New(\"%s\");\r\n",
+                                       _codeVariableName.c_str(), texture->name.c_str());
+    }
+    else {
+      codeString = StringTools::printf("%s.texture = Texture::New(\"%s\");\r\n",
+                                       _codeVariableName.c_str(), texture->name.c_str());
+    }
+    codeString += StringTools::printf("%s.texture->coordinatesMode = %u;\r\n",
+                                      _codeVariableName.c_str(), texture->coordinatesMode());
+  }
+
+  codeString
+    += StringTools::printf("%s.linkRefractionWithTransparency = %s;\r\n", _codeVariableName.c_str(),
+                           linkRefractionWithTransparency ? "true" : "false");
+  codeString += StringTools::printf("%s.invertRefractionY = %s;\r\n", _codeVariableName.c_str(),
+                                    invertRefractionY ? "true" : "false");
+
+  return codeString;
+}
+
+json RefractionBlock::serialize() const
+{
+  return nullptr;
+}
+
+void RefractionBlock::_deserialize(const json& /*serializationObject*/, Scene* /*scene*/,
+                                   const std::string& /*rootUrl*/)
+{
+}
+
+} // end of namespace BABYLON
