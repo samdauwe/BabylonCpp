743bf99514f49d9bf6d02e71e0f25f56d4094ac6
samdauwe <sam.dauwe@gmail.com>
2020-07-11 20:56:49 +0200

    Formatting

--- src/BabylonCpp/include/babylon/interfaces/idisposable.h
+++ src/BabylonCpp/include/babylon/interfaces/idisposable.h
@@ -14,8 +14,8 @@ struct BABYLON_SHARED_EXPORT IDisposable {
    * @brief Releases resources associated with this node.
    * @param doNotRecurse Set to true to not recurse into each children (recurse into each children
    * by default)
-   * @param disposeMaterialAndTextures Set to true to also dispose referenced
-   * materials and textures (false by default)
+   * @param disposeMaterialAndTextures Set to true to also dispose referenced materials and textures
+   * (false by default)
    */
   virtual void dispose(bool doNotRecurse = false, bool disposeMaterialAndTextures = false) = 0;
 
