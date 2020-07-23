d74449ec4e450c2f71fb72872433136424f89f31
samdauwe <sam.dauwe@gmail.com>
2020-07-23 21:08:22 +0200

    Adding doc

--- src/BabylonCpp/include/babylon/misc/optimization/scene_optimization.h
+++ src/BabylonCpp/include/babylon/misc/optimization/scene_optimization.h
@@ -7,15 +7,35 @@ namespace BABYLON {
 
 class Scene;
 
+/**
+ * @brief Defines the root class used to create scene optimization to use with SceneOptimizer
+ * @description More details at https://doc.babylonjs.com/how_to/how_to_use_sceneoptimizer
+ */
 class BABYLON_SHARED_EXPORT SceneOptimization {
 
 public:
+  /**
+   * @brief Creates the SceneOptimization object.
+   * @param priority defines the priority of this optimization (0 by default which means first in
+   * the list)
+   * @param desc defines the description associated with the optimization
+   */
   SceneOptimization(int priority = 0);
   virtual ~SceneOptimization(); // = default
 
+  /**
+   * @brief This function will be called by the SceneOptimizer when its priority is reached in order
+   * to apply the change required by the current optimization
+   * @param scene defines the current scene where to apply this optimization
+   * @param optimizer defines the current optimizer
+   * @returns true if everything that can be done was applied
+   */
   virtual bool apply(Scene* scene);
 
 public:
+  /**
+   * Defines the priority of this optimization (0 by default which means first in the list)
+   */
   int priority;
 
 }; // end of class SceneOptimization
