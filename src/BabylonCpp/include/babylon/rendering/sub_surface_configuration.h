4c043941d33ff96bc92ee0ca35132c3ab50356a5
samdauwe <sam.dauwe@gmail.com>
2020-07-24 19:42:52 +0200

    Added SubSurfaceConfiguration class

---
+++ src/BabylonCpp/include/babylon/rendering/sub_surface_configuration.h
@@ -0,0 +1,144 @@
+#ifndef BABYLON_RENDERING_SUB_SURFACE_CONFIGURATION_H
+#define BABYLON_RENDERING_SUB_SURFACE_CONFIGURATION_H
+
+#include <memory>
+
+#include <babylon/babylon_api.h>
+#include <babylon/babylon_common.h>
+#include <babylon/maths/color3.h>
+#include <babylon/rendering/pre_pass_effect_configuration.h>
+
+namespace BABYLON {
+
+class Scene;
+class SubSurfaceScatteringPostProcess;
+using SubSurfaceScatteringPostProcessPtr = std::shared_ptr<SubSurfaceScatteringPostProcess>;
+
+/**
+ * @brief Contains all parameters needed for the prepass to perform screen space subsurface
+ * scattering
+ */
+class BABYLON_SHARED_EXPORT SubSurfaceConfiguration : public PrePassEffectConfiguration {
+
+public:
+  /**
+   * @brief Builds a subsurface configuration object.
+   * @param scene The scene
+   */
+  SubSurfaceConfiguration(Scene* scene);
+  ~SubSurfaceConfiguration() = default;
+
+  /**
+   * @brief Adds a new diffusion profile.
+   * Useful for more realistic subsurface scattering on diverse materials.
+   * @param color The color of the diffusion profile. Should be the average color of the material.
+   * @return The index of the diffusion profile for the material subsurface configuration
+   */
+  size_t addDiffusionProfile(const Color3& color);
+
+  /**
+   * @brief Creates the sss post process.
+   * @return The created post process
+   */
+  PostProcessPtr createPostProcess() override;
+
+  /**
+   * @brief Deletes all diffusion profiles.
+   * Note that in order to render subsurface scattering, you should have at least 1 diffusion
+   * profile.
+   */
+  void clearAllDiffusionProfiles();
+
+  /**
+   * @brief Disposes this object.
+   */
+  void dispose() override;
+
+  // clang-format off
+  /**
+   * @hidden
+   * https://zero-radiance.github.io/post/sampling-diffusion/
+   *
+   * Importance sample the normalized diffuse reflectance profile for the computed value of 's'.
+   * ------------------------------------------------------------------------------------
+   * R[r, phi, s]   = s * (Exp[-r * s] + Exp[-r * s / 3]) / (8 * Pi * r)
+   * PDF[r, phi, s] = r * R[r, phi, s]
+   * CDF[r, s]      = 1 - 1/4 * Exp[-r * s] - 3/4 * Exp[-r * s / 3]
+   * ------------------------------------------------------------------------------------
+   * We importance sample the color channel with the widest scattering distance.
+   */
+  // clang-format on
+  float getDiffusionProfileParameters(const Color3& color) const;
+
+protected:
+  /**
+   * @brief Gets the diffusion profile color for subsurface scattering.
+   */
+  Float32Array& get_ssDiffusionS();
+
+  /**
+   * @brief Gets the diffusion profile max color channel value for subsurface scattering.
+   */
+  Float32Array& get_ssDiffusionD();
+
+  /**
+   * @brief Gets the diffusion profile filter radius for subsurface scattering.
+   */
+  Float32Array& get_ssFilterRadii();
+
+private:
+  /**
+   * @brief Performs sampling of a Normalized Burley diffusion profile in polar coordinates.
+   * 'u' is the random number (the value of the CDF): [0, 1).
+   * rcp(s) = 1 / ShapeParam = ScatteringDistance.
+   * Returns the sampled radial distance, s.t. (u = 0 -> r = 0) and (u = 1 -> r = Inf).
+   */
+  float _sampleBurleyDiffusionProfile(float u, float rcpS) const;
+
+public:
+  /**
+   * Post process to attach for screen space subsurface scattering
+   */
+  SubSurfaceScatteringPostProcessPtr postProcess;
+
+  /**
+   * Diffusion profile color for subsurface scattering
+   */
+  ReadOnlyProperty<SubSurfaceConfiguration, Float32Array> ssDiffusionS;
+
+  /**
+   * Diffusion profile max color channel value for subsurface scattering
+   */
+  ReadOnlyProperty<SubSurfaceConfiguration, Float32Array> ssDiffusionD;
+
+  /**
+   * Diffusion profile filter radius for subsurface scattering
+   */
+  ReadOnlyProperty<SubSurfaceConfiguration, Float32Array> ssFilterRadii;
+
+  /**
+   * Diffusion profile colors for subsurface scattering
+   * You can add one diffusion color using `addDiffusionProfile` on `scene.prePassRenderer`
+   * See ...
+   * Note that you can only store up to 5 of them
+   */
+  std::vector<Color3> ssDiffusionProfileColors;
+
+  /**
+   * Defines the ratio real world => scene units.
+   * Used for subsurface scattering
+   */
+  float metersPerUnit;
+
+private:
+  Float32Array _ssDiffusionS;
+  Float32Array _ssFilterRadii;
+  Float32Array _ssDiffusionD;
+
+  Scene* _scene;
+
+}; // end of class SubSurfaceConfiguration
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_RENDERING_SUB_SURFACE_CONFIGURATION_H
---
+++ src/BabylonCpp/src/rendering/sub_surface_configuration.cpp
@@ -0,0 +1,111 @@
+#include <babylon/rendering/sub_surface_configuration.h>
+
+#include <cmath>
+
+#include <babylon/babylon_stl_util.h>
+#include <babylon/core/logging.h>
+#include <babylon/engines/scene.h>
+#include <babylon/postprocesses/sub_surface_scattering_post_process.h>
+
+namespace BABYLON {
+
+SubSurfaceConfiguration::SubSurfaceConfiguration(Scene* scene)
+    : postProcess{nullptr}
+    , ssDiffusionS{this, &SubSurfaceConfiguration::get_ssDiffusionS}
+    , ssDiffusionD{this, &SubSurfaceConfiguration::get_ssDiffusionD}
+    , ssFilterRadii{this, &SubSurfaceConfiguration::get_ssFilterRadii}
+    , metersPerUnit{1.f}
+{
+  enabled = false;
+
+  // Adding default diffusion profile
+  addDiffusionProfile(Color3(1.f, 1.f, 1.f));
+  _scene = scene;
+}
+
+Float32Array& SubSurfaceConfiguration::get_ssDiffusionS()
+{
+  return _ssDiffusionS;
+}
+
+Float32Array& SubSurfaceConfiguration::get_ssDiffusionD()
+{
+  return _ssDiffusionD;
+}
+
+Float32Array& SubSurfaceConfiguration::get_ssFilterRadii()
+{
+  return _ssFilterRadii;
+}
+
+size_t SubSurfaceConfiguration::addDiffusionProfile(const Color3& color)
+{
+  if (_ssDiffusionD.size() >= 5) {
+    // We only suppport 5 diffusion profiles
+    BABYLON_LOG_ERROR("SubSurfaceConfiguration",
+                      "You already reached the maximum number of diffusion profiles.");
+    return 0; // default profile
+  }
+
+  // Do not add doubles
+  for (size_t i = 0; i < _ssDiffusionS.size() / 3; ++i) {
+    if (_ssDiffusionS[i * 3] == color.r        //
+        && _ssDiffusionS[i * 3 + 1] == color.g //
+        && _ssDiffusionS[i * 3 + 2] == color.b) {
+      return i;
+    }
+  }
+
+  stl_util::concat(_ssDiffusionS, {color.r, color.b, color.g});
+  _ssDiffusionD.emplace_back(std::max(std::max(color.r, color.b), color.g));
+  _ssFilterRadii.emplace_back(getDiffusionProfileParameters(color));
+  ssDiffusionProfileColors.emplace_back(color);
+
+  return _ssDiffusionD.size() - 1;
+}
+
+PostProcessPtr SubSurfaceConfiguration::createPostProcess()
+{
+  postProcess = SubSurfaceScatteringPostProcess::New("subSurfaceScattering", _scene, 1.f, nullptr,
+                                                     std::nullopt, _scene->getEngine());
+  postProcess->autoClear = false;
+
+  return postProcess;
+}
+
+void SubSurfaceConfiguration::clearAllDiffusionProfiles()
+{
+  _ssDiffusionD            = {};
+  _ssDiffusionS            = {};
+  _ssFilterRadii           = {};
+  ssDiffusionProfileColors = {};
+}
+
+void SubSurfaceConfiguration::dispose()
+{
+  clearAllDiffusionProfiles();
+  postProcess->dispose();
+}
+
+float SubSurfaceConfiguration::getDiffusionProfileParameters(const Color3& color) const
+{
+  const auto cdf                   = 0.997f;
+  const auto maxScatteringDistance = std::max(std::max(color.r, color.g), color.b);
+
+  return _sampleBurleyDiffusionProfile(cdf, maxScatteringDistance);
+}
+
+float SubSurfaceConfiguration::_sampleBurleyDiffusionProfile(float u, float rcpS) const
+{
+  u = 1.f - u; // Convert CDF to CCDF
+
+  const auto g = 1.f + (4 * u) * (2.f * u + std::sqrt(1.f + (4.f * u) * u));
+  const auto n = std::pow(g, -1.f / 3.f); // g^(-1/3)
+  const auto p = (g * n) * n;             // g^(+1/3)
+  const auto c = 1.f + p + n;             // 1 + g^(+1/3) + g^(-1/3)
+  const auto x = 3.f * std::log(c / (4.f * u));
+
+  return x * rcpS;
+}
+
+} // end of namespace BABYLON
