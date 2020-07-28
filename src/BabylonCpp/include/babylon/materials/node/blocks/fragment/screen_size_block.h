460870a7c508cbd07977522f3010b8936ca3bf71
samdauwe <sam.dauwe@gmail.com>
2020-07-28 12:59:34 +0200

    Added ScreenSizeBlock class

---
+++ src/BabylonCpp/include/babylon/materials/node/blocks/fragment/screen_size_block.h
@@ -0,0 +1,97 @@
+#ifndef BABYLON_MATERIALS_NODE_BLOCKS_FRAGMENT_SCREEN_SIZE_BLOCK_H
+#define BABYLON_MATERIALS_NODE_BLOCKS_FRAGMENT_SCREEN_SIZE_BLOCK_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/materials/node/node_material_block.h>
+
+namespace BABYLON {
+
+class ScreenSizeBlock;
+using ScreenSizeBlockPtr = std::shared_ptr<ScreenSizeBlock>;
+
+/**
+ * @brief Block used to get the screen sizes.
+ */
+class BABYLON_SHARED_EXPORT ScreenSizeBlock : public NodeMaterialBlock {
+
+public:
+  template <typename... Ts>
+  static ScreenSizeBlockPtr New(Ts&&... args)
+  {
+    return std::shared_ptr<ScreenSizeBlock>(new ScreenSizeBlock(std::forward<Ts>(args)...));
+  }
+  ~ScreenSizeBlock() override;
+
+  /**
+   * @brief Gets the current class name.
+   * @returns the class name
+   */
+  std::string getClassName() const override;
+
+  /**
+   * @brief Bind data to effect. Will only be called for blocks with isBindable === true
+   * @param effect defines the effect to bind data to
+   * @param nodeMaterial defines the hosting NodeMaterial
+   * @param mesh defines the mesh that will be rendered
+   * @param subMesh defines the submesh that will be rendered
+   */
+  void bind(const EffectPtr& effect, const NodeMaterialPtr& nodeMaterial, Mesh* mesh = nullptr,
+            SubMesh* subMesh = nullptr) override;
+
+protected:
+  /**
+   * @brief Creates a new ScreenSizeBlock.
+   * @param name defines the block name
+   */
+  ScreenSizeBlock(const std::string& name);
+
+  /**
+   * @brief Gets the xy component.
+   */
+  NodeMaterialConnectionPointPtr& get_xy();
+
+  /**
+   * @brief Gets the x component.
+   */
+  NodeMaterialConnectionPointPtr& get_x();
+
+  /**
+   * @brief Gets the y component.
+   */
+  NodeMaterialConnectionPointPtr& get_y();
+
+  /**
+   * @brief Hidden
+   */
+  std::string writeOutputs(NodeMaterialBuildState& state, const std::string& varName);
+
+  /**
+   * @brief Hidden
+   */
+  ScreenSizeBlock& _buildBlock(NodeMaterialBuildState& state) override;
+
+public:
+  /**
+   * Gets the xy component
+   */
+  ReadOnlyProperty<ScreenSizeBlock, NodeMaterialConnectionPointPtr> xy;
+
+  /**
+   * Gets the x component
+   */
+  ReadOnlyProperty<ScreenSizeBlock, NodeMaterialConnectionPointPtr> x;
+
+  /**
+   * Gets the y component
+   */
+  ReadOnlyProperty<ScreenSizeBlock, NodeMaterialConnectionPointPtr> y;
+
+private:
+  std::string _varName;
+  Scene* _scene;
+
+}; // end of class ScreenSizeBlock
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_NODE_BLOCKS_FRAGMENT_SCREEN_SIZE_BLOCK_H
---
+++ src/BabylonCpp/src/materials/node/blocks/fragment/screen_size_block.cpp
@@ -0,0 +1,93 @@
+#include <babylon/materials/node/blocks/fragment/screen_size_block.h>
+
+#include <babylon/engines/engine.h>
+#include <babylon/engines/scene.h>
+#include <babylon/materials/effect.h>
+#include <babylon/materials/node/node_material_build_state.h>
+#include <babylon/materials/node/node_material_build_state_shared_data.h>
+#include <babylon/materials/node/node_material_connection_point.h>
+#include <babylon/misc/string_tools.h>
+
+namespace BABYLON {
+
+ScreenSizeBlock::ScreenSizeBlock(const std::string& iName)
+    : NodeMaterialBlock{iName, NodeMaterialBlockTargets::Fragment}
+    , xy{this, &ScreenSizeBlock::get_xy}
+    , x{this, &ScreenSizeBlock::get_x}
+    , y{this, &ScreenSizeBlock::get_y}
+    , _scene{nullptr}
+{
+  registerOutput("xy", NodeMaterialBlockConnectionPointTypes::Vector2,
+                 NodeMaterialBlockTargets::Fragment);
+  registerOutput("x", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Fragment);
+  registerOutput("y", NodeMaterialBlockConnectionPointTypes::Float,
+                 NodeMaterialBlockTargets::Fragment);
+}
+
+ScreenSizeBlock::~ScreenSizeBlock() = default;
+
+std::string ScreenSizeBlock::getClassName() const
+{
+  return "ScreenSizeBlock";
+}
+
+NodeMaterialConnectionPointPtr& ScreenSizeBlock::get_xy()
+{
+  return _outputs[0];
+}
+
+NodeMaterialConnectionPointPtr& ScreenSizeBlock::get_x()
+{
+  return _outputs[1];
+}
+
+NodeMaterialConnectionPointPtr& ScreenSizeBlock::get_y()
+{
+  return _outputs[2];
+}
+
+void ScreenSizeBlock::bind(const EffectPtr& effect, const NodeMaterialPtr& /*nodeMaterial*/,
+                           Mesh* /*mesh*/, SubMesh* /*subMesh*/)
+{
+  const auto engine = _scene->getEngine();
+
+  effect->setFloat2(_varName, static_cast<float>(engine->getRenderWidth()),
+                    static_cast<float>(engine->getRenderWidth()));
+}
+
+std::string ScreenSizeBlock::writeOutputs(NodeMaterialBuildState& state, const std::string& varName)
+{
+  std::string code;
+
+  for (const auto& output : _outputs) {
+    if (output->hasEndpoints()) {
+      code += StringTools::printf("%s = %s.%s;\r\n", _declareOutput(output, state).c_str(),
+                                  varName.c_str(), output->name.c_str());
+    }
+  }
+
+  return code;
+}
+
+ScreenSizeBlock& ScreenSizeBlock::_buildBlock(NodeMaterialBuildState& state)
+{
+  NodeMaterialBlock::_buildBlock(state);
+
+  _scene = state.sharedData->scene;
+
+  if (state.target == NodeMaterialBlockTargets::Vertex) {
+    throw std::runtime_error("ScreenSizeBlock must only be used in a fragment shader");
+  }
+
+  state.sharedData->bindableBlocks.emplace_back(shared_from_this());
+
+  _varName = state._getFreeVariableName("screenSize");
+  state._emitUniformFromString(_varName, "vec2");
+
+  state.compilationString += writeOutputs(state, _varName);
+
+  return *this;
+}
+
+} // end of namespace BABYLON
