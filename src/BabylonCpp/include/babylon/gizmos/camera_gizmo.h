47b169beaa1c633c6ff54d7d9d204efe9fa8c2db
samdauwe <sam.dauwe@gmail.com>
2020-08-26 20:32:29 +0200

    Added CameraGizmo class

---
+++ src/BabylonCpp/include/babylon/gizmos/camera_gizmo.h
@@ -0,0 +1,99 @@
+#ifndef BABYLON_GIZMOS_CAMERA_GIZMO_H
+#define BABYLON_GIZMOS_CAMERA_GIZMO_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/gizmos/gizmo.h>
+#include <babylon/rendering/utility_layer_renderer.h>
+
+namespace BABYLON {
+
+class StandardMaterial;
+using StandardMaterialPtr = std::shared_ptr<StandardMaterial>;
+
+/**
+ * @brief Gizmo that enables viewing a camera.
+ */
+class BABYLON_SHARED_EXPORT CameraGizmo : public Gizmo {
+
+public:
+  /**
+   * @brief Creates a CameraGizmo.
+   * @param gizmoLayer The utility layer the gizmo will be added to
+   */
+  CameraGizmo(const UtilityLayerRendererPtr& gizmoLayer
+              = UtilityLayerRenderer::DefaultUtilityLayer());
+  ~CameraGizmo() override; // = default
+
+  /**
+   * @brief Disposes of the light gizmo.
+   */
+  void dispose(bool doNotRecurse = false, bool disposeMaterialAndTextures = false) override;
+
+protected:
+  /**
+   * @brief Updates the gizmo to match the attached mesh's position/rotation.
+   * Hidden
+   */
+  void _update();
+
+protected:
+  /**
+   * @brief Gets a boolean indicating if frustum lines must be rendered (true by default)).
+   */
+  bool get_displayFrustum() const;
+
+  /**
+   * @brief Sets a boolean indicating if frustum lines must be rendered (true by default)).
+   */
+  void set_displayFrustum(bool value);
+
+  /**
+   * @brief Gets the camera that the gizmo is attached to.
+   */
+  CameraPtr& get_camera();
+
+  /**
+   * @brief Sets the camera that the gizmo is attached to.
+   */
+  void set_camera(const CameraPtr& camera);
+
+  /**
+   * @brief Gets the material used to render the light gizmo.
+   */
+  StandardMaterialPtr& get_material();
+
+private:
+  static MeshPtr _CreateCameraMesh(Scene* scene);
+  static MeshPtr _CreateCameraFrustum(Scene* scene);
+
+public:
+  /**
+   * Gets or sets a boolean indicating if frustum lines must be rendered (true by default))
+   */
+  Property<CameraGizmo, bool> displayFrustum;
+
+  /**
+   * The camera that the gizmo is attached to
+   */
+  Property<CameraGizmo, CameraPtr> camera;
+
+  /**
+   * Gets the material used to render the camera gizmo
+   */
+  ReadOnlyProperty<CameraGizmo, StandardMaterialPtr> material;
+
+private:
+  MeshPtr _cameraMesh;
+  MeshPtr _cameraLinesMesh;
+  StandardMaterialPtr _material;
+  CameraPtr _camera;
+
+  // Static helper methods
+  static float _Scale;
+  Matrix _invProjection;
+
+}; // end of class CameraGizmo
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_GIZMOS_CAMERA_GIZMO_H
---
+++ src/BabylonCpp/src/gizmos/camera_gizmo.cpp
@@ -0,0 +1,213 @@
+#include <babylon/gizmos/camera_gizmo.h>
+
+#include <babylon/babylon_stl_util.h>
+#include <babylon/cameras/camera.h>
+#include <babylon/engines/scene.h>
+#include <babylon/lights/hemispheric_light.h>
+#include <babylon/materials/standard_material.h>
+#include <babylon/meshes/builders/box_builder.h>
+#include <babylon/meshes/builders/cylinder_builder.h>
+#include <babylon/meshes/builders/lines_builder.h>
+#include <babylon/meshes/builders/mesh_builder_options.h>
+#include <babylon/meshes/lines_mesh.h>
+#include <babylon/meshes/mesh.h>
+
+namespace BABYLON {
+
+float CameraGizmo::_Scale = 0.05f;
+
+CameraGizmo::CameraGizmo(const UtilityLayerRendererPtr& gizmoLayer)
+    : Gizmo{gizmoLayer}
+    , displayFrustum{this, &CameraGizmo::get_displayFrustum, &CameraGizmo::set_displayFrustum}
+    , camera{this, &CameraGizmo::get_camera, &CameraGizmo::set_camera}
+    , material{this, &CameraGizmo::get_material}
+    , _cameraMesh{nullptr}
+    , _cameraLinesMesh{nullptr}
+    , _camera{nullptr}
+{
+  _material = StandardMaterial::New("cameraGizmoMaterial", gizmoLayer->utilityLayerScene.get());
+  _material->diffuseColor  = Color3(0.5f, 0.5f, 0.5f);
+  _material->specularColor = Color3(0.1f, 0.1f, 0.1f);
+}
+
+CameraGizmo::~CameraGizmo() = default;
+
+bool CameraGizmo::get_displayFrustum() const
+{
+  return _cameraLinesMesh->isEnabled();
+}
+
+void CameraGizmo::set_displayFrustum(bool value)
+{
+  _cameraLinesMesh->setEnabled(value);
+}
+
+void CameraGizmo::set_camera(const CameraPtr& camera)
+{
+  _camera      = camera;
+  attachedNode = camera;
+  if (camera) {
+    // Create the mesh for the given camera
+    if (_cameraMesh) {
+      _cameraMesh->dispose();
+    }
+    if (_cameraLinesMesh) {
+      _cameraLinesMesh->dispose();
+    }
+    _cameraMesh      = CameraGizmo::_CreateCameraMesh(gizmoLayer->utilityLayerScene.get());
+    _cameraLinesMesh = CameraGizmo::_CreateCameraFrustum(gizmoLayer->utilityLayerScene.get());
+
+    for (const auto& m : _cameraMesh->getChildMeshes(false)) {
+      m->material = _material;
+    }
+    _cameraMesh->parent = _rootMesh.get();
+
+    _cameraLinesMesh->parent = _rootMesh.get();
+
+    if (gizmoLayer->utilityLayerScene->activeCamera()
+        && gizmoLayer->utilityLayerScene->activeCamera()->maxZ < camera->maxZ * 1.5f) {
+      gizmoLayer->utilityLayerScene->activeCamera()->maxZ = camera->maxZ * 1.5f;
+    }
+
+    /*if (!attachedNode->reservedDataStore) {
+        attachedNode!.reservedDataStore = {};
+    }
+    attachedNode!.reservedDataStore.cameraGizmo = this;*/
+
+    // Add lighting to the camera gizmo
+    auto gizmoLight = gizmoLayer->_getSharedGizmoLight();
+    gizmoLight->includedOnlyMeshes
+      = stl_util::concat(gizmoLight->includedOnlyMeshes(), _cameraMesh->getChildMeshes(false));
+
+    _update();
+  }
+}
+
+CameraPtr& CameraGizmo::get_camera()
+{
+  return _camera;
+}
+
+StandardMaterialPtr& CameraGizmo::get_material()
+{
+  return _material;
+}
+
+void CameraGizmo::_update()
+{
+  Gizmo::_update();
+  if (!_camera) {
+    return;
+  }
+
+  // frustum matrix
+  _camera->getProjectionMatrix().invertToRef(_invProjection);
+  _cameraLinesMesh->setPivotMatrix(_invProjection, false);
+
+  _cameraLinesMesh->scaling().x = 1.f / _rootMesh->scaling().x;
+  _cameraLinesMesh->scaling().y = 1.f / _rootMesh->scaling().y;
+  _cameraLinesMesh->scaling().z = 1.f / _rootMesh->scaling().z;
+}
+
+void CameraGizmo::dispose(bool /*doNotRecurse*/, bool /*disposeMaterialAndTextures*/)
+{
+  if (_cameraMesh) {
+    _cameraMesh->dispose();
+  }
+  if (_cameraLinesMesh) {
+    _cameraLinesMesh->dispose();
+  }
+  _material->dispose();
+  Gizmo::dispose();
+}
+
+MeshPtr CameraGizmo::_CreateCameraMesh(Scene* scene)
+{
+  auto root = Mesh::New("rootCameraGizmo", scene);
+
+  auto mesh    = Mesh::New(root->name, scene);
+  mesh->parent = root.get();
+
+  BoxOptions boxOptions;
+  boxOptions.width  = 1.f;
+  boxOptions.height = 0.8f;
+  boxOptions.depth  = 0.5f;
+  auto box          = BoxBuilder::CreateBox(root->name, boxOptions, scene);
+  box->parent       = mesh.get();
+
+  CylinderOptions cyl1Options;
+  cyl1Options.height         = 0.5f;
+  cyl1Options.diameterTop    = 0.8f;
+  cyl1Options.diameterBottom = 0.8f;
+  auto cyl1                  = CylinderBuilder::CreateCylinder(root->name, cyl1Options, scene);
+  cyl1->parent               = mesh.get();
+  cyl1->position().y         = 0.3f;
+  cyl1->position().x         = -0.6f;
+  cyl1->rotation().x         = Math::PI * 0.5f;
+
+  CylinderOptions cyl2Options;
+  cyl2Options.height         = 0.5f;
+  cyl1Options.diameterTop    = 0.6f;
+  cyl1Options.diameterBottom = 0.6f;
+  auto cyl2                  = CylinderBuilder::CreateCylinder(root->name, cyl2Options, scene);
+  cyl2->parent               = mesh.get();
+  cyl2->position().y         = 0.5f;
+  cyl2->position().x         = 0.4f;
+  cyl2->rotation().x         = Math::PI * 0.5f;
+
+  CylinderOptions cyl3Options;
+  cyl3Options.height         = 0.5f;
+  cyl1Options.diameterTop    = 0.5f;
+  cyl1Options.diameterBottom = 0.5f;
+  auto cyl3                  = CylinderBuilder::CreateCylinder(root->name, cyl3Options, scene);
+  cyl3->parent               = mesh.get();
+  cyl3->position().y         = 0.f;
+  cyl3->position().x         = 0.6f;
+  cyl3->rotation().z         = Math::PI * 0.5f;
+
+  root->scaling().scaleInPlace(CameraGizmo::_Scale);
+  root->rotation().y = -Math::PI * 0.5f;
+  mesh->position().x = -0.9f;
+
+  return root;
+}
+
+MeshPtr CameraGizmo::_CreateCameraFrustum(Scene* scene)
+{
+  auto root    = Mesh::New("rootCameraGizmo", scene);
+  auto mesh    = Mesh::New(root->name, scene);
+  mesh->parent = root.get();
+
+  for (unsigned y = 0; y < 4; y += 2) {
+    for (unsigned x = 0; x < 4; x += 2) {
+      {
+        LinesOptions options;
+        options.points = {Vector3(-1.f + x, -1.f + y, -1.f), Vector3(-1.f + x, -1.f + y, 1.f)};
+        auto line      = LinesBuilder::CreateLines("lines", options, scene);
+        line->parent   = mesh.get();
+        line->alwaysSelectAsActiveMesh = true;
+        line->isPickable               = false;
+      }
+      {
+        LinesOptions options;
+        options.points = {Vector3(-1.f, -1.f + x, -1.f + y), Vector3(1.f, -1.f + x, -1.f + y)};
+        auto line      = LinesBuilder::CreateLines("lines", options, scene);
+        line->parent   = mesh.get();
+        line->alwaysSelectAsActiveMesh = true;
+        line->isPickable               = false;
+      }
+      {
+        LinesOptions options;
+        options.points = {Vector3(-1.f + x, -1.f, -1.f + y), Vector3(-1.f + x, 1.f, -1.f + y)};
+        auto line      = LinesBuilder::CreateLines("lines", options, scene);
+        line->parent   = mesh.get();
+        line->alwaysSelectAsActiveMesh = true;
+        line->isPickable               = false;
+      }
+    }
+  }
+
+  return root;
+}
+
+} // end of namespace BABYLON
