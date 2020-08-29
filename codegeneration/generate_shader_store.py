2145250288a08d2209d9424b2be1de87e8d9e4bb
samdauwe <sam.dauwe@gmail.com>
2020-08-29 19:55:48 +0200

    Updated shader generator script to v4.2.0-alpha.32

--- codegeneration/generate_shader_store.py
+++ codegeneration/generate_shader_store.py
@@ -431,7 +431,7 @@ if __name__ == "__main__":
     # set default values when input is missing
     if not options.inputDir:
         options.inputDir = os.path.join(os.getcwd(), "..", "..", "..",
-                                    "Projects", "Babylon.js-4.2.0-alpha.17_2020_05_20")
+                                    "Projects", "Babylon.js-4.2.0-alpha.32_2020_08_12")
         args += [options.inputDir]
     if not options.outputDir:
         options.outputDir = os.path.join(os.getcwd(), "..", "src", "BabylonCpp")
