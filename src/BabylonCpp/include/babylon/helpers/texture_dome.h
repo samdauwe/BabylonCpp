2da6f905b97b607832f338a0678540c45bb851df
samdauwe <sam.dauwe@gmail.com>
2020-09-05 10:47:31 +0200

    Added TextureDome class

---
+++ src/BabylonCpp/include/babylon/helpers/texture_dome.h
@@ -0,0 +1,228 @@
+#ifndef BABYLON_HELPERS_TEXTURE_DOME_H
+#define BABYLON_HELPERS_TEXTURE_DOME_H
+
+#include <babylon/babylon_api.h>
+#include <babylon/meshes/transform_node.h>
+
+namespace BABYLON {
+
+class BackgroundMaterial;
+class Mesh;
+class Texture;
+struct TextureDomeOptions;
+using BackgroundMaterialPtr = std::shared_ptr<BackgroundMaterial>;
+using MeshPtr               = std::shared_ptr<Mesh>;
+using TexturePtr            = std::shared_ptr<Texture>;
+
+/**
+ * @brief Display a 360/180 degree texture on an approximately spherical surface, useful for VR
+ * applications or skyboxes. As a subclass of TransformNode, this allow parenting to the camera or
+ * multiple textures with different locations in the scene. This class achieves its effect with a
+ * Texture and a correctly configured BackgroundMaterial on an inverted sphere. Potential additions
+ * to this helper include zoom and and non-infinite distance rendering effects.
+ */
+class BABYLON_SHARED_EXPORT TextureDome : public TransformNode {
+
+public:
+  /**
+   * Define the image as a Monoscopic panoramic 360 image.
+   */
+  static constexpr unsigned int MODE_MONOSCOPIC = 0;
+  /**
+   * Define the image as a Stereoscopic TopBottom/OverUnder panoramic 360 image.
+   */
+  static constexpr unsigned int MODE_TOPBOTTOM = 1;
+  /**
+   * Define the image as a Stereoscopic Side by Side panoramic 360 image.
+   */
+  static constexpr unsigned int MODE_SIDEBYSIDE = 2;
+
+public:
+  /**
+   * @brief Create an instance of this class and pass through the parameters to the relevant
+   * classes- Texture, StandardMaterial, and Mesh.
+   * @param name Element's name, child elements will append suffixes for their own names.
+   * @param textureUrlOrElement defines the url(s) or the (video) HTML element to use
+   * @param options An object containing optional or exposed sub element properties
+   */
+  TextureDome(
+    const std::string& name, const std::string& textureUrlOrElement, TextureDomeOptions& options,
+    Scene* scene,
+    const std::function<void(const std::string& message, const std::string& exception)>& onError
+    = nullptr);
+  ~TextureDome(); // = default
+
+  /**
+   * @brief Hidden
+   */
+  void initializeTextureDome(const std::string& name, const std::string& textureUrlOrElement,
+                             TextureDomeOptions& options, Scene* scene);
+
+  /**
+   * @brief Releases resources associated with this node.
+   * @param doNotRecurse Set to true to not recurse into each children (recurse into each children
+   * by default)
+   * @param disposeMaterialAndTextures Set to true to also dispose referenced materials and textures
+   * (false by default)
+   */
+  void dispose(bool doNotRecurse = false, bool disposeMaterialAndTextures = false) override;
+
+protected:
+  /**
+   * @brief Gets the texture being displayed on the sphere.
+   */
+  TexturePtr& get_texture();
+
+  /**
+   * @brief Sets the texture being displayed on the sphere.
+   */
+  void set_texture(const TexturePtr& newTexture);
+
+  /**
+   * @brief Gets the mesh used for the dome.
+   */
+  MeshPtr& get_mesh();
+
+  /**
+   * @brief Gets the current fov(field of view) multiplier, 0.0 - 2.0. Defaults to 1.0. Lower values
+   * "zoom in" and higher values "zoom out". Also see the options.resolution property.
+   */
+  float get_fovMultiplier() const;
+
+  /**
+   * @brief Sets the current fov(field of view) multiplier, 0.0 - 2.0. Defaults to 1.0. Lower values
+   * "zoom in" and higher values "zoom out". Also see the options.resolution property.
+   */
+  void set_fovMultiplier(float value);
+
+  /**
+   * @brief Gets the current texture mode for the texture. It can be:
+   * * TextureDome.MODE_MONOSCOPIC : Define the texture source as a Monoscopic panoramic 360.
+   * * TextureDome.MODE_TOPBOTTOM  : Define the texture source as a Stereoscopic TopBottom/OverUnder
+   * panoramic 360.
+   * * TextureDome.MODE_SIDEBYSIDE : Define the texture source as a Stereoscopic Side by Side
+   * panoramic 360.
+   */
+  unsigned int get_textureMode() const;
+
+  /**
+   * @brief Sets the current texture mode for the texture. It can be:
+   * * TextureDome.MODE_MONOSCOPIC : Define the texture source as a Monoscopic panoramic 360.
+   * * TextureDome.MODE_TOPBOTTOM  : Define the texture source as a Stereoscopic TopBottom/OverUnder
+   * panoramic 360.
+   * * TextureDome.MODE_SIDEBYSIDE : Define the texture source as a Stereoscopic Side by Side
+   * panoramic 360.
+   */
+  void set_textureMode(unsigned int value);
+
+  /**
+   * @brief Is it a 180 degrees dome (half dome) or 360 texture (full dome).
+   */
+  bool get_halfDome() const;
+
+  /**
+   * @brief Set the halfDome mode. If set, only the front (180 degrees) will be displayed and the
+   * back will be blacked out.
+   */
+  void set_halfDome(bool enabled);
+
+  /**
+   * @brief Hidden
+   */
+  virtual TexturePtr _initTexture(const std::string& urlsOrElement, Scene* scene,
+                                  const TextureDomeOptions& options);
+
+private:
+  void _changeTextureMode(unsigned int value);
+
+public:
+  /**
+   * The texture being displayed on the sphere
+   */
+  Property<TextureDome, TexturePtr> texture;
+
+  /**
+   * Gets the mesh used for the dome
+   */
+  ReadOnlyProperty<TextureDome, MeshPtr> mesh;
+
+  /**
+   * The current fov(field of view) multiplier, 0.0 - 2.0. Defaults to 1.0. Lower values "zoom in"
+   * and higher values "zoom out". Also see the options.resolution property.
+   */
+  Property<TextureDome, float> fovMultiplier;
+
+  /**
+   * Gets or set the current texture mode for the texture. It can be:
+   * * TextureDome.MODE_MONOSCOPIC : Define the texture source as a Monoscopic panoramic 360.
+   * * TextureDome.MODE_TOPBOTTOM  : Define the texture source as a Stereoscopic TopBottom/OverUnder
+   * panoramic 360.
+   * * TextureDome.MODE_SIDEBYSIDE : Define the texture source as a Stereoscopic Side by Side
+   * panoramic 360.
+   */
+  Property<TextureDome, unsigned int> textureMode;
+
+  /**
+   * Is it a 180 degrees dome (half dome) or 360 texture (full dome)
+   */
+  Property<TextureDome, bool> halfDome;
+
+protected:
+  /**
+   * Hidden
+   */
+  bool _useDirectMapping;
+
+  /**
+   * The texture being displayed on the sphere
+   */
+  TexturePtr _texture;
+
+  /**
+   * The skybox material
+   */
+  BackgroundMaterialPtr _material;
+
+  /**
+   * The surface used for the dome
+   */
+  MeshPtr _mesh;
+
+  /**
+   * The current texture mode for the texture. It can be:
+   * * TextureDome.MODE_MONOSCOPIC : Define the texture source as a Monoscopic panoramic 360.
+   * * TextureDome.MODE_TOPBOTTOM  : Define the texture source as a Stereoscopic TopBottom/OverUnder
+   * panoramic 360.
+   * * TextureDome.MODE_SIDEBYSIDE : Define the texture source as a Stereoscopic Side by Side
+   * panoramic 360.
+   */
+  unsigned int _textureMode;
+
+  /**
+   * Observable raised when an error occured while loading the 360 image
+   */
+  Observable<std::string> onLoadErrorObservable;
+
+  /**
+   * Hidden
+   */
+  std::function<void(const std::string& message, const std::string& exception)> onError;
+
+private:
+  bool _halfDome;
+
+  /**
+   * A mesh that will be used to mask the back of the dome in case it is a 180 degree movie.
+   */
+  MeshPtr _halfDomeMask;
+
+  /**
+   * Oberserver used in Stereoscopic VR Mode.
+   */
+  Observer<Camera>::Ptr _onBeforeCameraRenderObserver;
+
+}; // end of class PhotoDome
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_HELPERS_TEXTURE_DOME_H
---
+++ src/BabylonCpp/src/helpers/texture_dome.cpp
@@ -0,0 +1,237 @@
+#include <babylon/helpers/texture_dome.h>
+
+#include <babylon/cameras/camera.h>
+#include <babylon/engines/scene.h>
+#include <babylon/helpers/texture_dome_options.h>
+#include <babylon/materials/background/background_material.h>
+#include <babylon/materials/textures/texture.h>
+#include <babylon/meshes/builders/mesh_builder_options.h>
+#include <babylon/meshes/builders/sphere_builder.h>
+#include <babylon/meshes/mesh.h>
+
+namespace BABYLON {
+
+TextureDome::TextureDome(
+  const std::string& iName, const std::string& /*textureUrlOrElement*/,
+  TextureDomeOptions& /*options*/, Scene* scene,
+  const std::function<void(const std::string& message, const std::string& exception)>& iOnError)
+    : TransformNode{iName, scene}
+    , texture{this, &TextureDome::get_texture, &TextureDome::set_texture}
+    , mesh{this, &TextureDome::get_mesh}
+    , fovMultiplier{this, &TextureDome::get_fovMultiplier, &TextureDome::set_fovMultiplier}
+    , textureMode{this, &TextureDome::get_textureMode, &TextureDome::set_textureMode}
+    , halfDome{this, &TextureDome::get_halfDome, &TextureDome::set_halfDome}
+    , _useDirectMapping{false}
+    , _texture{nullptr}
+    , _material{nullptr}
+    , _mesh{nullptr}
+    , _textureMode{TextureDome::MODE_MONOSCOPIC}
+    , onError{iOnError}
+    , _halfDome{false}
+    , _halfDomeMask{nullptr}
+    , _onBeforeCameraRenderObserver{nullptr}
+{
+}
+
+TextureDome::~TextureDome() = default;
+
+void TextureDome::initializeTextureDome(const std::string& iName,
+                                        const std::string& textureUrlOrElement,
+                                        TextureDomeOptions& options, Scene* scene)
+{
+  scene = getScene();
+
+  // set defaults and manage values
+  name                = !iName.empty() ? iName : "textureDome";
+  options.resolution  = options.resolution.value_or(32u);
+  options.clickToPlay = options.clickToPlay.value_or(false);
+  options.autoPlay    = options.autoPlay.value_or(true);
+  options.loop        = options.loop.value_or(true);
+  options.size        = options.size.value_or(
+    scene->activeCamera() ? static_cast<unsigned int>(scene->activeCamera()->maxZ * 0.48f) : 1000u);
+
+  if (!options.useDirectMapping) {
+    _useDirectMapping = true;
+  }
+  else {
+    _useDirectMapping = *options.useDirectMapping;
+  }
+
+  if (!options.faceForward) {
+    options.faceForward = true;
+  }
+
+  _setReady(false);
+  _mesh = Mesh::CreateSphere(name + "_mesh", *options.resolution, *options.size, scene, false,
+                             Mesh::BACKSIDE);
+  // configure material
+  auto material = (_material = BackgroundMaterial::New(name + "_material", scene));
+  material->useEquirectangularFOV = true;
+  material->fovMultiplier         = 1.f;
+  material->opacityFresnel        = false;
+
+  const auto iTexture = _initTexture(textureUrlOrElement, scene, options);
+  texture             = iTexture;
+
+  // configure mesh
+  _mesh->material = material;
+  _mesh->parent   = this;
+
+  // create a (disabled until needed) mask to cover unneeded segments of 180 texture.
+  SphereOptions halfDomeMaskOptions;
+  halfDomeMaskOptions.slice           = 0.5f;
+  halfDomeMaskOptions.diameter        = *options.size * 0.98f;
+  halfDomeMaskOptions.segments        = *options.resolution * 2u;
+  halfDomeMaskOptions.sideOrientation = Mesh::BACKSIDE;
+  _halfDomeMask                       = SphereBuilder::CreateSphere("", halfDomeMaskOptions, scene);
+  _halfDomeMask->rotate(Axis::X(), -Math::PI_2);
+  // set the parent, so it will always be positioned correctly AND will be disposed when the main
+  // sphere is disposed
+  _halfDomeMask->parent = _mesh.get();
+  _halfDome             = !!options.halfDomeMode;
+  // enable or disable according to the settings
+  _halfDomeMask->setEnabled(_halfDome);
+
+  // create
+  _texture->anisotropicFilteringLevel = 1;
+  _texture->onLoadObservable().addOnce(
+    [this](Texture* /*textre*/, EventState & /*es*/) -> void { _setReady(true); });
+
+  // Initial rotation
+  if (options.faceForward.value_or(false) && scene->activeCamera()) {
+    const auto camera = scene->activeCamera();
+
+    auto iForward  = Vector3::Forward();
+    auto direction = Vector3::TransformNormal(iForward, camera->getViewMatrix());
+    direction.normalize();
+
+    rotation().y = std::acos(Vector3::Dot(iForward, direction));
+  }
+
+  _changeTextureMode(_textureMode);
+}
+
+TexturePtr& TextureDome::get_texture()
+{
+  return _texture;
+}
+
+void TextureDome::set_texture(const TexturePtr& newTexture)
+{
+  if (_texture == newTexture) {
+    return;
+  }
+  _texture = newTexture;
+  if (_useDirectMapping) {
+    _texture->wrapU           = TextureConstants::CLAMP_ADDRESSMODE;
+    _texture->wrapV           = TextureConstants::CLAMP_ADDRESSMODE;
+    _material->diffuseTexture = _texture;
+  }
+  else {
+    _texture->coordinatesMode
+      = TextureConstants::FIXED_EQUIRECTANGULAR_MIRRORED_MODE; // matches orientation
+    _texture->wrapV              = TextureConstants::CLAMP_ADDRESSMODE;
+    _material->reflectionTexture = _texture;
+  }
+}
+
+MeshPtr& TextureDome::get_mesh()
+{
+  return _mesh;
+}
+
+float TextureDome::get_fovMultiplier() const
+{
+  return _material->fovMultiplier();
+}
+
+void TextureDome::set_fovMultiplier(float value)
+{
+  _material->fovMultiplier = value;
+}
+
+unsigned int TextureDome::get_textureMode() const
+{
+  return _textureMode;
+}
+
+void TextureDome::set_textureMode(unsigned int value)
+{
+  if (_textureMode == value) {
+    return;
+  }
+
+  _changeTextureMode(value);
+}
+
+bool TextureDome::get_halfDome() const
+{
+  return _halfDome;
+}
+
+void TextureDome::set_halfDome(bool enabled)
+{
+  _halfDome = enabled;
+  _halfDomeMask->setEnabled(enabled);
+}
+
+TexturePtr TextureDome::_initTexture(const std::string& /*urlsOrElement*/, Scene* /*scene*/,
+                                     const TextureDomeOptions& /*options*/)
+{
+  return nullptr;
+}
+
+void TextureDome::_changeTextureMode(unsigned int value)
+{
+  _scene->onBeforeCameraRenderObservable.remove(_onBeforeCameraRenderObserver);
+  _textureMode = value;
+
+  // Default Setup and Reset.
+  _texture->uScale  = 1.f;
+  _texture->vScale  = 1.f;
+  _texture->uOffset = 0.f;
+  _texture->vOffset = 0.f;
+
+  switch (value) {
+    case TextureDome::MODE_MONOSCOPIC: {
+      if (_halfDome) {
+        _texture->uScale  = 2.f;
+        _texture->uOffset = -1.f;
+      }
+    } break;
+    case TextureDome::MODE_SIDEBYSIDE: {
+      // in half-dome mode the uScale should be double of 360 texture
+      // Use 0.99999 to boost perf by not switching program
+      _texture->uScale              = _halfDome ? 0.99999f : 0.5f;
+      _onBeforeCameraRenderObserver = _scene->onBeforeCameraRenderObservable.add(
+        [this](Camera* camera, EventState & /*es*/) -> void {
+          const auto rightOffset = _halfDome ? 0.f : 0.5f;
+          const auto leftOffset  = _halfDome ? 0.5f : 0.f;
+          _texture->uOffset      = camera->isRightCamera() ? rightOffset : leftOffset;
+        });
+    } break;
+    case TextureDome::MODE_TOPBOTTOM: {
+      // in half-dome mode the vScale should be double of 360 texture
+      // Use 0.99999 to boost perf by not switching program
+      _texture->vScale              = _halfDome ? 0.99999f : 0.5f;
+      _onBeforeCameraRenderObserver = _scene->onBeforeCameraRenderObservable.add(
+        [this](Camera* camera, EventState & /*es*/) -> void {
+          _texture->vOffset = camera->isRightCamera() ? 0.5f : 0.f;
+        });
+    } break;
+  }
+}
+
+void TextureDome::dispose(bool doNotRecurse, bool disposeMaterialAndTextures)
+{
+  _texture->dispose();
+  _mesh->dispose();
+  _material->dispose();
+
+  _scene->onBeforeCameraRenderObservable.remove(_onBeforeCameraRenderObserver);
+  onLoadErrorObservable.clear();
+
+  TransformNode::dispose(doNotRecurse, disposeMaterialAndTextures);
+}
+
+} // end of namespace BABYLON
