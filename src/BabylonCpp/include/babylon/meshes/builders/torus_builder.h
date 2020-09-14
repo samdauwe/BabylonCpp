ddbc33be1e42b23bbd83e9f9947a444a5259a1d5
samdauwe <sam.dauwe@gmail.com>
2020-09-14 21:18:00 +0200

    Formatting doc

--- src/BabylonCpp/include/babylon/meshes/builders/torus_builder.h
+++ src/BabylonCpp/include/babylon/meshes/builders/torus_builder.h
@@ -18,26 +18,22 @@ using MeshPtr = std::shared_ptr<Mesh>;
  */
 struct BABYLON_SHARED_EXPORT TorusBuilder {
 
+  // clang-format off
   /**
    * @brief Creates a torus mesh.
    * * The parameter `diameter` sets the diameter size (float) of the torus (default 1)
-   * * The parameter `thickness` sets the diameter size of the tube of the torus (float, default
-   * 0.5)
+   * * The parameter `thickness` sets the diameter size of the tube of the torus (float, default 0.5)
    * * The parameter `tessellation` sets the number of torus sides (postive integer, default 16)
-   * * You can also set the mesh side orientation with the values : BABYLON.Mesh.FRONTSIDE
-   * (default), BABYLON.Mesh.BACKSIDE or BABYLON.Mesh.DOUBLESIDE
-   * * If you create a double-sided mesh, you can choose what parts of the texture image to crop and
-   * stick respectively on the front and the back sides with the parameters `frontUVs` and `backUVs`
-   * (Vector4). Detail here :
-   * https://doc.babylonjs.com/babylon101/discover_basic_elements#side-orientation
-   * * The mesh can be set to updatable with the boolean parameter `updatable` (default false) if
-   * its internal geometry is supposed to change once created.
+   * * You can also set the mesh side orientation with the values : BABYLON.Mesh.FRONTSIDE (default), BABYLON.Mesh.BACKSIDE or BABYLON.Mesh.DOUBLESIDE
+   * * If you create a double-sided mesh, you can choose what parts of the texture image to crop and stick respectively on the front and the back sides with the parameters `frontUVs` and `backUVs` (Vector4). Detail here : https://doc.babylonjs.com/babylon101/discover_basic_elements#side-orientation
+   * * The mesh can be set to updatable with the boolean parameter `updatable` (default false) if its internal geometry is supposed to change once created.
    * @param name defines the name of the mesh
    * @param options defines the options used to create the mesh
    * @param scene defines the hosting scene
    * @returns the torus mesh
    * @see https://doc.babylonjs.com/how_to/set_shapes#torus
    */
+  // clang-format on
   static MeshPtr CreateTorus(const std::string& name, TorusOptions& options,
                              Scene* scene = nullptr);
 
