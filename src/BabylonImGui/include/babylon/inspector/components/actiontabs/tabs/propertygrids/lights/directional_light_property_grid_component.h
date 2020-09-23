73b48ada893f751c58c6c4e91a5d41426e2c3970
samdauwe <sam.dauwe@gmail.com>
2020-09-23 20:56:19 +0200

    Added checkbox to toggle the 'Auto Calc Shadow ZBounds' state

--- src/BabylonImGui/include/babylon/inspector/components/actiontabs/tabs/propertygrids/lights/directional_light_property_grid_component.h
+++ src/BabylonImGui/include/babylon/inspector/components/actiontabs/tabs/propertygrids/lights/directional_light_property_grid_component.h
@@ -4,11 +4,13 @@
 #include <memory>
 
 #include <babylon/babylon_api.h>
+#include <babylon/inspector/components/actiontabs/lines/check_box_line_component.h>
 #include <babylon/inspector/components/actiontabs/lines/color3_line_component.h>
 #include <babylon/inspector/components/actiontabs/lines/vector3_line_component.h>
 #include <babylon/inspector/components/actiontabs/tabs/propertygrids/lights/common_light_property_grid_component.h>
 #include <babylon/inspector/components/actiontabs/tabs/propertygrids/lights/common_shadow_light_property_grid_component.h>
 #include <babylon/lights/directional_light.h>
+#include <babylon/lights/shadows/cascaded_shadow_generator.h>
 
 namespace BABYLON {
 
@@ -19,8 +21,12 @@ struct BABYLON_SHARED_EXPORT DirectionalLightPropertyGridComponent {
 
   static void render(const DirectionalLightPtr& light)
   {
-    CommonLightPropertyGridComponent::render(
-      std::static_pointer_cast<Light>(light));
+    const auto generator = light->getShadowGenerator();
+
+    const auto hideAutoCalcShadowZBounds
+      = std::static_pointer_cast<CascadedShadowGenerator>(generator);
+
+    CommonLightPropertyGridComponent::render(std::static_pointer_cast<Light>(light));
     // --- SETUP ---
     static auto setupContainerOpened = true;
     ImGui::SetNextTreeNodeOpen(setupContainerOpened, ImGuiCond_Always);
@@ -29,6 +35,12 @@ struct BABYLON_SHARED_EXPORT DirectionalLightPropertyGridComponent {
       Color3LineComponent::render("Specular", light->specular);
       Vector3LineComponent::render("Position", light->position);
       Vector3LineComponent::render("Direction", light->direction);
+      if (!hideAutoCalcShadowZBounds) {
+        if (CheckBoxLineComponent::render("Auto Calc Shadow ZBounds",
+                                          light->autoCalcShadowZBounds)) {
+          light->autoCalcShadowZBounds = !light->autoCalcShadowZBounds;
+        }
+      }
       setupContainerOpened = true;
     }
     else {
