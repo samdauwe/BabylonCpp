05df90c49449fb3f8da3df48fa7d13206a2cf3f0
samdauwe <sam.dauwe@gmail.com>
2020-09-26 19:53:06 +0200

    Formatting

--- src/BabylonImGui/include/babylon/inspector/components/actiontabs/tabs/propertygrids/materials/texture_property_grid_component.h
+++ src/BabylonImGui/include/babylon/inspector/components/actiontabs/tabs/propertygrids/materials/texture_property_grid_component.h
@@ -34,8 +34,7 @@ struct BABYLON_SHARED_EXPORT TexturePropertyGridComponent {
       {"Inverse cubic", TextureConstants::INVCUBIC_MODE},
       {"Equirectangular", TextureConstants::EQUIRECTANGULAR_MODE},
       {"Fixed equirectangular", TextureConstants::FIXED_EQUIRECTANGULAR_MODE},
-      {"Fixed equirectangular mirrored",
-       TextureConstants::FIXED_EQUIRECTANGULAR_MIRRORED_MODE},
+      {"Fixed equirectangular mirrored", TextureConstants::FIXED_EQUIRECTANGULAR_MIRRORED_MODE},
       {"Planar", TextureConstants::PLANAR_MODE},
       {"Projection", TextureConstants::PROJECTION_MODE},
       {"Skybox", TextureConstants::SKYBOX_MODE},
@@ -59,40 +58,33 @@ struct BABYLON_SHARED_EXPORT TexturePropertyGridComponent {
     static auto generalContainerOpened = true;
     ImGui::SetNextTreeNodeOpen(generalContainerOpened, ImGuiCond_Always);
     if (ImGui::CollapsingHeader("GENERAL")) {
-      TextLineComponent::render("Width",
-                                std::to_string(texture->getSize().width));
-      TextLineComponent::render("Height",
-                                std::to_string(texture->getSize().height));
+      TextLineComponent::render("Width", std::to_string(texture->getSize().width));
+      TextLineComponent::render("Height", std::to_string(texture->getSize().height));
       TextLineComponent::render("Unique ID", std::to_string(texture->uniqueId));
       TextLineComponent::render("Class", texture->getClassName());
-      TextLineComponent::render("Has alpha",
-                                texture->hasAlpha() ? "Yes" : "No");
+      TextLineComponent::render("Has alpha", texture->hasAlpha() ? "Yes" : "No");
       TextLineComponent::render("Is 3D", texture->is3D ? "Yes" : "No");
       TextLineComponent::render("Is cube", texture->isCube ? "Yes" : "No");
-      TextLineComponent::render("Is render target",
-                                texture->isRenderTarget ? "Yes" : "No");
+      TextLineComponent::render("Is render target", texture->isRenderTarget ? "Yes" : "No");
       auto sliderChange = SliderLineComponent::render(
-        "UV set", static_cast<float>(texture->coordinatesIndex), 0.f, 3.f, 1.f,
-        "%.3f");
+        "UV set", static_cast<float>(texture->coordinatesIndex), 0.f, 3.f, 1.f, "%.3f");
       if (sliderChange) {
-        texture->coordinatesIndex
-          = static_cast<unsigned int>(sliderChange.value());
+        texture->coordinatesIndex = static_cast<unsigned int>(sliderChange.value());
       }
       if (_texture) {
-        auto optionChange = OptionsLineComponent::render(
-          "Mode", _texture->coordinatesMode(), coordinatesMode);
+        auto optionChange
+          = OptionsLineComponent::render("Mode", _texture->coordinatesMode(), coordinatesMode);
         if (optionChange) {
           _texture->updateSamplingMode(optionChange.value());
         }
       }
-      sliderChange = SliderLineComponent::render("Level", texture->level, 0.f,
-                                                 2.f, 0.01f, "%.2f");
+      sliderChange = SliderLineComponent::render("Level", texture->level, 0.f, 2.f, 0.01f, "%.2f");
       if (sliderChange) {
         texture->level = sliderChange.value();
       }
       if (_texture) {
-        auto optionChange = OptionsLineComponent::render(
-          "Sampling", _texture->samplingMode(), samplingMode);
+        auto optionChange
+          = OptionsLineComponent::render("Sampling", _texture->samplingMode(), samplingMode);
         if (optionChange) {
           _texture->updateSamplingMode(optionChange.value());
         }
@@ -106,8 +98,7 @@ struct BABYLON_SHARED_EXPORT TexturePropertyGridComponent {
     ImGui::SetNextTreeNodeOpen(transformContainerOpened, ImGuiCond_Always);
     if (ImGui::CollapsingHeader("TRANSFORM")) {
       if (!texture->isCube && _texture) {
-        auto valueChange
-          = FloatLineComponent::render("U offset", _texture->uOffset);
+        auto valueChange = FloatLineComponent::render("U offset", _texture->uOffset);
         if (valueChange) {
           _texture->uOffset = valueChange.value();
         }
@@ -135,9 +126,8 @@ struct BABYLON_SHARED_EXPORT TexturePropertyGridComponent {
         if (valueChange) {
           _texture->wAng = valueChange.value();
         }
-        if (CheckBoxLineComponent::render(
-              "Clamp U",
-              _texture->wrapU == TextureConstants::CLAMP_ADDRESSMODE)) {
+        if (CheckBoxLineComponent::render("Clamp U",
+                                          _texture->wrapU == TextureConstants::CLAMP_ADDRESSMODE)) {
           if (_texture->wrapU == TextureConstants::CLAMP_ADDRESSMODE) {
             _texture->wrapU = TextureConstants::WRAP_ADDRESSMODE;
           }
@@ -145,9 +135,8 @@ struct BABYLON_SHARED_EXPORT TexturePropertyGridComponent {
             _texture->wrapU = TextureConstants::CLAMP_ADDRESSMODE;
           }
         }
-        if (CheckBoxLineComponent::render(
-              "Clamp V",
-              _texture->wrapV == TextureConstants::CLAMP_ADDRESSMODE)) {
+        if (CheckBoxLineComponent::render("Clamp V",
+                                          _texture->wrapV == TextureConstants::CLAMP_ADDRESSMODE)) {
           if (_texture->wrapV == TextureConstants::CLAMP_ADDRESSMODE) {
             _texture->wrapV = TextureConstants::WRAP_ADDRESSMODE;
           }
@@ -159,23 +148,19 @@ struct BABYLON_SHARED_EXPORT TexturePropertyGridComponent {
       else {
         auto cubeTexture = std::static_pointer_cast<CubeTexture>(texture);
         if (cubeTexture) {
-          auto sliderChange = SliderLineComponent::render(
-            "Rotation Y", cubeTexture->rotationY(), 0.f, Math::PI2, 0.1f,
-            "%.1f");
+          auto sliderChange = SliderLineComponent::render("Rotation Y", cubeTexture->rotationY(),
+                                                          0.f, Math::PI2, 0.1f, "%.1f");
           if (sliderChange) {
             cubeTexture->rotationY = static_cast<float>(sliderChange.value());
           }
         }
         else {
-          auto hdrCubeTexture
-            = std::static_pointer_cast<HDRCubeTexture>(texture);
+          auto hdrCubeTexture = std::static_pointer_cast<HDRCubeTexture>(texture);
           if (hdrCubeTexture) {
             auto sliderChange = SliderLineComponent::render(
-              "Rotation Y", hdrCubeTexture->rotationY(), 0, Math::PI2, 0.1f,
-              "%.1f");
+              "Rotation Y", hdrCubeTexture->rotationY(), 0, Math::PI2, 0.1f, "%.1f");
             if (sliderChange) {
-              hdrCubeTexture->rotationY
-                = static_cast<float>(sliderChange.value());
+              hdrCubeTexture->rotationY = static_cast<float>(sliderChange.value());
             }
           }
         }
