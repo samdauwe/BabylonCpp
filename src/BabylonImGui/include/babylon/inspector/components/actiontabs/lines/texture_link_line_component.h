5e6cdafa497b8a8e41cce9ab7593ab5b7a16103f
samdauwe <sam.dauwe@gmail.com>
2020-09-30 21:04:17 +0200

    Added extra render function prototype

--- src/BabylonImGui/include/babylon/inspector/components/actiontabs/lines/texture_link_line_component.h
+++ src/BabylonImGui/include/babylon/inspector/components/actiontabs/lines/texture_link_line_component.h
@@ -11,6 +11,10 @@ namespace BABYLON {
 
 struct BABYLON_SHARED_EXPORT TextureLinkLineComponent {
 
+  static void render(const char* /*label*/, const BaseTexturePtr& /*texture*/)
+  {
+  }
+
   static void render(const char* /*label*/, const MaterialPtr& /*material*/,
                      const BaseTexturePtr& /*texture*/)
   {
