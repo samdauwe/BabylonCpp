058a2c01df152d3473d7eca2c4133a2d86c0c6f8
samdauwe <sam.dauwe@gmail.com>
2020-10-03 20:15:44 +0200

    Added skeleton viewer interfaces

--- src/BabylonCpp/include/babylon/debug/iskeleton_viewer.h
+++ src/BabylonCpp/include/babylon/debug/iskeleton_viewer.h
@@ -3,12 +3,18 @@
 
 #include <optional>
 
+#include <babylon/babylon_api.h>
+#include <babylon/maths/color3.h>
+
 namespace BABYLON {
+
+class Skeleton;
+using SkeletonPtr = std::shared_ptr<Skeleton>;
 
 /**
  * @brief Defines how to display the various bone meshes for the viewer.
  */
-struct ISkeletonViewerDisplayOptions {
+struct BABYLON_SHARED_EXPORT ISkeletonViewerDisplayOptions {
   /** How far down to start tapering the bone spurs */
   std::optional<float> midStep = std::nullopt;
 
@@ -28,7 +34,7 @@ struct ISkeletonViewerDisplayOptions {
 /**
  * @brief Defines the options associated with the creation of a SkeletonViewer.
  */
-struct ISkeletonViewerOptions {
+struct BABYLON_SHARED_EXPORT ISkeletonViewerOptions {
   /** Should the system pause animations before building the Viewer? */
   std::optional<bool> pauseAnimations = std::nullopt;
 
@@ -43,7 +49,57 @@ struct ISkeletonViewerOptions {
 
   /** Flag to toggle if the Viewer should use the CPU for animations or not? */
   std::optional<bool> computeBonesUsingShaders = std::nullopt;
+
+  /** Flag ignore non weighted bones */
+  std::optional<bool> useAllBones = std::nullopt;
 }; // end of struct ISkeletonViewerOptions
+
+/**
+ * @brief Defines the constructor options for the BoneWeight Shader.
+ */
+struct BABYLON_SHARED_EXPORT IBoneWeightShaderOptions {
+  /** Skeleton to Map */
+  SkeletonPtr skeleton = nullptr;
+
+  /** Colors for Uninfluenced bones */
+  std::optional<Color3> colorBase = std::nullopt;
+
+  /** Colors for 0.0-0.25 Weight bones */
+  std::optional<Color3> colorZero = std::nullopt;
+
+  /** Color for 0.25-0.5 Weight Influence */
+  std::optional<Color3> colorQuarter = std::nullopt;
+
+  /** Color for 0.5-0.75 Weight Influence */
+  std::optional<Color3> colorHalf = std::nullopt;
+
+  /** Color for 0.75-1 Weight Influence */
+  std::optional<Color3> colorFull = std::nullopt;
+
+  /** Color for Zero Weight Influence */
+  std::optional<float> targetBoneIndex = std::nullopt;
+}; // end of struct IBoneWeightShaderOptions
+
+/**
+ * @brief Simple structure of the gradient steps for the Color Map.
+ */
+struct BABYLON_SHARED_EXPORT ISkeletonMapShaderColorMapKnot {
+  /** Color of the Knot */
+  std::optional<Color3> color = std::nullopt;
+  /** Location of the Knot */
+  std::optional<float> location = std::nullopt;
+}; // end of struct ISkeletonMapShaderColorMapKnot
+
+/**
+ * @brief Defines the constructor options for the SkeletonMap Shader.
+ */
+struct BABYLON_SHARED_EXPORT ISkeletonMapShaderOptions {
+  /** Skeleton to Map */
+  SkeletonPtr skeleton = nullptr;
+  /** Array of ColorMapKnots that make the gradient must be ordered with knot[i].location <
+   * knot[i+1].location*/
+  std::optional<std::vector<ISkeletonMapShaderColorMapKnot>> colorMap = std::nullopt;
+}; // end of struct ISkeletonMapShaderOptions
 
 } // end of namespace BABYLON
 
