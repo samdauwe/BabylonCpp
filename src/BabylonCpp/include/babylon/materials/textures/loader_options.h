970ee746055a040499d6b7b93e30f519281a3c3d
samdauwe <sam.dauwe@gmail.com>
2020-10-18 21:13:51 +0200

    Added LoaderOptions struct

---
+++ src/BabylonCpp/include/babylon/materials/textures/loader_options.h
@@ -0,0 +1,14 @@
+#ifndef BABYLON_MATERIALS_TEXTURES_LOADER_OPTIONS_H
+#define BABYLON_MATERIALS_TEXTURES_LOADER_OPTIONS_H
+
+#include <babylon/babylon_api.h>
+
+namespace BABYLON {
+
+struct BABYLON_SHARED_EXPORT LoaderOptions {
+
+}; // end of struct LoaderOptions
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_TEXTURES_LOADER_OPTIONS_H
