13facdf80433536c22951f36cac070c3af322c4a
samdauwe <sam.dauwe@gmail.com>
2020-10-20 20:30:29 +0200

    Updated for v4.2.0-beta.12

--- codegeneration/compare_files.py
+++ codegeneration/compare_files.py
@@ -67,7 +67,8 @@ def main():
     current, previous = "4.2.0-alpha.25", "4.2.0-alpha.17"
     current, previous = "4.2.0-alpha.32", "4.2.0-alpha.25"
     current, previous = "4.2.0-alpha.35", "4.2.0-alpha.32"
-    current, previous = "4.2.0-beta.1", "4.2.0-alpha.35"
+    current, previous = "4.2.0-beta.1", "4.2.0-alpha.35",
+    current, previous = "4.2.0-beta.12", "4.2.0-beta.1"
     # Dictionary mapping from BabylonJs version to relative path
     BabylonJsVersions = {
         "4.0.0"         : "4.0.0_2019_04_30",
@@ -80,7 +81,8 @@ def main():
         "4.2.0-alpha.25": "4.2.0-alpha.25_2020_07_17",
         "4.2.0-alpha.32": "4.2.0-alpha.32_2020_08_12",
         "4.2.0-alpha.35": "4.2.0-alpha.35_2020_08_31",
-        "4.2.0-beta.1"  : "4.2.0-beta.1_2020_09_11"
+        "4.2.0-beta.1"  : "4.2.0-beta.1_2020_09_11",
+        "4.2.0-beta.12" : "4.2.0-beta.12_2020_10_12"
     }
     # List containing the files to ignore
     whiteList = [
