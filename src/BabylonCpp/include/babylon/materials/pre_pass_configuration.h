0e66c0aa55b02ccaa966e181b6fd5515b31f6ea5
samdauwe <sam.dauwe@gmail.com>
2020-10-25 20:23:34 +0100

    Added PrePassConfiguration class

---
+++ src/BabylonCpp/include/babylon/materials/pre_pass_configuration.h
@@ -0,0 +1,69 @@
+#ifndef BABYLON_MATERIALS_PRE_PASS_CONFIGURATION_H
+#define BABYLON_MATERIALS_PRE_PASS_CONFIGURATION_H
+
+#include <unordered_map>
+
+#include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
+#include <babylon/maths/matrix.h>
+
+namespace BABYLON {
+
+class Scene;
+FWD_CLASS_SPTR(Effect)
+FWD_CLASS_SPTR(Mesh)
+
+/**
+ * @brief Configuration needed for prepass-capable materials.
+ */
+struct BABYLON_SHARED_EXPORT PrePassConfiguration {
+
+  PrePassConfiguration();
+  ~PrePassConfiguration(); // = default
+
+  /**
+   * Previous world matrices of meshes carrying this material
+   * Used for computing velocity
+   */
+  std::unordered_map<size_t, Matrix> previousWorldMatrices;
+
+  /**
+   * Previous view project matrix
+   * Used for computing velocity
+   */
+  std::optional<Matrix> previousViewProjection;
+
+  /**
+   * Previous bones of meshes carrying this material
+   * Used for computing velocity
+   */
+  std::unordered_map<unsigned int, Float32Array> previousBones;
+
+  /**
+   * @brief Add the required uniforms to the current list.
+   * @param uniforms defines the current uniform list.
+   */
+  static void AddUniforms(std::vector<std::string>& uniforms);
+
+  /**
+   * @brief Add the required samplers to the current list.
+   * @param samplers defines the current sampler list.
+   */
+  static void AddSamplers(std::vector<std::string>& samplers);
+
+  /**
+   * @brief Binds the material data.
+   * @param effect defines the effect to update
+   * @param scene defines the scene the material belongs to.
+   * @param mesh The mesh
+   * @param world World matrix of this mesh
+   * @param isFrozen Is the material frozen
+   */
+  void bindForSubMesh(const EffectPtr& effect, Scene* scene, const MeshPtr& mesh,
+                      const Matrix& world, bool isFrozen);
+
+}; // end of struct PrePassConfiguration
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_MATERIALS_PRE_PASS_CONFIGURATION_H
---
+++ src/BabylonCpp/src/materials/pre_pass_configuration.cpp
@@ -0,0 +1,52 @@
+#include <babylon/materials/pre_pass_configuration.h>
+
+#include <babylon/babylon_stl_util.h>
+#include <babylon/engines/constants.h>
+#include <babylon/engines/scene.h>
+#include <babylon/materials/effect.h>
+#include <babylon/meshes/mesh.h>
+#include <babylon/rendering/pre_pass_renderer.h>
+
+namespace BABYLON {
+
+PrePassConfiguration::PrePassConfiguration() : previousViewProjection{std::nullopt}
+{
+}
+
+PrePassConfiguration::~PrePassConfiguration() = default;
+
+void PrePassConfiguration::AddUniforms(std::vector<std::string>& uniforms)
+{
+  uniforms.emplace_back("previousWorld");
+  uniforms.emplace_back("previousViewProjection");
+}
+
+void PrePassConfiguration::AddSamplers(std::vector<std::string>& /*samplers*/)
+{
+  // pass
+}
+
+void PrePassConfiguration::bindForSubMesh(const EffectPtr& effect, Scene* scene,
+                                          const MeshPtr& mesh, const Matrix& world,
+                                          bool /*isFrozen*/)
+{
+  if (scene->prePassRenderer() && scene->prePassRenderer()->enabled()) {
+    if (scene->prePassRenderer()->getIndex(Constants::PREPASS_VELOCITY_TEXTURE_TYPE) != -1) {
+      if (!stl_util::contains(previousWorldMatrices, mesh->uniqueId)) {
+        previousWorldMatrices[mesh->uniqueId] = Matrix::Identity();
+      }
+
+      if (!previousViewProjection) {
+        previousViewProjection = scene->getTransformMatrix();
+      }
+
+      effect->setMatrix("previousWorld", previousWorldMatrices[mesh->uniqueId]);
+      effect->setMatrix("previousViewProjection", *previousViewProjection);
+
+      previousWorldMatrices[mesh->uniqueId] = world;
+      previousViewProjection                = scene->getTransformMatrix();
+    }
+  }
+}
+
+} // end of namespace BABYLON
