c4081342a9bdd7d5c1b337b817bca66b1feec989
samdauwe <sam.dauwe@gmail.com>
2020-10-27 20:32:58 +0100

    Added IMaterialClearCoatDefines values

--- src/BabylonCpp/src/materials/pbr/imaterial_clear_coat_defines.cpp
+++ src/BabylonCpp/src/materials/pbr/imaterial_clear_coat_defines.cpp
@@ -7,15 +7,24 @@ namespace BABYLON {
 IMaterialClearCoatDefines::IMaterialClearCoatDefines()
 {
   boolDef = {
-    {"CLEARCOAT", false},                      //
-    {"CLEARCOAT_DEFAULTIOR", false},           //
-    {"CLEARCOAT_TEXTURE", false},              //
-    {"CLEARCOAT_TEXTUREDIRECTUV", false},      //
-    {"CLEARCOAT_BUMP", false},                 //
-    {"CLEARCOAT_BUMPDIRECTUV", false},         //
+    {"CLEARCOAT", false},                                //
+    {"CLEARCOAT_DEFAULTIOR", false},                     //
+    {"CLEARCOAT_TEXTURE", false},                        //
+    {"CLEARCOAT_TEXTURE_ROUGHNESS", false},              //
+    {"CLEARCOAT_BUMP", false},                           //
+    {"CLEARCOAT_BUMPDIRECTUV", false},                   //
+    {"CLEARCOAT_USE_ROUGHNESS_FROM_MAINTEXTURE", false}, //
+    {"CLEARCOAT_TEXTURE_ROUGHNESS_IDENTICAL", false},    //
+    {"CLEARCOAT_REMAP_F0", false},                       //
+
     {"CLEARCOAT_TINT", false},                 //
     {"CLEARCOAT_TINT_TEXTURE", false},         //
     {"CLEARCOAT_TINT_TEXTUREDIRECTUV", false}, //
+  };
+
+  intDef = {
+    {"CLEARCOAT_TEXTUREDIRECTUV", 0},           //
+    {"CLEARCOAT_TEXTURE_ROUGHNESSDIRECTUV", 0}, //
   };
 }
 
