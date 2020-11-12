c1aa5b8ae5f787d2e605bddf63fc249f78103280
samdauwe <sam.dauwe@gmail.com>
2020-11-12 19:38:37 +0100

    Added FreeCameraMouseWheelInput class

---
+++ src/BabylonCpp/include/babylon/cameras/inputs/free_camera_mouse_wheel_input.h
@@ -0,0 +1,268 @@
+#ifndef BABYLON_CAMERAS_INPUTS_FREE_CAMERA_MOUSE_WHEEL_INPUT_H
+#define BABYLON_CAMERAS_INPUTS_FREE_CAMERA_MOUSE_WHEEL_INPUT_H
+
+#include <babylon/cameras/free_camera.h>
+#include <babylon/cameras/inputs/base_camera_mouse_wheel_input.h>
+
+namespace BABYLON {
+
+enum class _CameraProperty {
+  MoveRelative,
+  RotateRelative,
+  MoveScene
+}; // end of enum class _CameraProperty
+
+/**
+ * @brief Manage the mouse wheel inputs to control a free camera.
+ * @see https://doc.babylonjs.com/how_to/customizing_camera_inputs
+ */
+class BABYLON_SHARED_EXPORT FreeCameraMouseWheelInput : public BaseCameraMouseWheelInput,
+                                                        public ICameraInput<FreeCamera> {
+
+public:
+  /**
+   * @brief Listen to mouse wheel events to control the camera.
+   * @see https://doc.babylonjs.com/how_to/customizing_camera_inputs
+   */
+  FreeCameraMouseWheelInput();
+  ~FreeCameraMouseWheelInput() override; // = default
+
+  /**
+   * @brief Gets the class name of the current input.
+   * @returns the class name
+   */
+  std::string getClassName() const override;
+
+  /**
+   * @brief Get the friendly name associated with the input class.
+   * @returns the input friendly name
+   */
+  std::string getSimpleName() const override;
+
+  /**
+   * @brief Attach the mouse control to the HTML DOM element.
+   * @param element Defines the element that listens to the input events.
+   * @param noPreventDefault Defines whether events caught by the controls should call
+   * preventdefault().
+   */
+  void attachControl(ICanvas* canvas, bool noPreventDefault = false) override;
+
+  /**
+   * @brief Detach the current controls from the specified dom element.
+   * @param element Defines the element to stop listening the inputs from
+   */
+  void detachControl(ICanvas* canvas) override;
+
+  /**
+   * @brief Called for each rendered frame.
+   */
+  void checkInputs() override;
+
+protected:
+  /**
+   * @brief Returns the camera.
+   */
+  Camera* get_camera() const override;
+
+  /**
+   * @brief Set which movement axis (relative to camera's orientation) the mouse wheel's X axis
+   * controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelXMoveRelative(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured movement axis (relative to camera's orientation) the mouse wheel's X
+   * axis controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelXMoveRelative();
+
+  /**
+   * @brief Set which movement axis (relative to camera's orientation) the mouse wheel's Y axis
+   * controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelYMoveRelative(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured movement axis (relative to camera's orientation) the mouse wheel's Y
+   * axis controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelYMoveRelative();
+
+  /**
+   * @brief Set which movement axis (relative to camera's orientation) the mouse wheel's Z axis
+   * controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelZMoveRelative(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured movement axis (relative to camera's orientation) the mouse wheel's Z
+   * axis controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelZMoveRelative();
+
+  /**
+   * @brief Set which rotation axis (relative to camera's orientation) the mouse wheel's X axis
+   * controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelXRotateRelative(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured rotation axis (relative to camera's orientation) the mouse wheel's X
+   * axis controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelXRotateRelative();
+
+  /**
+   * @brief Set which rotation axis (relative to camera's orientation) the mouse wheel's Y axis
+   * controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelYRotateRelative(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured rotation axis (relative to camera's orientation) the mouse wheel's Y
+   * axis controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelYRotateRelative();
+
+  /**
+   * @brief Set which rotation axis (relative to camera's orientation) the mouse wheel's Z axis
+   * controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelZRotateRelative(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured rotation axis (relative to camera's orientation) the mouse wheel's Z
+   * axis controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelZRotateRelative();
+
+  /**
+   * @brief Set which movement axis (relative to the scene) the mouse wheel's X axis controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelXMoveScene(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured movement axis (relative to the scene) the mouse wheel's X axis
+   * controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelXMoveScene();
+
+  /**
+   * @brief Set which movement axis (relative to the scene) the mouse wheel's Y axis controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelYMoveScene(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured movement axis (relative to the scene) the mouse wheel's Y axis
+   * controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelYMoveScene();
+
+  /**
+   * @brief Set which movement axis (relative to the scene) the mouse wheel's Z axis controls.
+   * @param axis The axis to be moved. Set null to clear.
+   */
+  void set_wheelZMoveScene(const std::optional<Coordinate>& axis);
+
+  /**
+   * @brief Get the configured movement axis (relative to the scene) the mouse wheel's Z axis
+   * controls.
+   * @returns The configured axis or null if none.
+   */
+  std::optional<Coordinate>& get_wheelZMoveScene();
+
+private:
+  /**
+   * @brief Update the camera according to any configured properties for the 3 mouse-wheel axis.
+   */
+  void _updateCamera();
+
+public:
+  /**
+   * Gets or sets the movement axis (relative to camera's orientation) the mouse wheel's X axis
+   * controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelXMoveRelative;
+
+  /**
+   * Gets or sets the movement axis (relative to camera's orientation) the mouse wheel's Y axis
+   * controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelYMoveRelative;
+
+  /**
+   * Gets or sets the movement axis (relative to camera's orientation) the mouse wheel's Z axis
+   * controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelZMoveRelative;
+
+  /**
+   * Gets or sets the rotation axis (relative to camera's orientation) the mouse wheel's X axis
+   * controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelXRotateRelative;
+
+  /**
+   * Gets or sets the rotation axis (relative to camera's orientation) the mouse wheel's Y axis
+   * controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelYRotateRelative;
+
+  /**
+   * Gets or sets the rotation axis (relative to camera's orientation) the mouse wheel's Z axis
+   * controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelZRotateRelative;
+
+  /**
+   * Gets or sets the movement axis (relative to the scene) the mouse wheel's X axis controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelXMoveScene;
+
+  /**
+   * Gets or sets the movement axis (relative to the scene) the mouse wheel's Y axis controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelYMoveScene;
+
+  /**
+   * Gets or sets the movement axis (relative to the scene) the mouse wheel's Z axis controls.
+   */
+  Property<FreeCameraMouseWheelInput, std::optional<Coordinate>> wheelZMoveScene;
+
+private:
+  Vector3 _moveRelative;
+  Vector3 _rotateRelative;
+  Vector3 _moveScene;
+
+  /**
+   * These are set to the desired default behaviour.
+   */
+  std::optional<_CameraProperty> _wheelXAction;
+  std::optional<Coordinate> _wheelXActionCoordinate;
+  std::optional<_CameraProperty> _wheelYAction;
+  std::optional<Coordinate> _wheelYActionCoordinate;
+  std::optional<_CameraProperty> _wheelZAction;
+  std::optional<Coordinate> _wheelZActionCoordinate;
+  std::optional<Coordinate> _nullCoordinate;
+
+}; // end of class BaseCameraMouseWheelInput
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_CAMERAS_INPUTS_BASE_CAMERA_MOUSE_WHEEL_INPUT_H
---
+++ src/BabylonCpp/src/cameras/inputs/free_camera_mouse_wheel_input.cpp
@@ -0,0 +1,328 @@
+#include <babylon/cameras/inputs/free_camera_mouse_wheel_input.h>
+
+#include <babylon/cameras/camera.h>
+#include <babylon/cameras/free_camera.h>
+#include <babylon/engines/scene.h>
+
+namespace BABYLON {
+
+FreeCameraMouseWheelInput::FreeCameraMouseWheelInput()
+    : wheelXMoveRelative{this, &FreeCameraMouseWheelInput::get_wheelXMoveRelative,
+                         &FreeCameraMouseWheelInput::set_wheelXMoveRelative}
+    , wheelYMoveRelative{this, &FreeCameraMouseWheelInput::get_wheelYMoveRelative,
+                         &FreeCameraMouseWheelInput::set_wheelYMoveRelative}
+    , wheelZMoveRelative{this, &FreeCameraMouseWheelInput::get_wheelZMoveRelative,
+                         &FreeCameraMouseWheelInput::set_wheelZMoveRelative}
+    , wheelXRotateRelative{this, &FreeCameraMouseWheelInput::get_wheelXRotateRelative,
+                           &FreeCameraMouseWheelInput::set_wheelXRotateRelative}
+    , wheelYRotateRelative{this, &FreeCameraMouseWheelInput::get_wheelYRotateRelative,
+                           &FreeCameraMouseWheelInput::set_wheelYRotateRelative}
+    , wheelZRotateRelative{this, &FreeCameraMouseWheelInput::get_wheelZRotateRelative,
+                           &FreeCameraMouseWheelInput::set_wheelZRotateRelative}
+    , wheelXMoveScene{this, &FreeCameraMouseWheelInput::get_wheelXMoveScene,
+                      &FreeCameraMouseWheelInput::set_wheelXMoveScene}
+    , wheelYMoveScene{this, &FreeCameraMouseWheelInput::get_wheelYMoveScene,
+                      &FreeCameraMouseWheelInput::set_wheelYMoveScene}
+    , wheelZMoveScene{this, &FreeCameraMouseWheelInput::get_wheelZMoveScene,
+                      &FreeCameraMouseWheelInput::set_wheelZMoveScene}
+    , _moveRelative{Vector3::Zero()}
+    , _rotateRelative{Vector3::Zero()}
+    , _moveScene{Vector3::Zero()}
+    , _wheelXAction{_CameraProperty::MoveRelative}
+    , _wheelXActionCoordinate{Coordinate::X}
+    , _wheelYAction{_CameraProperty::MoveRelative}
+    , _wheelYActionCoordinate{Coordinate::Z}
+    , _wheelZAction{std::nullopt}
+    , _wheelZActionCoordinate{std::nullopt}
+{
+}
+
+FreeCameraMouseWheelInput::~FreeCameraMouseWheelInput() = default;
+
+Camera* FreeCameraMouseWheelInput::get_camera() const
+{
+  return camera;
+}
+
+std::string FreeCameraMouseWheelInput::getClassName() const
+{
+  return "FreeCameraMouseWheelInput";
+}
+
+std::string FreeCameraMouseWheelInput::getSimpleName() const
+{
+  return BaseCameraMouseWheelInput::getSimpleName();
+}
+
+void FreeCameraMouseWheelInput::set_wheelXMoveRelative(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelXAction.has_value() || *_wheelXAction != _CameraProperty::MoveRelative)) {
+    // Attempting to clear different _wheelXAction.
+    return;
+  }
+  _wheelXAction           = _CameraProperty::MoveRelative;
+  _wheelXActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelXMoveRelative()
+{
+  if (!_wheelXAction.has_value() || *_wheelXAction != _CameraProperty::MoveRelative) {
+    return _nullCoordinate;
+  }
+  return _wheelXActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::set_wheelYMoveRelative(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelYAction.has_value() || *_wheelYAction != _CameraProperty::MoveRelative)) {
+    // Attempting to clear different _wheelYAction.
+    return;
+  }
+  _wheelYAction           = _CameraProperty::MoveRelative;
+  _wheelYActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelYMoveRelative()
+{
+  if (!_wheelYAction.has_value() || *_wheelYAction != _CameraProperty::MoveRelative) {
+    return _nullCoordinate;
+  }
+  return _wheelYActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::set_wheelZMoveRelative(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelZAction.has_value() || *_wheelZAction != _CameraProperty::MoveRelative)) {
+    // Attempting to clear different _wheelZAction.
+    return;
+  }
+  _wheelZAction           = _CameraProperty::MoveRelative;
+  _wheelZActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelZMoveRelative()
+{
+  if (!_wheelZAction.has_value() || *_wheelZAction != _CameraProperty::MoveRelative) {
+    return _nullCoordinate;
+  }
+  return _wheelZActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::set_wheelXRotateRelative(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelXAction.has_value() || *_wheelXAction != _CameraProperty::RotateRelative)) {
+    // Attempting to clear different _wheelXAction.
+    return;
+  }
+  _wheelXAction           = _CameraProperty::RotateRelative;
+  _wheelXActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelXRotateRelative()
+{
+  if (!_wheelXAction.has_value() || *_wheelXAction != _CameraProperty::RotateRelative) {
+    return _nullCoordinate;
+  }
+  return _wheelXActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::set_wheelYRotateRelative(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelYAction.has_value() || *_wheelYAction != _CameraProperty::RotateRelative)) {
+    // Attempting to clear different _wheelYAction.
+    return;
+  }
+  _wheelYAction           = _CameraProperty::RotateRelative;
+  _wheelYActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelYRotateRelative()
+{
+  if (!_wheelYAction.has_value() || *_wheelYAction != _CameraProperty::RotateRelative) {
+    return _nullCoordinate;
+  }
+  return _wheelYActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::set_wheelZRotateRelative(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelZAction.has_value() || *_wheelZAction != _CameraProperty::RotateRelative)) {
+    // Attempting to clear different _wheelZAction.
+    return;
+  }
+  _wheelZAction           = _CameraProperty::RotateRelative;
+  _wheelZActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelZRotateRelative()
+{
+  if (!_wheelZAction.has_value() || *_wheelZAction != _CameraProperty::RotateRelative) {
+    return _nullCoordinate;
+  }
+  return _wheelZActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::set_wheelXMoveScene(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelXAction.has_value() || *_wheelXAction != _CameraProperty::MoveScene)) {
+    // Attempting to clear different _wheelXAction.
+    return;
+  }
+  _wheelXAction           = _CameraProperty::MoveScene;
+  _wheelXActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelXMoveScene()
+{
+  if (!_wheelXAction.has_value() || *_wheelXAction != _CameraProperty::MoveScene) {
+    return _nullCoordinate;
+  }
+  return _wheelXActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::set_wheelYMoveScene(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelYAction.has_value() || *_wheelYAction != _CameraProperty::MoveScene)) {
+    // Attempting to clear different _wheelYAction.
+    return;
+  }
+  _wheelYAction           = _CameraProperty::MoveScene;
+  _wheelYActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelYMoveScene()
+{
+  if (!_wheelYAction.has_value() || *_wheelYAction != _CameraProperty::MoveScene) {
+    return _nullCoordinate;
+  }
+  return _wheelYActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::set_wheelZMoveScene(const std::optional<Coordinate>& axis)
+{
+  if (!axis.has_value()
+      && (!_wheelZAction.has_value() || *_wheelZAction != _CameraProperty::MoveScene)) {
+    // Attempting to clear different _wheelZAction.
+    return;
+  }
+  _wheelZAction           = _CameraProperty::MoveScene;
+  _wheelZActionCoordinate = axis;
+}
+
+std::optional<Coordinate>& FreeCameraMouseWheelInput::get_wheelZMoveScene()
+{
+  if (!_wheelZAction.has_value() || *_wheelZAction != _CameraProperty::MoveScene) {
+    return _nullCoordinate;
+  }
+  return _wheelZActionCoordinate;
+}
+
+void FreeCameraMouseWheelInput::attachControl(ICanvas* canvas, bool noPreventDefault)
+{
+  BaseCameraMouseWheelInput::attachControl(canvas, noPreventDefault);
+}
+
+void FreeCameraMouseWheelInput::detachControl(ICanvas* canvas)
+{
+  BaseCameraMouseWheelInput::detachControl(canvas);
+}
+
+void FreeCameraMouseWheelInput::checkInputs()
+{
+  if (_wheelDeltaX == 0.f && _wheelDeltaY == 0.f && _wheelDeltaZ == 0.f) {
+    return;
+  }
+
+  // Clear the camera properties that we might be updating.
+  _moveRelative.setAll(0.f);
+  _rotateRelative.setAll(0.f);
+  _moveScene.setAll(0.f);
+
+  // Set the camera properties that are to be updated.
+  _updateCamera();
+
+  if (camera->getScene()->useRightHandedSystem()) {
+    // TODO: Does this need done for worldUpdate too?
+    _moveRelative.z *= -1.f;
+  }
+
+  // Convert updates relative to camera to world position update.
+  auto cameraTransformMatrix = Matrix::Zero();
+  camera->getViewMatrix().invertToRef(cameraTransformMatrix);
+
+  auto transformedDirection = Vector3::Zero();
+  Vector3::TransformNormalToRef(_moveRelative, cameraTransformMatrix, transformedDirection);
+
+  // Apply updates to camera position.
+  const auto freeCamera = static_cast<FreeCamera*>(camera);
+  freeCamera->cameraRotation->x += _rotateRelative.x / 200.f;
+  freeCamera->cameraRotation->y += _rotateRelative.y / 200.f;
+  freeCamera->cameraDirection->addInPlace(transformedDirection);
+  freeCamera->cameraDirection->addInPlace(_moveScene);
+
+  // Call the base class implementation to handle observers and do cleanup.
+  BaseCameraMouseWheelInput::checkInputs();
+}
+
+void FreeCameraMouseWheelInput::_updateCamera()
+{
+  const auto& moveRelative   = _moveRelative;
+  const auto& rotateRelative = _rotateRelative;
+  const auto& moveScene      = _moveScene;
+
+  const auto updateCameraProperty = [&](/* Mouse-wheel delta. */
+                                        float value,
+                                        /* Camera property to be changed. */
+                                        const std::optional<_CameraProperty>& cameraProperty,
+                                        /* Axis of Camera property to be changed. */
+                                        const std::optional<Coordinate>& coordinate) -> void {
+    if (value == 0.f) {
+      // Mouse wheel has not moved.
+      return;
+    }
+    if (!cameraProperty.has_value() || !coordinate.has_value()) {
+      // Mouse wheel axis not configured.
+      return;
+    }
+
+    Vector3 action;
+    switch (*cameraProperty) {
+      case _CameraProperty::MoveRelative:
+        action = moveRelative;
+        break;
+      case _CameraProperty::RotateRelative:
+        action = rotateRelative;
+        break;
+      case _CameraProperty::MoveScene:
+        action = moveScene;
+        break;
+    }
+
+    switch (*coordinate) {
+      case Coordinate::X:
+        action.set(value, 0.f, 0.f);
+        break;
+      case Coordinate::Y:
+        action.set(0.f, value, 0.f);
+        break;
+      case Coordinate::Z:
+        action.set(0.f, 0.f, value);
+        break;
+    }
+  };
+
+  // Do the camera updates for each of the 3 touch-wheel axis.
+  updateCameraProperty(_wheelDeltaX, _wheelXAction, _wheelXActionCoordinate);
+  updateCameraProperty(_wheelDeltaY, _wheelYAction, _wheelYActionCoordinate);
+  updateCameraProperty(_wheelDeltaZ, _wheelZAction, _wheelZActionCoordinate);
+}
+
+} // end of namespace BABYLON
