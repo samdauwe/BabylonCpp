1af5c05f44ef1af41d087d5e7e403c2d082c5e79
samdauwe <sam.dauwe@gmail.com>
2020-11-12 19:38:25 +0100

    Added BaseCameraMouseWheelInput class

---
+++ src/BabylonCpp/include/babylon/cameras/inputs/base_camera_mouse_wheel_input.h
@@ -0,0 +1,140 @@
+#ifndef BABYLON_CAMERAS_INPUTS_BASE_CAMERA_MOUSE_WHEEL_INPUT_H
+#define BABYLON_CAMERAS_INPUTS_BASE_CAMERA_MOUSE_WHEEL_INPUT_H
+
+#include <memory>
+
+#include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
+#include <babylon/cameras/icamera_input.h>
+#include <babylon/core/structs.h>
+#include <babylon/events/pointer_info.h>
+#include <babylon/misc/observable.h>
+#include <babylon/misc/observer.h>
+
+namespace BABYLON {
+
+FWD_CLASS_SPTR(Camera)
+
+/**
+ * @brief Base class for mouse wheel input..
+ * See FollowCameraMouseWheelInput in src/Cameras/Inputs/freeCameraMouseWheelInput.ts
+ * for example usage.
+ */
+class BABYLON_SHARED_EXPORT BaseCameraMouseWheelInput {
+
+public:
+  /**
+   * @brief Listen to mouse wheel events to control the camera.
+   * @see https://doc.babylonjs.com/how_to/customizing_camera_inputs
+   */
+  BaseCameraMouseWheelInput();
+  ~BaseCameraMouseWheelInput(); // = default
+
+  /**
+   * @brief Attach the mouse control to the HTML DOM element.
+   * @param element Defines the element that listens to the input events.
+   * @param noPreventDefault Defines whether events caught by the controls should call
+   * preventdefault().
+   */
+  void attachControl(ICanvas* canvas, bool noPreventDefault = false);
+
+  /**
+   * @brief Detach the current controls from the specified dom element.
+   * @param element Defines the element to stop listening the inputs from
+   */
+  void detachControl(ICanvas* canvas);
+
+  /**
+   * @brief Update the current camera state depending on the inputs that have been used this frame.
+   * This is a dynamically created lambda to avoid the performance penalty of looping for inputs in
+   * the render loop.
+   */
+  void checkInputs();
+
+  /**
+   * @brief Gets the class name of the current input.
+   * @returns the class name
+   */
+  std::string getClassName() const;
+
+  /**
+   * @brief Get the friendly name associated with the input class.
+   * @returns the input friendly name
+   */
+  std::string getSimpleName() const;
+
+protected:
+  /**
+   * @brief Returns the camera.
+   */
+  virtual Camera* get_camera() const = 0;
+
+public:
+  /**
+   * How fast is the camera moves in relation to X axis mouseWheel events.
+   * Use negative value to reverse direction.
+   */
+  float wheelPrecisionX;
+
+  /**
+   * How fast is the camera moves in relation to Y axis mouseWheel events.
+   * Use negative value to reverse direction.
+   */
+  float wheelPrecisionY;
+
+  /**
+   * How fast is the camera moves in relation to Z axis mouseWheel events.
+   * Use negative value to reverse direction.
+   */
+  float wheelPrecisionZ;
+
+  /**
+   * Observable for when a mouse wheel move event occurs.
+   */
+  Observable<MouseWheelChangeEvent> onChangedObservable;
+
+protected:
+  /**
+   * Incremental value of multiple mouse wheel movements of the X axis.
+   * Should be zero-ed when read.
+   */
+  float _wheelDeltaX;
+
+  /**
+   * Incremental value of multiple mouse wheel movements of the Y axis.
+   * Should be zero-ed when read.
+   */
+  float _wheelDeltaY;
+
+  /**
+   * Incremental value of multiple mouse wheel movements of the Z axis.
+   * Should be zero-ed when read.
+   */
+  float _wheelDeltaZ;
+
+private:
+  std::function<void(PointerInfo* pointer, EventState& es)> _wheel;
+  Observer<PointerInfo>::Ptr _observer;
+  MouseWheelChangeEvent _tmpMouseWheelChangeEvent;
+
+  /**
+   * Firefox uses a different scheme to report scroll distances to other
+   * browsers. Rather than use complicated methods to calculate the exact
+   * multiple we need to apply, let's just cheat and use a constant.
+   * https://developer.mozilla.org/en-US/docs/Web/API/WheelEvent/deltaMode
+   * https://stackoverflow.com/questions/20110224/what-is-the-height-of-a-line-in-a-wheel-event-deltamode-dom-delta-line
+   */
+  const float _ffMultiplier;
+
+  /**
+   * Different event attributes for wheel data fall into a few set ranges.
+   * Some relevant but dated date here:
+   * https://stackoverflow.com/questions/5527601/normalizing-mousewheel-speed-across-browsers
+   */
+  const float _normalize;
+
+}; // end of class BaseCameraMouseWheelInput
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_CAMERAS_INPUTS_BASE_CAMERA_MOUSE_WHEEL_INPUT_H
---
+++ src/BabylonCpp/src/cameras/inputs/base_camera_mouse_wheel_input.cpp
@@ -0,0 +1,106 @@
+#include <babylon/cameras/inputs/base_camera_mouse_wheel_input.h>
+
+#include <babylon/cameras/camera.h>
+#include <babylon/engines/scene.h>
+
+namespace BABYLON {
+
+BaseCameraMouseWheelInput::BaseCameraMouseWheelInput()
+    : wheelPrecisionX{3.f}
+    , wheelPrecisionY{3.f}
+    , wheelPrecisionZ{3.f}
+    , _wheelDeltaX{0.f}
+    , _wheelDeltaY{0.f}
+    , _wheelDeltaZ{0.f}
+    , _wheel{nullptr}
+    , _observer{nullptr}
+    , _ffMultiplier{12.f}
+    , _normalize{120.f}
+{
+}
+
+BaseCameraMouseWheelInput::~BaseCameraMouseWheelInput() = default;
+
+void BaseCameraMouseWheelInput::attachControl(ICanvas* /*canvas*/, bool noPreventDefault)
+{
+  _wheel = [this, noPreventDefault](PointerInfo* pointer, EventState& /*es*/) -> void {
+    // sanity check - this should be a PointerWheel event.
+    if (pointer->type != PointerEventTypes::POINTERWHEEL) {
+      return;
+    }
+
+    const auto& event = pointer->mouseWheelEvent;
+
+    const auto platformScale = event.deltaMode == WheelEvent::DOM_DELTA_LINE ? _ffMultiplier : 1.f;
+
+    if (event.deltaY != 0.f) {
+      // Most recent browsers versions have delta properties.
+      // Firefox >= v17  (Has WebGL >= v4)
+      // Chrome >=  v31  (Has WebGL >= v8)
+      // Edge >=    v12  (Has WebGl >= v12)
+      // https://developer.mozilla.org/en-US/docs/Web/API/WheelEvent
+      _wheelDeltaX += wheelPrecisionX * platformScale * event.deltaX / _normalize;
+      _wheelDeltaY -= wheelPrecisionY * platformScale * event.deltaY / _normalize;
+      _wheelDeltaZ += wheelPrecisionZ * platformScale * event.deltaZ / _normalize;
+    }
+    else if (event.wheelDeltaY != 0.f) {
+      // Unsure whether these catch anything more. Documentation
+      // online is contradictory.
+      _wheelDeltaX += wheelPrecisionX * platformScale * event.wheelDeltaX / _normalize;
+      _wheelDeltaY -= wheelPrecisionY * platformScale * event.wheelDeltaY / _normalize;
+      _wheelDeltaZ += wheelPrecisionZ * platformScale * event.wheelDeltaZ / _normalize;
+    }
+    else if (event.wheelDelta) {
+      // IE >= v9   (Has WebGL >= v11)
+      // Maybe others?
+      _wheelDeltaY -= wheelPrecisionY * event.wheelDelta / _normalize;
+    }
+
+    /* if (event.preventDefault) */ {
+      if (!noPreventDefault) {
+        event.preventDefault();
+      }
+    }
+  };
+
+  _observer = get_camera()->getScene()->onPointerObservable.add(
+    _wheel, static_cast<int>(PointerEventTypes::POINTERWHEEL));
+}
+
+void BaseCameraMouseWheelInput::detachControl(ICanvas* /*canvas*/)
+{
+  if (_observer) {
+    get_camera()->getScene()->onPointerObservable.remove(_observer);
+    _observer = nullptr;
+    _wheel    = nullptr;
+  }
+  if (onChangedObservable) {
+    onChangedObservable.clear();
+  }
+}
+
+void BaseCameraMouseWheelInput::checkInputs()
+{
+  _tmpMouseWheelChangeEvent.wheelDeltaX = _wheelDeltaX;
+  _tmpMouseWheelChangeEvent.wheelDeltaY = _wheelDeltaY;
+  _tmpMouseWheelChangeEvent.wheelDeltaZ = _wheelDeltaZ;
+
+  onChangedObservable.notifyObservers(&_tmpMouseWheelChangeEvent);
+
+  // Clear deltas.
+  _wheelDeltaX = 0.f;
+  _wheelDeltaY = 0.f;
+  _wheelDeltaZ = 0.f;
+}
+
+std::string BaseCameraMouseWheelInput::getClassName() const
+{
+  return "BaseCameraMouseWheelInput";
+}
+
+std::string BaseCameraMouseWheelInput::getSimpleName() const
+{
+  return "mousewheel";
+}
+
+} // end of namespace BABYLON
