e561fa2186db39df9c160e285ac27b1c95d993d3
samdauwe <sam.dauwe@gmail.com>
2020-12-25 14:08:45 +0100

    Formatting

--- src/BabylonCpp/include/babylon/misc/array_tools.h
+++ src/BabylonCpp/include/babylon/misc/array_tools.h
@@ -14,16 +14,15 @@ namespace BABYLON {
 class BABYLON_SHARED_EXPORT ArrayTools {
 
   /**
-   * @brief Returns an array of the given size filled with element built from
-   * the given constructor and the paramters.
+   * @brief Returns an array of the given size filled with element built from the given constructor
+   * and the paramters.
    * @param size the number of element to construct and put in the array
-   * @param itemBuilder a callback responsible for creating new instance of
-   * item. Called once per array entry.
+   * @param itemBuilder a callback responsible for creating new instance of item. Called once per
+   * array entry.
    * @returns a new array filled with new objects
    */
   template <typename T>
-  static std::vector<T> BuildArray(size_t size,
-                                   const std::function<T()>& itemBuilder)
+  static std::vector<T> BuildArray(size_t size, const std::function<T()>& itemBuilder)
   {
     std::vector<T> a;
     a.reserve(size);
