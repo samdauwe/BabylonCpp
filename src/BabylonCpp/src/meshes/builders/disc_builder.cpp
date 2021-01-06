e20190ac458da552f8a8257faceed6b24b8b86b9
samdauwe <sam.dauwe@gmail.com>
2021-01-06 21:13:01 +0100

    Using macro for forward declarations and const correctness

--- src/BabylonCpp/src/meshes/builders/disc_builder.cpp
+++ src/BabylonCpp/src/meshes/builders/disc_builder.cpp
@@ -8,12 +8,12 @@ namespace BABYLON {
 
 MeshPtr DiscBuilder::CreateDisc(const std::string& name, DiscOptions& options, Scene* scene)
 {
-  auto disc = Mesh::New(name, scene);
+  const auto disc = Mesh::New(name, scene);
 
   options.sideOrientation               = Mesh::_GetDefaultSideOrientation(options.sideOrientation);
   disc->_originalBuilderSideOrientation = *options.sideOrientation;
 
-  auto vertexData = VertexData::CreateDisc(options);
+  const auto vertexData = VertexData::CreateDisc(options);
 
   vertexData->applyToMesh(*disc, options.updatable);
 
