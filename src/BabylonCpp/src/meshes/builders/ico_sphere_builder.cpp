8fdb26f815f9425f8cf5853894a6c8e963055675
samdauwe <sam.dauwe@gmail.com>
2021-01-07 20:42:23 +0100

    Using macro for forward declarations and const correctness

--- src/BabylonCpp/src/meshes/builders/ico_sphere_builder.cpp
+++ src/BabylonCpp/src/meshes/builders/ico_sphere_builder.cpp
@@ -9,12 +9,12 @@ namespace BABYLON {
 MeshPtr IcoSphereBuilder::CreateIcoSphere(const std::string& name, IcoSphereOptions& options,
                                           Scene* scene)
 {
-  auto sphere = Mesh::New(name, scene);
+  const auto sphere = Mesh::New(name, scene);
 
   options.sideOrientation = Mesh::_GetDefaultSideOrientation(options.sideOrientation);
   sphere->_originalBuilderSideOrientation = *options.sideOrientation;
 
-  auto vertexData = VertexData::CreateIcoSphere(options);
+  const auto vertexData = VertexData::CreateIcoSphere(options);
 
   vertexData->applyToMesh(*sphere, options.updatable);
 
