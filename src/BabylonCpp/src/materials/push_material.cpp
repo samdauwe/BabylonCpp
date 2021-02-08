222f15720f9567b9375c24933835f7220e8670eb
samdauwe <sam.dauwe@gmail.com>
2021-02-08 20:27:14 +0100

    Calling material afterBind function with effect argument

--- src/BabylonCpp/src/materials/push_material.cpp
+++ src/BabylonCpp/src/materials/push_material.cpp
@@ -77,7 +77,7 @@ void PushMaterial::bind(Matrix& world, Mesh* mesh, const EffectPtr& /*effectOver
 
 void PushMaterial::_afterBind(Mesh* mesh, const EffectPtr& effect)
 {
-  Material::_afterBind(mesh);
+  Material::_afterBind(mesh, effect);
   getScene()->_cachedEffect = effect;
 }
 
