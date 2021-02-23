a53594cdfcf4c170c295cc292f2e3bd96bee805f
samdauwe <sam.dauwe@gmail.com>
2021-02-23 10:20:18 +0100

    Fixed typo in comment

--- src/BabylonCpp/src/meshes/buffer.cpp
+++ src/BabylonCpp/src/meshes/buffer.cpp
@@ -67,7 +67,7 @@ std::unique_ptr<VertexBuffer> Buffer::createVertexBuffer(const std::string& kind
   const auto _byteStride
     = stride.has_value() ? (useBytes ? *stride : *stride * sizeof(float)) : byteStride;
 
-  // a lot of these parameters are ignored as they are overriden by the buffer
+  // a lot of these parameters are ignored as they are overridden by the buffer
   return std::make_unique<VertexBuffer>(
     _engine, shared_from_this(), kind, _updatable, true, _byteStride,
     !instanced.has_value() ? _instanced : *instanced, _byteOffset, size, std::nullopt, false, true,
