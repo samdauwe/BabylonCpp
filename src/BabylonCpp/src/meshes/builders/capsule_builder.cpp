e942a2f895653baa4256f80f1839ca60a670a1a6
samdauwe <sam.dauwe@gmail.com>
2021-03-02 21:25:13 +0100

    Setting default capsule create options

--- src/BabylonCpp/src/meshes/builders/capsule_builder.cpp
+++ src/BabylonCpp/src/meshes/builders/capsule_builder.cpp
@@ -9,9 +9,20 @@ namespace BABYLON {
 MeshPtr CapsuleBuilder::CreateCapsule(const std::string& name, ICreateCapsuleOptions& options,
                                       Scene* scene)
 {
+  // Set default values
+  {
+    options.orientation     = options.orientation.value_or(Vector3::Up());
+    options.subdivisions    = options.subdivisions.value_or(2u);
+    options.tessellation    = options.tessellation.value_or(16u);
+    options.height          = options.height.value_or(1.f);
+    options.radius          = options.radius.value_or(0.25f);
+    options.capSubdivisions = options.capSubdivisions.value_or(6u);
+    options.updatable       = options.updatable.value_or(false);
+  }
+
   const auto capsule    = Mesh::New(name, scene);
   const auto vertexData = VertexData::CreateCapsule(options);
-  vertexData->applyToMesh(*capsule);
+  vertexData->applyToMesh(*capsule, options.updatable);
   return capsule;
 }
 
