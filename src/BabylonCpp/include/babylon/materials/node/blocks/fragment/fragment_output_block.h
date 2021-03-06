cb470451c1a7781b9280cd971e92efb7cb605a43
samdauwe <sam.dauwe@gmail.com>
2021-03-06 11:19:39 +0100

    Added extra defines and helper methods

--- src/BabylonCpp/include/babylon/materials/node/blocks/fragment/fragment_output_block.h
+++ src/BabylonCpp/include/babylon/materials/node/blocks/fragment/fragment_output_block.h
@@ -2,12 +2,12 @@
 #define BABYLON_MATERIALS_NODE_BLOCKS_FRAGMENT_FRAGMENT_OUTPUT_BLOCK_H
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 #include <babylon/materials/node/node_material_block.h>
 
 namespace BABYLON {
 
-class FragmentOutputBlock;
-using FragmentOutputBlockPtr = std::shared_ptr<FragmentOutputBlock>;
+FWD_CLASS_SPTR(FragmentOutputBlock)
 
 /**
  * @brief Block used to output the final color.
@@ -27,6 +27,30 @@ public:
    * @returns the class name
    */
   std::string getClassName() const override;
+
+  /**
+   * @brief Update defines for shader compilation.
+   * @param mesh defines the mesh to be rendered
+   * @param nodeMaterial defines the node material requesting the update
+   * @param defines defines the material defines to update
+   * @param useInstances specifies that instances should be used
+   * @param subMesh defines which submesh to render
+   */
+  void prepareDefines(AbstractMesh* mesh, const NodeMaterialPtr& nodeMaterial,
+                      NodeMaterialDefines& defines, bool useInstances = false,
+                      SubMesh* subMesh = nullptr) override;
+
+  /**
+   * @brief Serializes this block in a JSON representation.
+   * @returns the serialized block object
+   */
+  json serialize() const override;
+
+  /**
+   * @brief Hidden
+   */
+  void _deserialize(const json& serializationObject, Scene* scene,
+                    const std::string& rootUrl) override;
 
 protected:
   /**
@@ -55,7 +79,25 @@ protected:
    */
   FragmentOutputBlock& _buildBlock(NodeMaterialBuildState& state) override;
 
+  std::string _dumpPropertiesCode() override;
+
 public:
+  /**
+   * Gets or sets a boolean indicating if content needs to be converted to gamma space
+   */
+  // clang-format off
+  // @editableInPropertyPage("Convert to gamma space", PropertyTypeForEdition.Boolean, "PROPERTIES", { "notifiers": { "update": true }})
+  // clang-format on
+  bool convertToGammaSpace;
+
+  /**
+   * Gets or sets a boolean indicating if content needs to be converted to linear space
+   */
+  // clang-format off
+  // @editableInPropertyPage("Convert to linear space", PropertyTypeForEdition.Boolean, "PROPERTIES", { "notifiers": { "update": true }})
+  // clang-format on
+  bool convertToLinearSpace;
+
   /**
    * Gets the rgba input component
    */
@@ -70,6 +112,10 @@ public:
    * Gets the a input component
    */
   ReadOnlyProperty<FragmentOutputBlock, NodeMaterialConnectionPointPtr> a;
+
+private:
+  std::string _linearDefineName;
+  std::string _gammaDefineName;
 
 }; // end of class FragmentOutputBlock
 
--- src/BabylonCpp/src/materials/node/blocks/fragment/fragment_output_block.cpp
+++ src/BabylonCpp/src/materials/node/blocks/fragment/fragment_output_block.cpp
@@ -1,14 +1,18 @@
 #include <babylon/materials/node/blocks/fragment/fragment_output_block.h>
 
+#include <babylon/core/json_util.h>
 #include <babylon/materials/node/node_material_build_state.h>
 #include <babylon/materials/node/node_material_build_state_shared_data.h>
 #include <babylon/materials/node/node_material_connection_point.h>
+#include <babylon/materials/node/node_material_defines.h>
 #include <babylon/misc/string_tools.h>
 
 namespace BABYLON {
 
 FragmentOutputBlock::FragmentOutputBlock(const std::string& iName)
     : NodeMaterialBlock{iName, NodeMaterialBlockTargets::Fragment, true}
+    , convertToGammaSpace{false}
+    , convertToLinearSpace{false}
     , rgba{this, &FragmentOutputBlock::get_rgba}
     , rgb{this, &FragmentOutputBlock::get_rgb}
     , a{this, &FragmentOutputBlock::get_a}
@@ -42,14 +46,31 @@ NodeMaterialConnectionPointPtr& FragmentOutputBlock::get_a()
   return _inputs[2];
 }
 
+void FragmentOutputBlock::prepareDefines(AbstractMesh* /*mesh*/,
+                                         const NodeMaterialPtr& /*nodeMaterial*/,
+                                         NodeMaterialDefines& defines, bool /*useInstances*/,
+                                         SubMesh* /*subMesh*/)
+{
+  defines.setValue(_linearDefineName, convertToLinearSpace, true);
+  defines.setValue(_gammaDefineName, convertToGammaSpace, true);
+}
+
 FragmentOutputBlock& FragmentOutputBlock::_buildBlock(NodeMaterialBuildState& state)
 {
   NodeMaterialBlock::_buildBlock(state);
 
-  const auto& _rgba                         = rgba();
-  const auto& _rgb                          = rgb();
-  const auto& _a                            = a();
+  const auto& _rgba = rgba();
+  const auto& _rgb  = rgb();
+  const auto& _a    = a();
+
   state.sharedData->hints.needAlphaBlending = _rgba->isConnected() || _a->isConnected();
+  state.sharedData->blocksWithDefines.emplace_back(shared_from_this());
+
+  _linearDefineName = state._getFreeDefineName("CONVERTTOLINEAR");
+  _gammaDefineName  = state._getFreeDefineName("CONVERTTOGAMMA");
+
+  const auto iComments = StringTools::printf("//%s", name().c_str());
+  state._emitFunctionFromInclude("helperFunctions", iComments);
 
   if (_rgba->connectedPoint()) {
     if (_a->isConnected()) {
@@ -84,7 +105,36 @@ FragmentOutputBlock& FragmentOutputBlock::_buildBlock(NodeMaterialBuildState& st
     state.sharedData->checks.notConnectedNonOptionalInputs.emplace_back(_rgba);
   }
 
+  state.compilationString += StringTools::printf("#ifdef %s\r\n", _linearDefineName.c_str());
+  state.compilationString += "gl_FragColor = toLinearSpace(gl_FragColor);\r\n";
+  state.compilationString += "#endif\r\n";
+
+  state.compilationString += StringTools::printf("#ifdef %s\r\n", _gammaDefineName.c_str());
+  state.compilationString += "gl_FragColor = toGammaSpace(gl_FragColor);\r\n";
+  state.compilationString += "#endif\r\n";
+
   return *this;
+}
+
+std::string FragmentOutputBlock::_dumpPropertiesCode()
+{
+  std::string codeString = "";
+  codeString += StringTools::printf("%s.convertToGammaSpace = %s;\r\n", _codeVariableName.c_str(),
+                                    convertToGammaSpace ? "true" : "false");
+  codeString += StringTools::printf("%s.convertToLinearSpace = %s;\r\n", _codeVariableName.c_str(),
+                                    convertToLinearSpace ? "true" : "false");
+
+  return codeString;
+}
+
+json FragmentOutputBlock::serialize() const
+{
+  return nullptr;
+}
+
+void FragmentOutputBlock::_deserialize(const json& /*serializationObject*/, Scene* /*scene*/,
+                                       const std::string& /*rootUrl*/)
+{
 }
 
 } // end of namespace BABYLON
