2f23b624048e20a017ba6fae30dd3ff7fee3cff4
samdauwe <sam.dauwe@gmail.com>
2021-03-20 18:02:36 +0100

    Using macro for forward declaration

--- src/BabylonCpp/include/babylon/materials/node/blocks/arc_tan2_block.h
+++ src/BabylonCpp/include/babylon/materials/node/blocks/arc_tan2_block.h
@@ -2,12 +2,12 @@
 #define BABYLON_MATERIALS_NODE_BLOCKS_ARC_TAN2_BLOCK_H
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 #include <babylon/materials/node/node_material_block.h>
 
 namespace BABYLON {
 
-class ArcTan2Block;
-using ArcTan2BlockPtr = std::shared_ptr<ArcTan2Block>;
+FWD_CLASS_SPTR(ArcTan2Block)
 
 /**
  * @brief Block used to compute arc tangent of 2 values.
