ac0c1f52e24f5bd76c5ab53dc20a9adb6f99d177
samdauwe <sam.dauwe@gmail.com>
2021-03-21 11:26:20 +0100

    Using macro for forward declaration

--- src/BabylonCpp/include/babylon/materials/node/blocks/clamp_block.h
+++ src/BabylonCpp/include/babylon/materials/node/blocks/clamp_block.h
@@ -2,12 +2,12 @@
 #define BABYLON_MATERIALS_NODE_BLOCKS_CLAMP_BLOCK_H
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 #include <babylon/materials/node/node_material_block.h>
 
 namespace BABYLON {
 
-class ClampBlock;
-using ClampBlockPtr = std::shared_ptr<ClampBlock>;
+FWD_CLASS_SPTR(ClampBlock)
 
 /**
  * @brief Block used to clamp a float.
