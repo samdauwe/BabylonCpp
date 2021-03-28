d7a678d921eeec634b4f2a781cf9559f32f3a676
samdauwe <sam.dauwe@gmail.com>
2021-03-28 20:09:59 +0200

    Added IDrawContext struct

---
+++ src/BabylonCpp/include/babylon/engines/idraw_context.h
@@ -0,0 +1,17 @@
+#ifndef BABYLON_ENGINES_IDRAW_CONTEXT_H
+#define BABYLON_ENGINES_IDRAW_CONTEXT_H
+
+#include <babylon/babylon_api.h>
+
+namespace BABYLON {
+
+/**
+ * @brief Hidden
+ */
+struct BABYLON_SHARED_EXPORT IDrawContext {
+
+}; // end of struct IDrawContext
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_ENGINES_IDRAW_CONTEXT_H
