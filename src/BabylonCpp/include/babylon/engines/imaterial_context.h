78b6351c0637d86b1944f78575a9cd202fbb3251
samdauwe <sam.dauwe@gmail.com>
2021-03-28 20:08:10 +0200

    Added IMaterialContext struct

---
+++ src/BabylonCpp/include/babylon/engines/imaterial_context.h
@@ -0,0 +1,17 @@
+#ifndef BABYLON_ENGINES_IMATERIAL_CONTEXT_H
+#define BABYLON_ENGINES_IMATERIAL_CONTEXT_H
+
+#include <babylon/babylon_api.h>
+
+namespace BABYLON {
+
+/**
+ * @brief Hidden
+ */
+struct BABYLON_SHARED_EXPORT IMaterialContext {
+
+}; // end of struct IMaterialContext
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_ENGINES_IMATERIAL_CONTEXT_H
