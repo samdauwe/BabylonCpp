414c7c02883b193628d762e3466c5541399dab00
samdauwe <sam.dauwe@gmail.com>
2021-03-29 20:44:06 +0200

    Formatting and using macro for forward declarations

--- src/BabylonCpp/include/babylon/collisions/icollision_coordinator.h
+++ src/BabylonCpp/include/babylon/collisions/icollision_coordinator.h
@@ -5,15 +5,14 @@
 #include <memory>
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 
 namespace BABYLON {
 
-class AbstractMesh;
-class Collider;
 class Scene;
 class Vector3;
-using AbstractMeshPtr = std::shared_ptr<AbstractMesh>;
-using ColliderPtr     = std::shared_ptr<Collider>;
+FWD_CLASS_SPTR(AbstractMesh)
+FWD_CLASS_SPTR(Collider)
 
 /**
  * @brief Hidden
@@ -21,13 +20,12 @@ using ColliderPtr     = std::shared_ptr<Collider>;
 struct BABYLON_SHARED_EXPORT ICollisionCoordinator {
   virtual ~ICollisionCoordinator()     = default;
   virtual ColliderPtr createCollider() = 0;
-  virtual void getNewPosition(
-    Vector3& position, Vector3& displacement, const ColliderPtr& collider,
-    unsigned int maximumRetry, const AbstractMeshPtr& excludedMesh,
-    const std::function<void(size_t collisionIndex, Vector3& newPosition,
-                             const AbstractMeshPtr& collidedMesh)>&
-      onNewPosition,
-    size_t collisionIndex)
+  virtual void
+  getNewPosition(Vector3& position, Vector3& displacement, const ColliderPtr& collider,
+                 unsigned int maximumRetry, const AbstractMeshPtr& excludedMesh,
+                 const std::function<void(size_t collisionIndex, Vector3& newPosition,
+                                          const AbstractMeshPtr& collidedMesh)>& onNewPosition,
+                 size_t collisionIndex)
     = 0;
   virtual void init(Scene* scene) = 0;
 }; // end of struct ICollisionCoordinator
