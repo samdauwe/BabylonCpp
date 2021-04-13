50a1e5e78879439eebab65f06000304cb7e6f620
samdauwe <sam.dauwe@gmail.com>
2021-04-13 21:39:44 +0200

    Using macro for forward declaration

--- src/BabylonCpp/include/babylon/gamepads/controllers/gear_vr_controller.h
+++ src/BabylonCpp/include/babylon/gamepads/controllers/gear_vr_controller.h
@@ -2,13 +2,13 @@
 #define BABYLON_GAMEPADS_CONTROLLERS_GEAR_VR_CONTROLLER_H
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 #include <babylon/gamepads/controllers/_game_pad_factory.h>
 #include <babylon/gamepads/controllers/web_vr_controller.h>
 
 namespace BABYLON {
 
-class GearVRController;
-using GearVRControllerPtr = std::shared_ptr<GearVRController>;
+FWD_CLASS_SPTR(GearVRController)
 
 /**
  * @brief Gear VR Controller factory.
