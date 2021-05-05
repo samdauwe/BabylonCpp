1ac8f053fb3afb851542da14b6cd7bd30fff9b04
samdauwe <sam.dauwe@gmail.com>
2021-05-05 21:56:42 +0200

    Updated GradientBlock::_dumpPropertiesCode function to use base code string from superclass

--- src/BabylonCpp/src/materials/node/blocks/gradient_block.cpp
+++ src/BabylonCpp/src/materials/node/blocks/gradient_block.cpp
@@ -152,7 +152,7 @@ void GradientBlock::_deserialize(const json& /*serializationObject*/, Scene* /*s
 
 std::string GradientBlock::_dumpPropertiesCode()
 {
-  std::string codeString = "";
+  std::string codeString = NodeMaterialBlock::_dumpPropertiesCode();
 
   codeString += StringTools::printf("%s.colorSteps = {};\r\n", _codeVariableName.c_str());
 
