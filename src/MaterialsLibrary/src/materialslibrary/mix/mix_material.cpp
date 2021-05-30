7eec5f98b02a0177c8770b4879926ae3d615dbae
samdauwe <sam.dauwe@gmail.com>
2021-05-30 20:23:25 +0200

    Updated Mix Material to use bindEyePosition function from scene class

--- src/MaterialsLibrary/src/materialslibrary/mix/mix_material.cpp
+++ src/MaterialsLibrary/src/materialslibrary/mix/mix_material.cpp
@@ -488,7 +488,7 @@ void MixMaterial::bindForSubMesh(Matrix& world, Mesh* mesh, SubMesh* subMesh)
       _activeEffect->setFloat("pointSize", pointSize);
     }
 
-    MaterialHelper::BindEyePosition(effect.get(), scene);
+    scene->bindEyePosition(effect.get());
   }
 
   _activeEffect->setColor4("vDiffuseColor", diffuseColor, alpha * mesh->visibility);
