aa6cc1449128545a1db19df5d0a6619fa7325ee8
samdauwe <sam.dauwe@gmail.com>
2021-06-15 20:34:46 +0200

    Updated Terrain Material to use bindEyePosition function from scene class

--- src/MaterialsLibrary/src/materialslibrary/terrain/terrain_material.cpp
+++ src/MaterialsLibrary/src/materialslibrary/terrain/terrain_material.cpp
@@ -422,7 +422,7 @@ void TerrainMaterial::bindForSubMesh(Matrix& world, Mesh* mesh, SubMesh* subMesh
       _activeEffect->setFloat("pointSize", pointSize);
     }
 
-    MaterialHelper::BindEyePosition(effect.get(), scene);
+    scene->bindEyePosition(effect.get());
   }
 
   _activeEffect->setColor4("vDiffuseColor", diffuseColor, alpha * mesh->visibility);
