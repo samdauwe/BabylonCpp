fc587510a3396476c989350ab694db520e8b0ef2
samdauwe <sam.dauwe@gmail.com>
2021-07-14 20:36:10 +0200

    Updated TrigonometryBlock class to use macro for forward declaration

--- src/BabylonCpp/include/babylon/materials/node/blocks/trigonometry_block.h
+++ src/BabylonCpp/include/babylon/materials/node/blocks/trigonometry_block.h
@@ -2,12 +2,12 @@
 #define BABYLON_MATERIALS_NODE_BLOCKS_TRIGONOMETRY_BLOCK_H
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 #include <babylon/materials/node/node_material_block.h>
 
 namespace BABYLON {
 
-class TrigonometryBlock;
-using TrigonometryBlockPtr = std::shared_ptr<TrigonometryBlock>;
+FWD_CLASS_SPTR(TrigonometryBlock)
 
 /**
  * @brief Operations supported by the Trigonometry block.
