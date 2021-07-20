e9d8cefca0acc5cc4239339f1209437216dca7eb
samdauwe <sam.dauwe@gmail.com>
2021-07-20 21:33:15 +0200

    Updated LengthBlock class to use macro for forward declaration

--- src/BabylonCpp/include/babylon/materials/node/blocks/length_block.h
+++ src/BabylonCpp/include/babylon/materials/node/blocks/length_block.h
@@ -2,12 +2,12 @@
 #define BABYLON_MATERIALS_NODE_BLOCKS_LENGTH_BLOCK_H
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 #include <babylon/materials/node/node_material_block.h>
 
 namespace BABYLON {
 
-class LengthBlock;
-using LengthBlockPtr = std::shared_ptr<LengthBlock>;
+FWD_CLASS_SPTR(LengthBlock)
 
 /**
  * @brief Block used to get the length of a vector.
