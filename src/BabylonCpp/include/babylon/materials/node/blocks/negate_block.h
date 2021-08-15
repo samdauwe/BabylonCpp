66d62c710eba3ebb476ebded90ce11be898aa099
samdauwe <sam.dauwe@gmail.com>
2021-08-15 21:20:46 +0200

    Updated NegateBlock class to use macro for forward declaration

--- src/BabylonCpp/include/babylon/materials/node/blocks/negate_block.h
+++ src/BabylonCpp/include/babylon/materials/node/blocks/negate_block.h
@@ -2,12 +2,12 @@
 #define BABYLON_MATERIALS_NODE_BLOCKS_NEGATE_BLOCK_H
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 #include <babylon/materials/node/node_material_block.h>
 
 namespace BABYLON {
 
-class NegateBlock;
-using NegateBlockPtr = std::shared_ptr<NegateBlock>;
+FWD_CLASS_SPTR(NegateBlock)
 
 /**
  * @brief Block used to get negative version of a value (i.e. x * -1).
