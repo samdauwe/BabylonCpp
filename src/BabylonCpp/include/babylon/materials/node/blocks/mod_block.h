fb0fbd6a279800afed3931b4cd1410c1d5b6adb6
samdauwe <sam.dauwe@gmail.com>
2021-08-15 21:20:07 +0200

    Updated ModBlock class to use macro for forward declaration

--- src/BabylonCpp/include/babylon/materials/node/blocks/mod_block.h
+++ src/BabylonCpp/include/babylon/materials/node/blocks/mod_block.h
@@ -2,12 +2,12 @@
 #define BABYLON_MATERIALS_NODE_BLOCKS_MOD_BLOCK_H
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_fwd.h>
 #include <babylon/materials/node/node_material_block.h>
 
 namespace BABYLON {
 
-class ModBlock;
-using ModBlockPtr = std::shared_ptr<ModBlock>;
+FWD_CLASS_SPTR(ModBlock)
 
 /**
  * @brief Block used to compute value of one parameter modulo another.
