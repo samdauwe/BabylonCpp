a945e75344fc998bf21880d3574e7b48ec209829
samdauwe <sam.dauwe@gmail.com>
2021-09-29 22:00:34 +0200

    Added StencilStateComposer class

---
+++ src/BabylonCpp/include/babylon/states/stencil_state_composer.h
@@ -0,0 +1,17 @@
+#ifndef BABYLON_STENCIL_STATE_COMPOSER_H
+#define BABYLON_STENCIL_STATE_COMPOSER_H
+
+#include <babylon/babylon_api.h>
+
+namespace BABYLON {
+
+class BABYLON_SHARED_EXPORT StencilStateComposer {
+
+public:
+  bool useStencilGlobalOnly;
+
+}; // end of class StencilState
+
+} // end of namespace BABYLON
+
+#endif // end of BABYLON_STENCIL_STATE_H
---
+++ src/BabylonCpp/src/states/stencil_state_composer.cpp
@@ -0,0 +1,5 @@
+#include <babylon/states/stencil_state_composer.h>
+
+namespace BABYLON {
+
+} // end of namespace BABYLON
