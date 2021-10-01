67170b29277abafff16ef4053e7c808e64371037
samdauwe <sam.dauwe@gmail.com>
2021-10-01 21:29:49 +0200

    Removed [[nodiscard]] annotation

--- src/MaterialsLibrary/include/babylon/materialslibrary/cell/cell_material.h
+++ src/MaterialsLibrary/include/babylon/materialslibrary/cell/cell_material.h
@@ -29,20 +29,19 @@ public:
   }
   ~CellMaterial() override; // = default
 
-  [[nodiscard]] bool needAlphaBlending() const override;
-  [[nodiscard]] bool needAlphaTesting() const override;
+  bool needAlphaBlending() const override;
+  bool needAlphaTesting() const override;
   BaseTexturePtr getAlphaTestTexture() override;
   bool isReadyForSubMesh(AbstractMesh* mesh, SubMesh* subMesh, bool useInstances) override;
   void bindForSubMesh(Matrix& world, Mesh* mesh, SubMesh* subMesh) override;
   std::vector<IAnimatablePtr> getAnimatables() override;
-  [[nodiscard]] std::vector<BaseTexturePtr> getActiveTextures() const override;
-  [[nodiscard]] bool hasTexture(const BaseTexturePtr& texture) const override;
+  std::vector<BaseTexturePtr> getActiveTextures() const override;
+  bool hasTexture(const BaseTexturePtr& texture) const override;
   void dispose(bool forceDisposeEffect = false, bool forceDisposeTextures = false,
                bool notBoundToMesh = false) override;
-  [[nodiscard]] std::string getClassName() const override;
-  [[nodiscard]] MaterialPtr clone(const std::string& name,
-                                  bool cloneChildren = false) const override;
-  [[nodiscard]] json serialize() const;
+  std::string getClassName() const override;
+  MaterialPtr clone(const std::string& name, bool cloneChildren = false) const override;
+  json serialize() const;
 
   /** Statics **/
   static CellMaterial* Parse(const json& source, Scene* scene, const std::string& rootUrl);
@@ -52,11 +51,11 @@ protected:
 
   TexturePtr& get_diffuseTexture();
   void set_diffuseTexture(const TexturePtr& value);
-  [[nodiscard]] bool get_computeHighLevel() const;
+  bool get_computeHighLevel() const;
   void set_computeHighLevel(bool value);
-  [[nodiscard]] bool get_disableLighting() const;
+  bool get_disableLighting() const;
   void set_disableLighting(bool value);
-  [[nodiscard]] unsigned int get_maxSimultaneousLights() const;
+  unsigned int get_maxSimultaneousLights() const;
   void set_maxSimultaneousLights(unsigned int value);
 
 public:
