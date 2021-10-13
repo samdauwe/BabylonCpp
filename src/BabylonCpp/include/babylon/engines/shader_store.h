0b12a70e0dfb67ed7622c9597957f11e9d34f2b7
samdauwe <sam.dauwe@gmail.com>
2021-10-13 21:25:56 +0200

    Added functions for getting the shaders store of a given shader language

--- src/BabylonCpp/include/babylon/engines/shader_store.h
+++ src/BabylonCpp/include/babylon/engines/shader_store.h
@@ -5,6 +5,7 @@
 #include <unordered_map>
 
 #include <babylon/babylon_api.h>
+#include <babylon/babylon_enums.h>
 
 namespace BABYLON {
 
@@ -12,6 +13,7 @@ namespace BABYLON {
  * @brief Defines the shader related stores and directory.
  */
 struct BABYLON_SHARED_EXPORT ShaderStore {
+
   /**
    * Gets or sets the relative url used to load shaders if using the engine in non-minified mode
    */
@@ -24,6 +26,44 @@ struct BABYLON_SHARED_EXPORT ShaderStore {
    * Store of each included file for a shader (The can be looked up using effect.key)
    */
   static std::unordered_map<std::string, std::string> IncludesShadersStore;
+  /**
+   * Gets or sets the relative url used to load shaders (WGSL) if using the engine in non-minified
+   * mode
+   */
+  static std::string ShadersRepositoryWGSL;
+  /**
+   * Store of each shader  (WGSL)
+   */
+  static std::unordered_map<std::string, std::string> ShadersStoreWGSL;
+  /**
+   * Store of each included file for a shader (WGSL)
+   */
+  static std::unordered_map<std::string, std::string> IncludesShadersStoreWGSL;
+
+  /**
+   * @brief Gets the shaders repository path for a given shader language.
+   * @param shaderLanguage the shader language
+   * @returns the path to the shaders repository
+   */
+  static std::string& GetShadersRepository(const ShaderLanguage shaderLanguage
+                                           = ShaderLanguage::GLSL);
+
+  /**
+   * @brief Gets the shaders store of a given shader language.
+   * @param shaderLanguage the shader language
+   * @returns the shaders store
+   */
+  static std::unordered_map<std::string, std::string>& GetShadersStore(const ShaderLanguage
+                                                                       = ShaderLanguage::GLSL);
+
+  /**
+   * @brief Gets the include shaders store of a given shader language.
+   * @param shaderLanguage the shader language
+   * @returns the include shaders store
+   */
+  static std::unordered_map<std::string, std::string>&
+  GetIncludesShadersStore(const ShaderLanguage = ShaderLanguage::GLSL);
+
 }; // end of struct ShaderStore
 
 } // end of namespace BABYLON
