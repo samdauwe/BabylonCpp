d4aac2a3c39ec6e70b668bf1e66e7c00d087ccd4
samdauwe <sam.dauwe@gmail.com>
2021-11-20 20:05:48 +0100

    Setting default values

--- src/BabylonCpp/include/babylon/meshes/_creation_data_storage.h
+++ src/BabylonCpp/include/babylon/meshes/_creation_data_storage.h
@@ -11,17 +11,17 @@ namespace BABYLON {
  * @brief Hidden
  */
 struct BABYLON_SHARED_EXPORT _CreationDataStorage {
-  std::optional<bool> closePath  = std::nullopt;
-  std::optional<bool> closeArray = std::nullopt;
-  IndicesArray idx;
-  float dashSize;
-  float gapSize;
-  Path3D path3D;
-  std::vector<std::vector<Vector3>> pathArray;
-  float arc;
-  float radius;
-  unsigned int cap;
-  unsigned int tessellation;
+  std::optional<bool> closePath{std::nullopt};
+  std::optional<bool> closeArray{std::nullopt};
+  IndicesArray idx{};
+  float dashSize{0.f};
+  float gapSize{0.f};
+  Path3D path3D{};
+  std::vector<std::vector<Vector3>> pathArray{};
+  float arc{0.f};
+  float radius{0.f};
+  unsigned int cap{0u};
+  unsigned int tessellation{0u};
 }; // end of struct _CreationDataStorage
 
 } // end of namespace BABYLON
