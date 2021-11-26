b5ed4ce5d2a6d2c5f4565ec73b0bc244c24d9611
samdauwe <sam.dauwe@gmail.com>
2021-11-26 20:47:15 +0100

    Updated doc

--- src/MaterialsLibrary/include/babylon/materialslibrary/mix/mix_material.h
+++ src/MaterialsLibrary/include/babylon/materialslibrary/mix/mix_material.h
@@ -47,6 +47,11 @@ public:
   static MixMaterial* Parse(const json& source, Scene* scene, const std::string& rootUrl);
 
 protected:
+  /**
+   * @brief Constructor.
+   * @param name The name given to the material in order to identify it afterwards.
+   * @param scene The scene the material is used in.
+   */
   MixMaterial(const std::string& name, Scene* scene);
 
   BaseTexturePtr& get_mixTexture1();
