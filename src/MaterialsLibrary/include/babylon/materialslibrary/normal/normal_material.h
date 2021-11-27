21bae29bc8f9e9c261ac0fbd76e77d72a03e5481
samdauwe <sam.dauwe@gmail.com>
2021-11-27 20:12:18 +0100

    Update doc

--- src/MaterialsLibrary/include/babylon/materialslibrary/normal/normal_material.h
+++ src/MaterialsLibrary/include/babylon/materialslibrary/normal/normal_material.h
@@ -49,6 +49,11 @@ public:
   static NormalMaterial* Parse(const json& source, Scene* scene, const std::string& rootUrl);
 
 protected:
+  /**
+   * @brief Constructor
+   * @param name The name given to the material in order to identify it afterwards.
+   * @param scene The scene the material is used in.
+   */
   NormalMaterial(const std::string& name, Scene* scene);
 
   BaseTexturePtr& get_diffuseTexture();
