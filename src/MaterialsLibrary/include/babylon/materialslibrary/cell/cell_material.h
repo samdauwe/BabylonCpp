2123d2cee583c3cba7a850c4f7171841f4496525
samdauwe <sam.dauwe@gmail.com>
2021-11-28 18:09:14 +0100

    Updated doc

--- src/MaterialsLibrary/include/babylon/materialslibrary/cell/cell_material.h
+++ src/MaterialsLibrary/include/babylon/materialslibrary/cell/cell_material.h
@@ -47,6 +47,11 @@ public:
   static CellMaterial* Parse(const json& source, Scene* scene, const std::string& rootUrl);
 
 protected:
+  /**
+   * @brief Constructor.
+   * @param name The name given to the material in order to identify it afterwards.
+   * @param scene The scene the material is used in.
+   */
   CellMaterial(const std::string& name, Scene* scene);
 
   TexturePtr& get_diffuseTexture();
