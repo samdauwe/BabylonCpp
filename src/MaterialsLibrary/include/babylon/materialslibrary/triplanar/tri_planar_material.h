0516ebc2dd4720dc697e93ed894b7fa07e07388b
samdauwe <sam.dauwe@gmail.com>
2021-12-03 19:41:22 +0100

    Updated doc

--- src/MaterialsLibrary/include/babylon/materialslibrary/triplanar/tri_planar_material.h
+++ src/MaterialsLibrary/include/babylon/materialslibrary/triplanar/tri_planar_material.h
@@ -50,6 +50,11 @@ public:
   static TriPlanarMaterial* Parse(const json& source, Scene* scene, const std::string& rootUrl);
 
 protected:
+  /**
+   * @brief Constructor
+   * @param name The name given to the material in order to identify it afterwards.
+   * @param scene The scene the material is used in.
+   */
   TriPlanarMaterial(const std::string& name, Scene* scene);
 
   BaseTexturePtr& get_mixTexture();
