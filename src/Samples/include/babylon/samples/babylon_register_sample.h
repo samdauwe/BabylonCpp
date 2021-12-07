c7663212e62db1c13d530e30d3bad2ad22cca87c
samdauwe <sam.dauwe@gmail.com>
2021-12-07 20:40:03 +0100

    Formatting

--- src/Samples/include/babylon/samples/babylon_register_sample.h
+++ src/Samples/include/babylon/samples/babylon_register_sample.h
@@ -6,10 +6,10 @@
 #define BABYLONCPP_BABYLON_REGISTER_SAMPLE_H
 
 // BABYLON_REGISTER_SAMPLE : registers an example in the Samples Index
-#define  BABYLON_REGISTER_SAMPLE(categoryName, sampleClassName)                          \
-std::shared_ptr<IRenderableScene> make_##sampleClassName(ICanvas* canvas)                \
-{                                                                                        \
-  return std::make_shared<sampleClassName>(canvas);                                      \
-}
+#define BABYLON_REGISTER_SAMPLE(categoryName, sampleClassName)                                     \
+  std::shared_ptr<IRenderableScene> make_##sampleClassName(ICanvas* canvas)                        \
+  {                                                                                                \
+    return std::make_shared<sampleClassName>(canvas);                                              \
+  }
 
 #endif // BABYLONCPP_BABYLON_REGISTER_SAMPLE_H
